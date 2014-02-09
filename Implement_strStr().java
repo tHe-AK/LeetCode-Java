8d386b28c3c4fcea144394b854056b58c6b44a0d
Jason51122 <jason51122@users.noreply.github.com>
2014-02-09 13:47:00 -0500

    Update Implement_strStr().java

--- Implement_strStr().java
+++ Implement_strStr().java
@@ -2,6 +2,39 @@ public class Solution {
     public String strStr(String haystack, String needle) {
         int len1 = haystack.length();
         int len2 = needle.length();
+
+        if (len2 == 0) {
+            return haystack;
+        }
+        
+        if (len2 > len1) {
+            return null;
+        }
+        
+        int i;
+        for (i = 0; i < len1 - len2 + 1; i++) {
+            boolean flag = true;
+            int j = i;
+            for (int k = 0; k < len2; j++, k++) {
+                if (haystack.charAt(j) != needle.charAt(k)) {
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if (flag) {
+                return haystack.substring(i);
+            }
+        }
+        
+        return null;
+    }
+}
+
+public class Solution {
+    public String strStr(String haystack, String needle) {
+        int len1 = haystack.length();
+        int len2 = needle.length();
         
         if (len2 == 0) {
             return haystack;
