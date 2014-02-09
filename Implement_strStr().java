8c59586f4455d7f98772f979153cf5d6ce37b640
Jason51122 <jason51122@users.noreply.github.com>
2014-02-09 13:36:18 -0500

    Update and rename Implement strStr().java to Implement_strStr().java

--- Implement_strStr().java
+++ Implement_strStr().java
@@ -3,22 +3,17 @@ public class Solution {
         int len1 = haystack.length();
         int len2 = needle.length();
         
+        if (len2 == 0) {
+            return haystack;
+        }
+        
         if (len2 > len1) {
             return null;
         }
         
         int i;
         for (i = 0; i < len1 - len2 + 1; i++) {
-            boolean flag = true;
-            int j = i;
-            for (int k = 0; k < len2; j++, k++) {
-                if (haystack.charAt(j) != needle.charAt(k)) {
-                    flag = false;
-                    break;
-                }
-            }
-            
-            if (flag) {
+            if (haystack.substring(i).startsWith(needle)) {
                 return haystack.substring(i);
             }
         }
