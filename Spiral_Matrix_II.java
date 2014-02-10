20c79239234d9754c6f04a6808ad25c7b2056012
Jason51122 <jason51122@users.noreply.github.com>
2014-02-10 12:41:43 -0500

    Create Spiral_Matrix_II.java

---
+++ Spiral_Matrix_II.java
@@ -0,0 +1,46 @@
+public class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int count = 1;
+        int left = 0;
+        int right = n - 1;
+        int top = 0;
+        int bottom = n - 1;
+        
+        while (true) {
+            if (left > right) {
+                break;
+            }
+            for (int j = left; j <= right; j++) {
+                matrix[top][j] = count++;
+            }
+            top++;
+            
+            if (top > bottom) {
+                break;
+            }
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = count++;
+            }
+            right--;
+            
+            if (left > right) {
+                break;
+            }
+            for (int j = right; j >= left; j--) {
+                matrix[bottom][j] = count++;
+            }
+            bottom--;
+            
+            if (top > bottom) {
+                break;
+            }
+            for (int i = bottom; i >= top; i--) {
+                matrix[i][left] = count++;
+            }
+            left++;
+        }
+        
+        return matrix;
+    }
+}
