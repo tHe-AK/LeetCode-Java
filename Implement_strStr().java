3c1610631e75e8058165192e2174519c16c5bc9e
Jason51122 <jason51122@users.noreply.github.com>
2014-02-10 22:01:28 -0500

    Update Implement_strStr().java

--- Implement_strStr().java
+++ Implement_strStr().java
@@ -30,39 +30,3 @@ public class Solution {
         return null;
     }
 }
-
-public class Solution {
-    public String strStr(String haystack, String needle) {
-        int len1 = haystack.length();
-        int len2 = needle.length();
-        
-        if (len2 == 0) {
-            return haystack;
-        }
-        
-        if (len2 > len1) {
-            return null;
-        }
-        
-        int i;
-        for (i = 0; i < len1 - len2 + 1; i++) {
-            if (haystack.substring(i).startsWith(needle)) {
-                return haystack.substring(i);
-            }
-        }
-        
-        return null;
-    }
-}
-
-public class Solution {
-    public String strStr(String haystack, String needle) {        
-        int pos = haystack.indexOf(needle);
-        if (pos == -1) {
-            return null;
-        }
-        else {
-            return haystack.substring(pos);
-        }
-    }
-}
