c90db7ae116d2f264e8d3622cace19e92ce3ebd4
Jason51122 <jason51122@users.noreply.github.com>
2014-02-23 19:59:05 -0500

    Update Implement_strStr().java

--- Implement_strStr().java
+++ Implement_strStr().java
@@ -15,8 +15,7 @@ public class Solution {
             return null;
         }
         
-        int i;
-        for (i = 0; i < len1 - len2 + 1; i++) {
+        for (int i = 0; i < len1 - len2 + 1; i++) {
             boolean flag = true;
             int j = i;
             for (int k = 0; k < len2; j++, k++) {
@@ -28,6 +27,64 @@ public class Solution {
             
             if (flag) {
                 return haystack.substring(i);
+            }
+        }
+        
+        return null;
+    }
+}
+
+public class Solution {
+    public String strStr(String haystack, String needle) {
+        if (haystack == null || needle == null) {
+            return null;
+        }
+        
+        int len1 = haystack.length();
+        int len2 = needle.length();
+
+        if (len2 == 0) {
+            return haystack;
+        }
+        
+        if (len2 > len1) {
+            return null;
+        }
+        
+        int[] rec = new int[len2];
+        rec[0] = 0;
+        int i = 1;
+        int j = 0;
+        while (i < len2) {
+            if (needle.charAt(i) == needle.charAt(j)) {
+                j++;
+                rec[i] = j;
+                i++;
+            }
+            else if (j != 0) {
+                j = rec[j - 1];
+            }
+            else {
+                rec[i] = 0;
+                i++;
+            }
+        }
+        
+        i = 0;
+        j = 0;
+        while (i < len1) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+                if (j == len2) {
+                    return haystack.substring(i - j);
+                }
+            }
+            else if (j != 0) {
+                j = rec[j - 1];
+            }
+            else {
+                i++;
             }
         }
         
