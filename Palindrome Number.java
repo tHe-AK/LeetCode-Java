e3759c3224e783e662b28fc0858d64114ca8ee44
Jason51122 <jason51122@users.noreply.github.com>
2014-02-24 21:01:16 -0500

    Create Palindrome Number.java

---
+++ Palindrome Number.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        
+        int len = Integer.toString(x).length();
+        int low = 1;
+        int high = (int) Math.pow(10, len - 1);
+        
+        while (low < high) {
+            if (x / low % 10 != x / high % 10) {
+                return false;
+            }
+            low *= 10;
+            high /= 10;
+        }
+        
+        return true;
+    }
+}
