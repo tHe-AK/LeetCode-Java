97df5543b69e508cc9e64b6a010426c8f7f46af1
Jason51122 <jason51122@users.noreply.github.com>
2014-03-04 15:34:26 -0500

    Create Unique Paths.java

---
+++ Unique Paths.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        if (m <= 0 || n <= 0) {
+            return 0;
+        }
+        
+        int[][] rec = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    rec[i][j] = 1;
+                }
+                else {
+                    rec[i][j] = rec[i - 1][j] + rec[i][j - 1];
+                }
+            }
+        }
+        
+        return rec[m - 1][n - 1];
+    }
+}
