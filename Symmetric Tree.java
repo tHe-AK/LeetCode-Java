8039f0083a81ff1508fdbd97d7c5d0294c68ac16
Jason51122 <jason51122@users.noreply.github.com>
2014-03-04 14:11:14 -0500

    Update Symmetric Tree.java

--- Symmetric Tree.java
+++ Symmetric Tree.java
@@ -28,3 +28,47 @@ public class Solution {
         }
     }
 }
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        
+        Queue<TreeNode> q1 = new LinkedList<TreeNode>();
+        Queue<TreeNode> q2 = new LinkedList<TreeNode>();
+        q1.offer(root.left);
+        q2.offer(root.right);
+        
+        while (q1.isEmpty() == false) {
+            TreeNode cur1 = q1.peek();
+            TreeNode cur2 = q2.peek();
+            q1.poll();
+            q2.poll();
+            
+            if (cur1 == null && cur2 == null) {
+                continue;
+            }
+            else if (cur1 != null && cur2 != null && cur1.val == cur2.val) {
+                q1.offer(cur1.left);
+                q2.offer(cur2.right);
+                q1.offer(cur1.right);
+                q2.offer(cur2.left);
+            }
+            else {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
