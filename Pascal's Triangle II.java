fccafbcbc0939a5812603cc331785bb250a6a1bc
Jason51122 <jason51122@users.noreply.github.com>
2014-03-25 01:37:18 -0400

    Update Pascal's Triangle II.java

--- Pascal's Triangle II.java
+++ Pascal's Triangle II.java
@@ -5,8 +5,7 @@ public class Solution {
         }
         
         ArrayList<Integer> result = new ArrayList<Integer>();
-        result.add(1);
-        for (int i = 1; i <= rowIndex; i++) {
+        for (int i = 0; i <= rowIndex; i++) {
             for (int j = result.size() - 1; j > 0; j--) {
                 result.set(j, result.get(j - 1) + result.get(j));
             }
