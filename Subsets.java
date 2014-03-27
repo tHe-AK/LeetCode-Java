d7402ee61ac143572f1fdb7b2a2d72c89fc3f819
Jason51122 <jason51122@users.noreply.github.com>
2014-03-27 17:20:22 -0400

    Update Subsets.java

--- Subsets.java
+++ Subsets.java
@@ -1,5 +1,29 @@
 public class Solution {
     public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+        if (S == null || S.length == 0) {
+            return new ArrayList<ArrayList<Integer>>();
+        }
+        
+        Arrays.sort(S);
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        helper(new ArrayList<Integer>(), 0, S, result);
+        
+        return result;
+    }
+    
+    private void helper(ArrayList<Integer> cur, int index, int[] S, ArrayList<ArrayList<Integer>> result) {
+        result.add(new ArrayList<Integer>(cur));
+        
+        for (int i = index; i < S.length; i++) {
+            cur.add(S[i]);
+            helper(cur, i + 1, S, result);
+            cur.remove(cur.size() - 1);
+        }
+    }
+}
+
+public class Solution {
+    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
         if (S.length == 0) {
             return new ArrayList<ArrayList<Integer>>();
         }
