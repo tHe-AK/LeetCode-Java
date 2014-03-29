e630da4c3ec550de1906b3423e630f1f5658dc58
Jason51122 <jason51122@users.noreply.github.com>
2014-03-29 15:55:14 -0400

    Create Regular Expression Matching.java

---
+++ Regular Expression Matching.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        
+        return helper(s, 0, p, 0);
+    }
+    
+    private boolean helper(String s, int i, String p, int j) {
+        if (j == p.length()) {
+            return i == s.length();
+        }
+        else if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+            int k = i;
+            while (k < s.length() && (s.charAt(k) == p.charAt(j) || p.charAt(j) == '.')) {
+                if (helper(s, k + 1, p, j + 2) == true) {
+                    return true;
+                }
+                k++;
+            }
+            return helper(s, i, p, j + 2);
+        }
+        else {
+            return (i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) && helper(s, i + 1, p, j + 1) == true;
+        }
+    }
+}
