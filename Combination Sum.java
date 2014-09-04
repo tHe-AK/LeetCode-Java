0c05cc57deb6afbb6badaebc47154cd150e43b33
Jason51122 <jason51122@users.noreply.github.com>
2014-09-03 22:06:37 -0400

    Update Combination Sum.java

--- Combination Sum.java
+++ Combination Sum.java
@@ -11,7 +11,7 @@ public class Solution {
         return result;
     }
     
-    private void helper(int[] candidates, int target, int num, List<Integer> cur, List<List<Integer>> result) {
+    private void helper(int[] candidates, int target, int start, List<Integer> cur, List<List<Integer>> result) {
         if (target < 0) {
             return;
         }
@@ -21,7 +21,7 @@ public class Solution {
             return;
         }
         
-        for (int i = num; i < candidates.length; i++) {
+        for (int i = start; i < candidates.length; i++) {
             cur.add(candidates[i]);
             helper(candidates, target - candidates[i], i, cur, result);
             cur.remove(cur.size() - 1);
