c02492a9df185fc235ed602cd2eb985afc52eb9c
Jason51122 <jason51122@users.noreply.github.com>
2014-09-03 21:21:20 -0400

    Update Next Permutation.java

--- Next Permutation.java
+++ Next Permutation.java
@@ -4,34 +4,36 @@ public class Solution {
             return;
         }
         
-        int len = num.length;
-        int i = len - 2;
-        while (i >= 0 && num[i] >= num[i + 1]) {
+        int end = num.length - 1;
+        int i = end;
+        while (i > 0 && num[i - 1] >= num[i]) {
             i--;
         }
         
-        if (i == -1) {
-            Arrays.sort(num);
+        if (i == 0) {
+            reverse(num, 0, end);
             return;
         }
         
-        int j = len - 1;
-        while (num[i] >= num[j]) {
+        int j = end;
+        while (num[i - 1] >= num[j]) {
             j--;
         }
         
-        int temp = num[i];
-        num[i] = num[j];
+        int temp = num[i - 1];
+        num[i - 1] = num[j];
         num[j] = temp;
         
-        int left = i + 1;
-        int right = len - 1;
-        while (left < right) {
-            temp = num[left];
-            num[left] = num[right];
-            num[right] = temp;
-            left++;
-            right--;
+        reverse(num, i, end);
+    }
+    
+    private void reverse(int[] num, int start, int end) {
+        while (start < end) {
+            int temp = num[start];
+            num[start] = num[end];
+            num[end] = temp;
+            start++;
+            end--;
         }
     }
 }
