6b5885934b680dd9930306910d4ed8d0a7c7e253
Jason51122 <jason51122@users.noreply.github.com>
2014-09-04 13:42:07 -0400

    Update Subsets II.java

--- Subsets II.java
+++ Subsets II.java
@@ -1,25 +1,26 @@
 public class Solution {
-    public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
+    public List<List<Integer>> subsetsWithDup(int[] num) {
         if (num == null || num.length == 0) {
-            return new ArrayList<ArrayList<Integer>>();
+            return null;
         }
         
         Arrays.sort(num);
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        helper(new ArrayList<Integer>(), 0, num, result);
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        helper(num, 0, new ArrayList<Integer>(), result);
         
         return result;
     }
     
-    private void helper(ArrayList<Integer> cur, int index, int[] num, ArrayList<ArrayList<Integer>> result) {
+    private void helper(int[] num, int start, List<Integer> cur, List<List<Integer>> result) {
         result.add(new ArrayList<Integer>(cur));
         
-        for (int i = index; i < num.length; i++) {
-            if (i != index && num[i] == num[i - 1]) {
+        for (int i = start; i < num.length; i++) {
+            if (i != start && num[i - 1] == num[i]) {
                 continue;
             }
+            
             cur.add(num[i]);
-            helper(cur, i + 1, num, result);
+            helper(num, i + 1, cur, result);
             cur.remove(cur.size() - 1);
         }
     }
