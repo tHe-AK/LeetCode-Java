d0fef3008ef66f1b5f38109c8e4f17aee73eeb98
Jason51122 <jason51122@users.noreply.github.com>
2014-09-04 16:02:31 -0400

    Update Binary Search.java

--- Binary Search.java
+++ Binary Search.java
@@ -1,30 +1,30 @@
 class Solution {
-	/**
-	 * @param array
-	 *            source array
-	 * @param target
-	 *            target to search
-	 * @return the first occurrence position of target
-	 */
-	public static int binarySearch(int[] array, int target) {
-		if (array == null || array.length == 0) {
-			return -1;
-		}
+    /**
+     * @param array
+     *            source array
+     * @param target
+     *            target to search
+     * @return the first occurrence position of target
+     */
+    public static int binarySearch(int[] array, int target) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
 
-		int start = 0;
-		int end = array.length - 1;
-		int mid;
+        int start = 0;
+        int end = array.length - 1;
+        int mid;
 
-		while (start <= end) {
-			mid = start + (end - start) / 2;
-			if (array[mid] == target) {
-				return mid;
-			} else if (array[mid] < target) {
-				start = mid + 1;
-			} else {
-				end = mid - 1;
-			}
-		}
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
 
-		return -1;
-	}
+        return -1;
+    }
