94df4a225b09990f2946dc4dc736700779968ac4
Jason51122 <jason51122@users.noreply.github.com>
2014-09-04 20:50:47 -0400

    Create Search a 2D Matrix II.java

---
+++ Search a 2D Matrix II.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
+            return false;
+        }
+        
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int x = row - 1;
+        int y = 0;
+
+        while (x >= 0 && y < col) {
+            if (matrix[x][y] == target) {
+                return true;
+            }
+            else if (matrix[x][y] < target) {
+                y++;
+            }
+            else {
+                x--;
+            }
+        }
+        
+        return false;
+    }
+}
