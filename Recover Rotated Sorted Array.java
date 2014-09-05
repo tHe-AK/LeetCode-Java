55bfc04101bc057616f892038976ec1620f9f7c7
Jason51122 <jason51122@users.noreply.github.com>
2014-09-05 17:54:55 -0400

    Update Recover Rotated Sorted Array.java

--- Recover Rotated Sorted Array.java
+++ Recover Rotated Sorted Array.java
@@ -1,28 +1,28 @@
 public class Solution {
-	/**
-	 * @param nums
-	 *            The rotated sorted array
-	 * @return: The recovered sorted array
-	 */
-	public void recoverRotatedSortedArray(ArrayList<Integer> nums) {
-		if (nums == null || nums.size() <= 1) {
-			return;
-		}
+    /**
+     * @param nums
+     *            The rotated sorted array
+     * @return: The recovered sorted array
+     */
+    public void recoverRotatedSortedArray(ArrayList<Integer> nums) {
+        if (nums == null || nums.size() <= 1) {
+            return;
+        }
 
-		for (int i = 0; i < nums.size() - 1; i++) {
-			if (nums.get(i) > nums.get(i + 1)) {
-				helper(nums, 0, i);
-				helper(nums, i + 1, nums.size() - 1);
-				helper(nums, 0, nums.size() - 1);
-			}
-		}
-	}
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums.get(i) > nums.get(i + 1)) {
+                helper(nums, 0, i);
+                helper(nums, i + 1, nums.size() - 1);
+                helper(nums, 0, nums.size() - 1);
+            }
+        }
+    }
 
-	private void helper(ArrayList<Integer> nums, int start, int end) {
-		while (start < end) {
-			int temp = nums.get(start);
-			nums.set(start, nums.get(end));
-			nums.set(end, temp);
-		}
-	}
+    private void helper(ArrayList<Integer> nums, int start, int end) {
+        while (start < end) {
+            int temp = nums.get(start);
+            nums.set(start, nums.get(end));
+            nums.set(end, temp);
+        }
+    }
 }
