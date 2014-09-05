eb7fa02a4fdd220f5d6db1e77d1d7e209ad5fcb1
Jason51122 <jason51122@users.noreply.github.com>
2014-09-04 21:09:41 -0400

    Update Find First Bad Version.java

--- Find First Bad Version.java
+++ Find First Bad Version.java
@@ -1,27 +1,27 @@
 class Solution {
-	public static int findFirstBadVersion(int[] array) {
-		if (array == null || array.length == 0) {
-			return -1;
-		}
+    public static int findFirstBadVersion(int[] array) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
 
-		int low = 0;
-		int high = array.length - 1;
-		int mid;
+        int low = 0;
+        int high = array.length - 1;
+        int mid;
 
-		while (low <= high) {
-			mid = low + (high - low) / 2;
-			if (isBadVersion(array[mid])) {
-				high = mid - 1;
-			} else {
-				low = mid + 1;
-			}
-		}
-		
-		if (low == array.length) {
-			return -1;
-		}
-		else {
-			return low;
-		}
-	}
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (isBadVersion(array[mid])) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        if (low == array.length) {
+            return -1;
+        }
+        else {
+            return low;
+        }
+    }
 }
