b17d1fc0f0728ca0758c39ba8593de55d78f9dbe
Jason51122 <jason51122@users.noreply.github.com>
2014-09-05 18:37:45 -0400

    Update Reverse Words in a String.java

--- Reverse Words in a String.java
+++ Reverse Words in a String.java
@@ -1,28 +1,31 @@
 public class Solution {
     public String reverseWords(String s) {
-        if (s == null || s.length() == 0) {
+        if (s == null || s.length() < 1) {
             return s;
         }
         
-        StringBuffer result = new StringBuffer("");
-        int end = 0;
+        StringBuilder sb = new StringBuilder();
+        int pre = s.length() - 1;
+
         for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) != ' ') {
-                if (i == s.length() - 1 || s.charAt(i + 1) == ' ') {
-                    end = i;
-                    result.append(' ');
-                }
-                if (i == 0 || s.charAt(i - 1) == ' ') {
-                    for (int j = i; j <= end; j++) {
-                        result.append(s.charAt(j));
-                    }
-                }
+            if (s.charAt(i) == ' ') {
+                continue;
+            }
+            
+            if (i == s.length() - 1 || s.charAt(i + 1) == ' ') {
+                pre = i;
+            }
+            
+            if (i == 0 || s.charAt(i - 1) == ' ') {
+                sb.append(' ');
+                sb.append(s.substring(i, pre + 1));
             }
         }
         
-        if (result.length() > 0) {
-            result.deleteCharAt(0);
+        if (sb.length() > 0) {
+            sb.deleteCharAt(0);
         }
-        return result.toString();
+        
+        return sb.toString();
     }
 }
