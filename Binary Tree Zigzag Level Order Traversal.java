7d3feec84684ca58f462b90dee5156c0d019e70b
Jason51122 <jason51122@users.noreply.github.com>
2014-09-06 14:56:42 -0400

    Update Binary Tree Zigzag Level Order Traversal.java

--- Binary Tree Zigzag Level Order Traversal.java
+++ Binary Tree Zigzag Level Order Traversal.java
@@ -8,37 +8,40 @@
  * }
  */
 public class Solution {
-    public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        
         if (root == null) {
-            return new ArrayList<ArrayList<Integer>>();
+            return result;
         }
         
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        Queue<TreeNode> level = new LinkedList<TreeNode>();
-        level.offer(root);
-        boolean flag = false;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        boolean reverse = false;
         
-        while (level.isEmpty() == false) {
-            ArrayList<Integer> cur = new ArrayList<Integer>();
-            int size = level.size();
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<Integer>();
+            
             for (int i = 0; i < size; i++) {
-                TreeNode peek = level.peek();
-                level.poll();
-                cur.add(peek.val);
-                if (peek.left != null) {
-                    level.offer(peek.left);
+                TreeNode cur = queue.remove();
+                level.add(cur.val);
+                
+                if (cur.left != null) {
+                    queue.add(cur.left);
                 }
-                if (peek.right != null) {
-                    level.offer(peek.right);
+                
+                if (cur.right != null) {
+                    queue.add(cur.right);
                 }
             }
             
-            if (flag == true) {
-                Collections.reverse(cur);
+            if (reverse) {
+                Collections.reverse(level);
             }
+            reverse = !reverse;
             
-            result.add(new ArrayList<Integer>(cur));
-            flag = !flag;
+            result.add(level);
         }
         
         return result;
