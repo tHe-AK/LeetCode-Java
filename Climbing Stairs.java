613c4f3e191e1cae5248aa4214f9b418ad9a79df
Jason51122 <jason51122@users.noreply.github.com>
2014-09-08 22:54:17 -0400

    Update Climbing Stairs.java

--- Climbing Stairs.java
+++ Climbing Stairs.java
@@ -1,15 +1,17 @@
 public class Solution {
     public int climbStairs(int n) {
-        if (n <= 0) {
-            return 0;
+        if (n < 0) {
+            throw new IllegalArgumentException();
         }
-        else if (n < 3) {
+        
+        if (n <= 2) {
             return n;
         }
         
         int pre1 = 1;
         int pre2 = 2;
-        int cur = 0;
+        int cur = 3;
+        
         for (int i = 3; i <= n; i++) {
             cur = pre1 + pre2;
             pre1 = pre2;
