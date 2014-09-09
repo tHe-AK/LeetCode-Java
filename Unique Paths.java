4cdc37f6180c8f7bda5088a9fdc9cbf2fa8003b3
Jason51122 <jason51122@users.noreply.github.com>
2014-09-08 21:50:30 -0400

    Update Unique Paths.java

--- Unique Paths.java
+++ Unique Paths.java
@@ -5,6 +5,7 @@ public class Solution {
         }
         
         int[][] rec = new int[m][n];
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (i == 0 || j == 0) {
