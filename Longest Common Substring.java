55ea243af4e2248619b9464cbbc09dd0de8c041a
Jason51122 <jason51122@users.noreply.github.com>
2014-09-11 12:40:01 -0400

    Create Longest Common Substring.java

---
+++ Longest Common Substring.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int longestCommonSubstring(String str1, String str2) {
+        if (str1 == null || str1.length() == 0 || str2 == null || str2.length() == 0) {
+            return 0;
+        }
+        
+        int len1 = str1.length();
+        int len2 = str2.length();
+        int[][] rec = new int[len1 + 1][len2 + 1];
+        
+        for (int i = 0; i < len1 + 1; i++) {
+            for (int j = 0;j < len2 + 1; j++) {
+                if (i == 0 || j == 0 || str1.charAt(i - 1) != str2.charAt(j - 1)) {
+                    rec[i][j] = 0;
+                }
+                else {
+                    rec[i][j] = rec[i - 1][j - 1] + 1;
+                }
+            }
+        }
+        
+        return rec[len1][len2];
+    }
+}
