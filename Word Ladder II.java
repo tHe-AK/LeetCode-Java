85355ff03c183bceaa4bd27da1d7a0dac3a3a644
Jason51122 <jason51122@users.noreply.github.com>
2014-09-13 01:52:16 -0400

    Update Word Ladder II.java

--- Word Ladder II.java
+++ Word Ladder II.java
@@ -1,73 +1,87 @@
 public class Solution {
-    public ArrayList<ArrayList<String>> findLadders(String start, String end, HashSet<String> dict) {
-        ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
-        if (start == null || end == null || dict == null || start.equals(end) == true || dict.size() < 2 || dict.contains(start) == false || dict.contains(end) == false) {
+    public List<List<String>> findLadders(String start, String end, Set<String> dict) {
+        if (start == null || end == null || dict == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        List<List<String>> result = new ArrayList<List<String>>();
+
+        if (start.equals(end)) {
+            List<String> path = new ArrayList<String>();
+            path.add(start);
+            result.add(path);
             return result;
         }
         
+        boolean flag = true;
+        HashMap<String, List<String>> hashMap = new HashMap<String, List<String>>();
+        Set<String> visited = new HashSet<String>();
         Queue<String> queue = new LinkedList<String>();
-        HashSet<String> visited = new HashSet<String>();
-        HashMap<String, ArrayList<String>> hashMap = new HashMap<String, ArrayList<String>>();
-        queue.add(start);
         visited.add(start);
-        boolean flag = false;
+        queue.offer(start);
         
-        while (queue.isEmpty() == false && flag == false) {
+        while (flag && !queue.isEmpty()) {
             int size = queue.size();
-            HashSet<String> level = new HashSet<String>();
-
+            Set<String> level = new HashSet<String>();
+            
             for (int i = 0; i < size; i++) {
-                String front = queue.remove();
-                ArrayList<String> value = new ArrayList<String>();
-                for (int j = 0; j < front.length(); j++) {
-                    StringBuilder temp = new StringBuilder(front);
+                String cur = queue.poll();
+                hashMap.put(cur, new ArrayList<String>());
+
+                for (int j = 0; j < cur.length(); j++) {
+                    StringBuilder sb = new StringBuilder(cur);
+                            
                     for (char k = 'a'; k <= 'z'; k++) {
-                        temp.setCharAt(j, k);
-                        String str = temp.toString();
-                        if (visited.contains(str) == false && dict.contains(str) == true) {
-                            value.add(str);
-                            level.add(str);
+                        if (k == sb.charAt(j)) {
+                            continue;
+                        }
+                                
+                        sb.setCharAt(j, k);
+                        String word = sb.toString();
+                                
+                        if (word.equals(end)) {
+                        	flag = false;
+                        }
+                        
+                        if (dict.contains(word) && !visited.contains(word)) {
+                            hashMap.get(cur).add(word);
+                            level.add(word);
                         }
                     }
                 }
-                hashMap.put(front, value);
             }
             
-            for (String str : level) {
-                if (str.equals(end) == true) {
-                    flag = true;
-                    break;
-                }
-                queue.add(str);
-                visited.add(str);
+            for (String word : level) {               
+                visited.add(word);
+                queue.offer(word);
             }
         }
         
-        if (flag == false) {
+        if (flag) {
             return result;
         }
         
-        ArrayList<String> path = new ArrayList<String>();
-        path.add(start);
-        helper(start, end, path, hashMap, result);
+        List<String> cur = new ArrayList<String>();
+        cur.add(start);
+        helper(start, end, hashMap, cur, result);
         
         return result;
     }
     
-    private void helper(String start, String end, ArrayList<String> path, HashMap<String, ArrayList<String>> hashMap, ArrayList<ArrayList<String>> result) {
-        if (start.equals(end) == true) {
-            result.add(new ArrayList<String>(path));
+    private void helper(String start, String end, HashMap<String, List<String>> hashMap, List<String> cur, List<List<String>> result) {
+        if (start.equals(end)) {
+            result.add(new ArrayList<String>(cur));
             return;
         }
         
-        if (hashMap.containsKey(start) == false) {
+        if (!hashMap.containsKey(start)) {
             return;
         }
         
-        for (String value : hashMap.get(start)) {
-            path.add(value);
-            helper(value, end, path, hashMap, result);
-            path.remove(path.size() - 1);
+        for (String word : hashMap.get(start)) {
+            cur.add(word);
+            helper(word, end, hashMap, cur, result);
+            cur.remove(cur.size() - 1);
         }
     }
 }
