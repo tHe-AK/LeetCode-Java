ea6d4d353012e98ed32c62ea3ae20000cd037ab1
Jason51122 <jason51122@users.noreply.github.com>
2014-09-14 22:49:37 -0400

    Update Least Common Ancestor.java

--- Least Common Ancestor.java
+++ Least Common Ancestor.java
@@ -11,26 +11,6 @@ class TreeNode {
 public class Solution {
     private TreeNode root;
 
-    public void insert(int val) {
-        root = insert(root, val);
-    }
-
-    private TreeNode insert(TreeNode p, int val) {
-        if (p == null) {
-            return new TreeNode(val);
-        }
-
-        if (p.val == val) {
-            throw new RuntimeException("Can not insert");
-        } else if (p.val < val) {
-            p.right = insert(p.right, val);
-        } else {
-            p.left = insert(p.left, val);
-        }
-
-        return p;
-    }
-
     public TreeNode LeastCommonAncestor(TreeNode x, TreeNode y) {
         if (x == null || y == null) {
             return null;
@@ -54,19 +34,5 @@ public class Solution {
         } else {
             return p;
         }
-    }
-
-    public static void main(String[] args) {
-        Solution s = new Solution();
-
-        s.insert(4);
-        s.insert(2);
-        s.insert(6);
-        s.insert(1);
-        s.insert(3);
-        s.insert(5);
-        s.insert(7);
-        
-        System.out.println(s.LeastCommonAncestor(s.root.left, s.root.right).val);
     }
 }
