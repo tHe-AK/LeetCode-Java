1b435b1fcfa1f461b07cf8fd7ce896f4d68324ba
Jason51122 <jason51122@users.noreply.github.com>
2014-09-16 23:05:05 -0400

    Update Best Time to Buy and Sell Stock III.java

--- Best Time to Buy and Sell Stock III.java
+++ Best Time to Buy and Sell Stock III.java
@@ -1,28 +1,32 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) {
+        if (prices == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        if (prices.length == 0) {
             return 0;
         }
         
-        int len = prices.length;
-        int[] first = new int[len];
-        int[] second = new int[len];
-        
-        int min = prices[0];
-        for (int i = 1; i < len; i++) {
-            first[i] = Math.max(prices[i] - min, first[i - 1]);
-            min = Math.min(min, prices[i]);
-        }
-        
-        int max = prices[len - 1];
-        for (int i = len - 2; i >= 0; i--) {
-            second[i] = Math.max(max - prices[i], second[i + 1]);
-            max = Math.max(max, prices[i]);
-        }
-        
         int profit = 0;
+        int len = prices.length;
+        int[] left = new int[len];
+        int[] right = new int[len];
+        int low = prices[0];
+        int high = prices[len - 1];
+        
+        for (int i = 1; i < len; i++) {
+            left[i] = Math.max(left[i - 1], prices[i] - low);
+            low = Math.min(low, prices[i]);
+        }
+        
+        for (int i = len - 2; i >= 0; i--) {
+            right[i] = Math.max(right[i + 1], high - prices[i]);
+            high = Math.max(high, prices[i]);
+        }
+        
         for (int i = 0; i < len; i++) {
-            profit = Math.max(profit, first[i] + second[i]);
+            profit = Math.max(profit, left[i] + right[i]);
         }
         
         return profit;
