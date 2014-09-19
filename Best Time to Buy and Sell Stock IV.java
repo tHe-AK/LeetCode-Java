4b8c5ea593f3e4656c30ba4d44750e54a6123bcb
Jason51122 <jason51122@users.noreply.github.com>
2014-09-18 23:02:04 -0400

    Update Best Time to Buy and Sell Stock IV.java

--- Best Time to Buy and Sell Stock IV.java
+++ Best Time to Buy and Sell Stock IV.java
@@ -3,41 +3,40 @@ public class Solution {
         if (prices == null) {
             throw new IllegalArgumentException();
         }
-        
-        if (prices.length == 0) {
+
+        if (prices.length == 0 || prices.length == 1) {
             return 0;
         }
-        
+
         int len = prices.length;
         int[][] rec = new int[len][len];
         int[][] result = new int[count][len];
-        
+
         for (int i = 0; i < len; i++) {
             int low = prices[i];
-            
+
             for (int j = i + 1; j < len; j++) {
                 rec[i][j] = Math.max(rec[i][j - 1], prices[j] - low);
                 low = Math.min(low, prices[j]);
             }
         }
-        
+
         for (int i = 0; i < count; i++) {
-            for (int j = i; j < len; j++) {
+            for (int j = i + 1; j < len; j++) {
                 if (i == 0) {
                     result[i][j] = rec[0][j];
-                }
-                else {
+                } else {
                     int profit = 0;
-                    
-                    for (int k = i - 1; k <= j; k++) {
+
+                    for (int k = i + 1; k < j; k++) {
                         profit = Math.max(profit, result[i - 1][k] + rec[k][j]);
                     }
-                    
+
                     result[i][j] = profit;
                 }
             }
         }
-        
+
         return result[count - 1][len - 1];
     }
 }
