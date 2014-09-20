e4f013776f8e00cb05871ba8f397769fa646c75d
Jason51122 <jason51122@users.noreply.github.com>
2014-09-19 23:33:11 -0400

    Create Sqrt.java

---
+++ Sqrt.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public double sqrt(double x) {
+        double xhalf = 0.5d * x;
+        
+        long i = Double.doubleToLongBits(x);
+        i = 0x5fe6ec85e7de30daL - (i >> 1);
+        x = Double.longBitsToDouble(i);
+        
+        x = x * (1.5d - xhalf * x * x);
+        
+        return x;
+    }
+}
