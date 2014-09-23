39a588df6c3eeb1586787baefb2282c95cb722e8
Jason51122 <wuzhidong@Zhidongs-MacBook-Pro.local>
2014-09-22 23:27:17 -0400

    Start LeetCode

--- Binary Search/Median of Two Sorted Arrays.java
+++ Binary Search/Median of Two Sorted Arrays.java
@@ -1,12 +1,13 @@
 public class Solution {
     public double findMedianSortedArrays(int A[], int B[]) {
         if (A == null || B == null) {
-            return 0;
+            throw new IllegalArgumentException();
         }
         
         int len = A.length + B.length;
+        
         if (len % 2 == 0) {
-            return (helper(A, 0, B, 0, len / 2) + helper(A, 0, B, 0, len / 2 + 1)) / 2.0; 
+            return (helper(A, 0, B, 0, len / 2) + helper(A, 0, B, 0, len / 2 + 1)) / 2.0;
         }
         else {
             return helper(A, 0, B, 0, len / 2 + 1);
@@ -26,10 +27,10 @@ public class Solution {
             return Math.min(A[A_start], B[B_start]);
         }
         
-        int A_key = (A_start + k / 2 - 1 < A.length) ? A[A_start + k / 2 - 1] : Integer.MAX_VALUE;
-        int B_key = (B_start + k / 2 - 1 < B.length) ? B[B_start + k / 2 - 1] : Integer.MAX_VALUE;
+        int A_mid = A_start + k / 2 - 1 < A.length ? A[A_start + k / 2 - 1] : Integer.MAX_VALUE;
+        int B_mid = B_start + k / 2 - 1 < B.length ? B[B_start + k / 2 - 1] : Integer.MAX_VALUE;
         
-        if (A_key < B_key) {
+        if (A_mid <= B_mid) {
             return helper(A, A_start + k / 2, B, B_start, k - k / 2);
         }
         else {
