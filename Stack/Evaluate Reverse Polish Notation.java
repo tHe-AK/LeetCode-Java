c3a12f561841eb3e6bc4ab4f921f88a22ba2a243
Jason51122 <wuzhidong@Zhidongs-MacBook-Pro.local>
2014-09-27 17:58:54 -0400

    Update

---
+++ Stack/Evaluate Reverse Polish Notation.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        if (tokens == null || tokens.length < 3) {
+            throw new IllegalArgumentException();
+        }
+        
+        Stack<Integer> stack = new Stack<Integer>();
+        HashSet<String> rec = new HashSet<String>();
+        rec.add("+");
+        rec.add("-");
+        rec.add("*");
+        rec.add("/");
+        
+        for (int i = 0; i < tokens.length; i++) {
+            if (rec.contains(tokens[i])) {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                
+                if (tokens[i].equals("+")) {
+                    stack.push(num1 + num2);
+                }
+                else if (tokens[i].equals("-")) {
+                    stack.push(num1 - num2);
+                }
+                else if (tokens[i].equals("*")) {
+                    stack.push(num1 * num2);
+                }
+                else {
+                    stack.push(num1 / num2);
+                }
+            }
+            else {
+                stack.push(Integer.parseInt(tokens[i]));
+            }
+        }
+        
+        return stack.peek();
+    }
+}
--- Stack/Largest Rectangle in Histogram.java
+++ Stack/Largest Rectangle in Histogram.java
@@ -4,23 +4,20 @@ public class Solution {
             throw new IllegalArgumentException();
         }
         
-        int len = height.length + 1;
-        int[] rec = new int[len];
-        System.arraycopy(height, 0, rec, 0, len - 1);
-        rec[len - 1] = 0;
-        
         int max = 0;
+        int[] rec = new int[height.length + 1];
+        System.arraycopy(height, 0, rec, 0, height.length);
         Stack<Integer> stack = new Stack<Integer>();
         int i = 0;
         
-        while (i < len) {
-            if (stack.empty() || rec[i] > rec[stack.peek()]) {
+        while (i < rec.length) {
+            if (stack.empty() || rec[stack.peek()] < rec[i]) {
                 stack.push(i);
                 i++;
             }
             else {
-                int pos = stack.pop();
-                max = Math.max(max, stack.empty() ? i * rec[pos] : (i - stack.peek() - 1) * rec[pos]);
+                int peek = stack.pop();
+                max = Math.max(max, stack.empty() ? rec[peek] * i : rec[peek] * (i - stack.peek() - 1));
             }
         }
         
