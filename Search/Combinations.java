53b3bc5599db1536d9db7d70f8c4b9591f1b9879
Jason51122 <wuzhidong@Zhidongs-MacBook-Pro.local>
2014-10-01 23:10:59 -0400

    Update

--- Search/Combinations.java
+++ Search/Combinations.java
@@ -5,6 +5,38 @@ public class Solution {
         }
         
         List<List<Integer>> result = new ArrayList<List<Integer>>();
+        result.add(new ArrayList<Integer>());
+        
+        for (int i = 1; i <= k; i++) {
+            List<List<Integer>> next = new ArrayList<List<Integer>>();
+            
+            for (int j = 0; j < result.size(); j++) {
+                for (int l = 1; l <= n; l++) {
+                    List<Integer> cur = new ArrayList<Integer>(result.get(j));
+                    
+                    if (cur.size() > 0 && cur.get(cur.size() - 1) >= l) {
+                        continue;
+                    }
+                    
+                    cur.add(l);
+                    next.add(cur);
+                }
+            }
+            
+            result = next;
+        }
+        
+        return result;
+    }
+}
+
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        if (n < 0 || k < 0) {
+            throw new IllegalArgumentException();
+        }
+        
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
         
         helper(n, k, 1, new ArrayList<Integer>(), result);
         
--- Search/Letter Combinations of a Phone Number.java
+++ Search/Letter Combinations of a Phone Number.java
@@ -5,6 +5,44 @@ public class Solution {
         }
         
         List<String> result = new ArrayList<String>();
+        result.add("");
+        HashMap<Character, String> rec = new HashMap<Character, String>();
+        rec.put('1', "");
+        rec.put('2', "abc");
+        rec.put('3', "def");
+        rec.put('4', "ghi");
+        rec.put('5', "jkl");
+        rec.put('6', "mno");
+        rec.put('7', "pqrs");
+        rec.put('8', "tuv");
+        rec.put('9', "wxyz");
+        rec.put('0', " ");
+        
+        for (int i = 0; i < digits.length(); i++) {
+            List<String> next = new ArrayList<String>();
+            
+            for (int j = 0; j < result.size(); j++) {
+                String val = rec.get(digits.charAt(i));
+                
+                for (int k = 0; k < val.length(); k++) {
+                    next.add(result.get(j) + val.charAt(k));
+                }
+            }
+            
+            result = next;
+        }
+        
+        return result;
+    }
+}
+
+public class Solution {
+    public List<String> letterCombinations(String digits) {
+        if (digits == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        List<String> result = new ArrayList<String>();
         HashMap<Character, String> rec = new HashMap<Character, String>();
         rec.put('1', "");
         rec.put('2', "abc");
