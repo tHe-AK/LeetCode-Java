ba3cea7c1af6da2069a4730fcd2d2746dc4191fa
Jason51122 <jason51122@hotmail.com>
2014-10-22 21:12:34 -0400

    Update

--- Tree/Flatten Binary Tree to Linked List.java
+++ Tree/Flatten Binary Tree to Linked List.java
@@ -13,6 +13,51 @@ public class Solution {
             return;
         }
         
+        helper(root);
+    }
+    
+    private TreeNode helper(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+        TreeNode left = helper(root.left);
+        TreeNode right = helper(root.right);
+        
+        root.left = null;
+        
+        if (left == null) {
+            root.right = right;
+        }
+        else {
+            root.right = left;
+
+            while (left.right != null) {
+                left = left.right;
+            }
+            
+            left.right = right;
+        }
+        
+        return root;
+    }
+}
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        
         while (root != null) {
             TreeNode cur = root.left;
             
