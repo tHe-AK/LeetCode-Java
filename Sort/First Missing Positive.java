ab2c637fd1b3ba8b411205db34c8ba99aae7e026
Jason51122 <jason51122@hotmail.com>
2014-10-23 22:01:07 -0400

    Update

--- Sort/First Missing Positive.java
+++ Sort/First Missing Positive.java
@@ -5,7 +5,7 @@ public class Solution {
         }
         
         for (int i = 0; i < A.length; i++) {
-            while (A[i] > 0 && A[i] != i + 1 && A[i] - 1 < A.length && A[A[i] - 1] != A[i]) {
+            while (A[i] - 1 >= 0 && A[i] - 1 != i && A[i] - 1 < A.length && A[A[i] - 1] != A[i]) {
                 int temp = A[A[i] - 1];
                 A[A[i] - 1] = A[i];
                 A[i] = temp;
