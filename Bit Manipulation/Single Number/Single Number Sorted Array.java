edd18c69d2b047c91b5719ff0d4367742ae332bd
Jason51122 <jason51122@hotmail.com>
2014-10-26 21:14:13 -0400

    Update

--- Bit Manipulation/Single Number/Single Number Sorted Array.java
+++ Bit Manipulation/Single Number/Single Number Sorted Array.java
@@ -1,36 +1,36 @@
 public class Solution {
-	public int singleNum(int[] array) {
-		if (array == null || array.length % 2 == 0) {
-			throw new IllegalArgumentException();
-		}
-		
-		int low = 0;
-		int high = array.length - 1;
-		
-		while (low <= high) {
-			int mid = low + (high - low) / 2;
-			
-			if (low == high || (array[mid] != array[mid - 1] && array[mid] != array[mid + 1])) {
-				return array[mid];
-			}
-			else if (array[mid] == array[mid - 1]) {
-				if ((mid - 1) % 2 == 0) {
-					low = mid + 1;
-				}
-				else {
-					high = mid - 2;
-				}
-			}
-			else {
-				if (mid % 2 == 0) {
-					low = mid + 2;
-				}
-				else {
-					high = mid - 1;
-				}
-			}
-		}
-		
-		throw new IllegalArgumentException();
-	}
+    public int singleNum(int[] array) {
+        if (array == null || array.length % 2 == 0) {
+            throw new IllegalArgumentException();
+        }
+        
+        int low = 0;
+        int high = array.length - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (low == high || (array[mid] != array[mid - 1] && array[mid] != array[mid + 1])) {
+                return array[mid];
+            }
+            else if (array[mid] == array[mid - 1]) {
+                if ((mid - 1) % 2 == 0) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 2;
+                }
+            }
+            else {
+                if (mid % 2 == 0) {
+                    low = mid + 2;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+        
+        throw new IllegalArgumentException();
+    }
 }
