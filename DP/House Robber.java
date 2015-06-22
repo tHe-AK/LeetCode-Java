a1683f63b59344e0f38642d91ee877b2b13a0166
Jason51122 <jason51122@users.noreply.github.com>
2015-06-21 18:09:45 -0700

    Create House Robber.java

---
+++ DP/House Robber.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int size = nums.length;
+        int[] rec = new int[size + 1];
+        rec[0] = 0;
+        rec[1] = nums[0];
+        
+        for (int i = 2; i <= size; i++) {
+            rec[i] = Math.max(rec[i - 2] + nums[i - 1], rec[i - 1]);
+        }
+        
+        return rec[size];
+    }
+}
