fe384104c46424020654468cf3c7bb44c58a0958
Jason51122 <jason51122@users.noreply.github.com>
2015-06-24 23:49:59 -0700

    Update Best Time to Buy and Sell Stock IV.java

--- DP/Stock/Best Time to Buy and Sell Stock IV.java
+++ DP/Stock/Best Time to Buy and Sell Stock IV.java
@@ -14,12 +14,12 @@ public class Solution {
         int[][] global = new int[len][k + 1];
          
         for (int i = 1; i < len; i++) {
-        	int diff = prices[i] - prices[i - 1];
+            int diff = prices[i] - prices[i - 1];
         
-        	for (int j = 1; j <= k; j++) {
-        		local[i][j] = Math.max(global[i - 1][j - 1] + Math.max(diff, 0), local[i - 1][j] + diff);
-        		global[i][j] = Math.max(global[i - 1][j], local[i][j]);
-        	}
+            for (int j = 1; j <= k; j++) {
+                local[i][j] = Math.max(global[i - 1][j - 1] + Math.max(diff, 0), local[i - 1][j] + diff);
+                global[i][j] = Math.max(global[i - 1][j], local[i][j]);
+            }
         }
         
         return global[len - 1][k];
