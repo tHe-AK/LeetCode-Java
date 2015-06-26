2d6d7acddf7f32553b0485a601077371e6f7f536
Jason51122 <jason51122@users.noreply.github.com>
2015-06-25 22:17:13 -0700

    Create Summary Ranges.java

---
+++ Array/Summary Ranges.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<String>();
+        
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        
+        int pre = 0;
+        int len = nums.length;
+        
+        for (int i = 1; i <= len; i++) {
+            if (i == len || nums[i] != nums[i - 1] + 1) {
+                String cur = String.valueOf(nums[pre]);
+                
+                if (pre < i - 1) {
+                    cur += "->" + nums[i - 1];
+                }
+                
+                result.add(cur);
+                pre = i;
+            }
+        }
+        
+        return result;
+    }
+}
