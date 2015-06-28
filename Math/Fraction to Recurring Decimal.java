d6067dada681a6eb39a8a33e160e332cee0165ee
Jason51122 <jason51122@users.noreply.github.com>
2015-06-28 14:26:23 -0700

    Create Fraction to Recurring Decimal.java

---
+++ Math/Fraction to Recurring Decimal.java
@@ -0,0 +1,46 @@
+public class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) {
+            return "0";
+        }
+        
+        if (denominator == 0) {
+            return null;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        if ((numerator < 0) ^ (denominator < 0)) {
+            sb.append("-");
+        }
+        
+        long num = numerator;
+        long den = denominator;
+        num = Math.abs(num);
+        den = Math.abs(den);
+        sb.append(num / den);
+        
+        if (num % den == 0) {
+            return sb.toString();
+        }
+        
+        sb.append(".");
+        num = num % den * 10;
+        HashMap<Long, Integer> rec = new HashMap<Long, Integer>();
+        
+        while (num != 0) {
+            if (rec.containsKey(num)) {
+                sb.insert(rec.get(num), "(");
+                sb.append(")");
+                return sb.toString();
+            }
+            else {
+                rec.put(num, sb.length() - 1);
+                sb.append(num / den);
+                num = num % den * 10;
+            }
+        }
+        
+        return sb.toString();
+    }
+}
