578de3fb545144f9fb3a3c291da244759eebe7a4
Jason51122 <jason51122@users.noreply.github.com>
2015-06-29 22:24:32 -0700

    Update and rename BufferedReader.java to Read N Characters Given Read4 II - Call multiple times.java

---
+++ Design/Read N Characters Given Read4 II - Call multiple times.java
@@ -0,0 +1,48 @@
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+    private char[] buffer = new char[4];
+    private int start = 0;
+    private int size = 0;
+    
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    public int read(char[] buf, int n) {
+        int count = 0;
+        
+        if (n <= 0) {
+            return count;
+        }
+        
+        while (n > 0) {
+            if (size == 0) {
+                size = read4(buffer);
+                
+                if (size == 0) {
+                    return count;
+                }
+            }
+
+            if (size <= n) {
+                System.arraycopy(buffer, start, buf, count, size);
+                count += size;
+                n -= size;
+                start = 0;
+                size = 0;
+            }
+            else {
+                System.arraycopy(buffer, start, buf, count, n);
+                start += n;
+                size -= n;
+                count += n;
+                n = 0;
+            }
+        }
+        
+        return count;
+    }
+}
