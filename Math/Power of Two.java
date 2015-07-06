bd616282c25d052c811cb00f3d2e884875eb2e57
Jason51122 <jason51122@users.noreply.github.com>
2015-07-05 18:10:21 -0700

    Create Power of Two.java

---
+++ Math/Power of Two.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0 || n == Integer.MIN_VALUE) {
+            return false;
+        }
+        
+        return (n & (n - 1)) == 0;
+    }
+}
