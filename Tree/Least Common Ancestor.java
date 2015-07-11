ab63cbb1ea91a7e6ebfe8b234664860ccbde26fc
Jason51122 <jason51122@users.noreply.github.com>
2015-07-10 17:05:15 -0700

    Update Least Common Ancestor.java

--- Tree/Least Common Ancestor.java
+++ Tree/Least Common Ancestor.java
@@ -1,47 +1,29 @@
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-}
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
-    public TreeNode LeastCommonAncestor(TreeNode root, TreeNode x, TreeNode y) {
-        if (x == null || y == null) {
-            return null;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
         }
 
-        boolean[] flag = new boolean[1];
-
-        TreeNode ancestor = helper(root, x, y, flag);
-
-        if (flag[0]) {
-            return ancestor;
-        }
-        else {
-            return null;
-        }
-    }
-
-    private TreeNode helper(TreeNode p, TreeNode x, TreeNode y, boolean[] flag) {
-        if (p == null || p == x || p == y) {
-            return p;
-        }
-
-        TreeNode left = helper(p.left, x, y);
-        TreeNode right = helper(p.right, x, y);
-
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
         if (left == null) {
             return right;
-        } else if (right == null) {
+        }
+        else if (right == null) {
             return left;
-        } else {
-            flag[0] = true;
-            
-            return p;
+        }
+        else {
+            return root;
         }
     }
 }
