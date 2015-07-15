326291f6b5787edc0e3aacd43ac179c758ff6680
Jason51122 <jason51122@users.noreply.github.com>
2015-07-14 22:25:45 -0700

    Update Delete Node in a Linked List.java

--- Linked List/Delete Node in a Linked List.java
+++ Linked List/Delete Node in a Linked List.java
@@ -8,17 +8,7 @@
  */
 public class Solution {
     public void deleteNode(ListNode node) {
-        if (node == null) {
-            return;
-        }
-        else if (node.next == null) {
-            node = null;
-            return;
-        }
-        else {
-            node.val = node.next.val;
-            node.next = node.next.next;
-            return;
-        }
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 }
