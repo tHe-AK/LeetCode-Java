fbced3dfe000589b1c669fd21df29e9bb60aa97a
Jason51122 <jason51122@users.noreply.github.com>
2015-07-29 00:04:33 -0700

    Update Implement strStr().java

--- String/Implement strStr().java
+++ String/Implement strStr().java
@@ -1,23 +1,26 @@
 public class Solution {
-    public String strStr(String haystack, String needle) {
-        if (haystack == null || needle == null) {
-            throw new IllegalArgumentException();
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null || haystack.length() < needle.length()) {
+            return -1;
         }
         
-        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
-            int k;
+        int len1 = haystack.length();
+        int len2 = needle.length();
+        
+        for (int i = 0; i < len1 - len2 + 1; i++) {
+            int j;
             
-            for (k = 0; k < needle.length(); k++) {
-                if (haystack.charAt(i + k) != needle.charAt(k)) {
+            for (j = 0; j < len2; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
                     break;
                 }
             }
             
-            if (k == needle.length()) {
-                return haystack.substring(i, haystack.length());
+            if (j == len2) {
+                return i;
             }
         }
         
-        return null;
+        return -1;
     }
 }
