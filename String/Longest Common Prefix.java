6ccc6413a3950c762b5e998d63d2158d32504920
Jason51122 <jason51122@users.noreply.github.com>
2015-07-29 00:35:56 -0700

    Update Longest Common Prefix.java

--- String/Longest Common Prefix.java
+++ String/Longest Common Prefix.java
@@ -1,16 +1,14 @@
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null) {
-            throw new IllegalArgumentException();
-        }
-        
-        if (strs.length == 0) {
+        if (strs == null || strs.length == 0 || strs[0] == null) {
             return "";
         }
-
-        for (int i = 0; i < strs[0].length(); i++) {
+        
+        int len = strs[0].length();
+        
+        for (int i = 0; i < len; i++) {
             for (int j = 1; j < strs.length; j++) {
-                if (i == strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i)) {
+                if (i == strs[j].length() || strs[0].charAt(i) != strs[j].charAt(i)) {
                     return strs[0].substring(0, i);
                 }
             }
