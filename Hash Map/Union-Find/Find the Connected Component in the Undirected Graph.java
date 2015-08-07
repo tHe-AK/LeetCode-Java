afaf2e79ab5b7b6d88f9b595333e07e73d147c03
Jason51122 <jason51122@users.noreply.github.com>
2015-08-06 21:27:58 -0700

    Create Find the Connected Component in the Undirected Graph.java

---
+++ Hash Map/Union-Find/Find the Connected Component in the Undirected Graph.java
@@ -0,0 +1,93 @@
+/**
+ * Definition for Undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     ArrayList<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+ 
+class UnionFind {
+    HashMap<Integer, Integer> rec;
+    
+    UnionFind() {
+        rec = new HashMap<Integer, Integer>();
+    }
+    
+    void add(int child) {
+        rec.put(child, child);
+    }
+    
+    int find(int child) {
+        if (!rec.containsKey(child)) {
+            throw new RuntimeException();
+        }
+        
+        while (child != rec.get(child)) {
+            child = rec.get(child);
+        }
+            
+        return child;
+    }
+    
+    void union(int child1, int child2) {
+        int parent1 = find(child1);
+        int parent2 = find(child2);
+        
+        if (parent1 != parent2) {
+            rec.put(parent1, parent2);
+        }
+    }
+    
+    List<List<Integer>> print() {
+        HashMap<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        
+        for (Integer child : rec.keySet()) {
+            int parent = find(child);
+            
+            if (map.containsKey(parent)) {
+                map.get(parent).add(child);
+            }
+            else {
+                List<Integer> list = new ArrayList<Integer>();
+                list.add(child);
+                map.put(parent, list);
+            }
+        }
+        
+        for (Integer parent : map.keySet()) {
+            List<Integer> list = map.get(parent);
+            Collections.sort(list);
+            result.add(list);
+        }
+        
+        return result;
+    }
+}
+
+public class Solution {
+    /**
+     * @param nodes a array of Undirected graph node
+     * @return a connected set of a Undirected graph
+     */
+    public List<List<Integer>> connectedSet(ArrayList<UndirectedGraphNode> nodes) {
+        if (nodes == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        UnionFind uf = new UnionFind();
+        
+        for (UndirectedGraphNode node : nodes) {
+            uf.add(node.label);
+        }
+        
+        for (UndirectedGraphNode node : nodes) {
+            for (UndirectedGraphNode neighbor : node.neighbors) {
+                uf.union(node.label, neighbor.label);
+            }
+        }
+        
+        return uf.print();
+    }
+}
