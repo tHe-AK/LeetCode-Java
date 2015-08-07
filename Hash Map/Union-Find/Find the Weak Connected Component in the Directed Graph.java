23b8357ff11914d77f1b5f890f5a8a6982f4e689
Jason51122 <jason51122@users.noreply.github.com>
2015-08-06 22:24:44 -0700

    Create Find the Weak Connected Component in the Directed Graph.java

---
+++ Hash Map/Union-Find/Find the Weak Connected Component in the Directed Graph.java
@@ -0,0 +1,98 @@
+/**
+ * Definition for Directed graph.
+ * class DirectedGraphNode {
+ *     int label;
+ *     ArrayList<DirectedGraphNode> neighbors;
+ *     DirectedGraphNode(int x) { label = x; neighbors = new ArrayList<DirectedGraphNode>(); }
+ * };
+ */
+ 
+class UnionFind {
+    HashMap<Integer, Integer> rec;
+    
+    UnionFind() {
+        rec = new HashMap<Integer, Integer>();
+    }
+    
+    boolean contain(int child) {
+        return rec.containsKey(child);
+    }
+    
+    void add(int child) {
+        if (!contain(child)) {
+            rec.put(child, child);
+        }
+    }
+    
+    int find(int child) {
+        if (!rec.containsKey(child)) {
+            throw new RuntimeException();
+        }
+        
+        while (child != rec.get(child)) {
+            child = rec.get(child);
+        }
+            
+        return child;
+    }
+    
+    void union(int child1, int child2) {
+        int parent1 = find(child1);
+        int parent2 = find(child2);
+        
+        if (parent1 != parent2) {
+            rec.put(parent1, parent2);
+        }
+    }
+    
+    List<List<Integer>> print() {
+        HashMap<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        
+        for (Integer child : rec.keySet()) {
+            int parent = find(child);
+            
+            if (map.containsKey(parent)) {
+                map.get(parent).add(child);
+            }
+            else {
+                List<Integer> list = new ArrayList<Integer>();
+                list.add(child);
+                map.put(parent, list);
+            }
+        }
+        
+        for (Integer parent : map.keySet()) {
+            List<Integer> list = map.get(parent);
+            Collections.sort(list);
+            result.add(list);
+        }
+        
+        return result;
+    }
+}
+
+public class Solution {
+    /**
+     * @param nodes a array of Directed graph node
+     * @return a connected set of a directed graph
+     */
+    public List<List<Integer>> connectedSet2(ArrayList<DirectedGraphNode> nodes) {
+        if (nodes == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        UnionFind uf = new UnionFind();
+        
+        for (DirectedGraphNode node : nodes) {
+            uf.add(node.label);
+            
+            for (DirectedGraphNode neighbor : node.neighbors) {
+                uf.add(neighbor.label);
+                uf.union(node.label, neighbor.label);
+            }
+        }
+        
+        return uf.print();
+    }
+}
