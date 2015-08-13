ae53c1253357bac885c3a27913f116c8efae48bb
Jason51122 <jason51122@users.noreply.github.com>
2015-08-12 20:31:08 -0700

    Update Valid Palindrome.java

--- DP/Palindrome/Valid Palindrome.java
+++ DP/Palindrome/Valid Palindrome.java
@@ -11,7 +11,7 @@ public class Solution {
         int low = 0;
         int high = s.length() - 1;
         
-        while (low <= high) {
+        while (low < high) {
             if (!Character.isLetterOrDigit(s.charAt(low))) {
                 low++;
             }
