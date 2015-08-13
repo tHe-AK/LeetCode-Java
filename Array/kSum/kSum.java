d16a16e36d47248bf9acc3ade246cb4648673459
Jason51122 <jason51122@users.noreply.github.com>
2015-08-12 22:27:49 -0700

    Update kSum.java

--- Array/kSum/kSum.java
+++ Array/kSum/kSum.java
@@ -1,47 +1,35 @@
 public class Solution {
-    public int kSumNum(int[] num, int k, int target) {
-        if (num == null || num.length == 0 || k < 1 || k > num.length) {
-            return 0;
+    /**
+     * @param A: an integer array.
+     * @param k: a positive integer (k <= length(A))
+     * @param target: a integer
+     * @return an integer
+     */
+    public int kSum(int A[], int k, int target) {
+        if (A == null || k < 0 || target < 0) {
+            throw new IllegalArgumentException();
         }
         
-        Arrays.sort(num);
-        int len = num.length;
-        int minK = 0;
-        int maxK = 0;
+        int n = A.length;
+        int[][][] rec = new int[n + 1][k + 1][target + 1];
         
-        for (int i = 0; i < k; i++) {
-            minK += num[i];
-            maxK += num[len - i - 1];
+        for (int i = 0; i <= n; i++) {
+            rec[i][0][0] = 1;
         }
         
-        if (target < minK || target > maxK) {
-            return 0;
-        }
-        
-        int range = maxK - minK + 1;
-        int[][][] rec = new int[k][len][range];
-        
-        for (int i = 0; i < k; i++) {
-            for (int j = i; j < len; j++) {
-                if (j != len - 1 && num[j] == num[j + 1]) {
-                    continue;
-                }
-                
-                if (i == 0) {
-                    rec[i][j][num[j] - minK] = 1;
-                }
-                else {
-                    for (int l = 0; l < j; l++) {
-                        for (int t = 0; t < range; t++) {
-                            if (rec[i - 1][l][t] > 0) {
-                                rec[i][j][t + num[j]] += rec[i - 1][l][t];
-                            }
-                        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i && j <= k; j++) {
+                for (int t = 1; t <= target; t++) {
+                    rec[i][j][t] = 0;
+                    rec[i][j][t] += rec[i - 1][j][t];
+                    
+                    if (t >= A[i - 1]) {
+                        rec[i][j][t] += rec[i - 1][j - 1][t - A[i - 1]];
                     }
                 }
             }
         }
         
-        return rec[k - 1][len - 1][target - minK];
+        return rec[n][k][target];
     }
 }
