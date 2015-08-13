e7029538397e5aeed6c6f4d145d6089aba79f14c
Jason51122 <jason51122@users.noreply.github.com>
2015-08-12 20:53:08 -0700

    Update Partition Array by Odd and Even.java

--- Sort/Partition Array by Odd and Even.java
+++ Sort/Partition Array by Odd and Even.java
@@ -8,6 +8,29 @@ public class Solution {
             throw new IllegalArgumentException();
         }
         
+        int cur = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 1) {
+                int temp = nums[cur];
+                nums[cur] = nums[i];
+                nums[i] = temp;
+                cur++;
+            }
+        }
+    }
+}
+
+public class Solution {
+    /**
+     * @param nums: an array of integers
+     * @return: nothing
+     */
+    public void partitionArray(int[] nums) {
+        if (nums == null) {
+            throw new IllegalArgumentException();
+        }
+        
         int odd = 0;
         int even = nums.length - 1;
 
