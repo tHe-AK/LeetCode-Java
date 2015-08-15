1633421e7fa707b0c51dba1ee778260776d8bdd8
Jason51122 <jason51122@users.noreply.github.com>
2015-08-14 22:57:21 -0700

    Update Longest Increasing Subsequence.java

--- DP/Longest Increasing Subsequence.java
+++ DP/Longest Increasing Subsequence.java
@@ -1,26 +1,34 @@
 public class Solution {
-    public int longestIncreasingSubsequence(int[] num) {
-        if (num == null || num.length == 0) {
+    /**
+     * @param nums: The integer array
+     * @return: The length of LIS (longest increasing subsequence)
+     */
+    public int longestIncreasingSubsequence(int[] nums) {
+        if (nums == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        if (nums.length == 0) {
             return 0;
         }
         
-        int len = num.length;
+        int len = nums.length;
         int[] result = new int[len];
-        int max = 1;
-
+        int max = 0;
+        
         for (int i = 0; i < len; i++) {
             result[i] = 1;
-        }
-        
-        for (int i = 1; i < len; i++) {
+            
             for (int j = 0; j < i; j++) {
-                if (num[j] <= num[i]) {
+                if (nums[j] <= nums[i]) {
                     result[i] = Math.max(result[i], result[j] + 1);
-                    max = Math.max(max, result[i]);
                 }
             }
+            
+            max = Math.max(max, result[i]);
         }
         
         return max;
     }
 }
+
