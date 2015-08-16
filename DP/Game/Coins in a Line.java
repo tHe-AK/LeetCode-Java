d35eedd52771e9b0840f420d7788fc8241732647
Jason51122 <jason51122@users.noreply.github.com>
2015-08-16 16:41:41 -0700

    Update Coins in a Line.java

--- DP/Game/Coins in a Line.java
+++ DP/Game/Coins in a Line.java
@@ -27,3 +27,17 @@ public class Solution {
         return rec[n];
     }
 }
+
+public class Solution {
+    /**
+     * @param n: an integer
+     * @return: a boolean which equals to true if the first player will win
+     */
+    public boolean firstWillWin(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+        
+        return n % 3 != 0;
+    }
+}
