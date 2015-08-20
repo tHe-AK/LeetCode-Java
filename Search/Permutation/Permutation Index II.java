ec0f7413f260a1ca3a7ca9bba2c4922018424ad3
Jason51122 <jason51122@users.noreply.github.com>
2015-08-19 21:26:42 -0700

    Create Permutation Index II.java

---
+++ Search/Permutation/Permutation Index II.java
@@ -0,0 +1,51 @@
+public class Solution {
+    /**
+     * @param A an integer array
+     * @return a long integer
+     */
+    public long permutationIndex(int[] A) {
+        if (A == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        int len = A.length;
+        long result = 0;
+        long factorial = 1;
+        
+        for (int i = len - 2; i >= 0; i--) {
+            int count = 0;
+            HashMap<Integer, Integer> rec = new HashMap<Integer, Integer>();
+            rec.put(A[i], 1);
+            
+            for (int j = i + 1; j < len; j++) {
+                if (rec.containsKey(A[j])) {
+                    rec.put(A[j], rec.get(A[j]) + 1);
+                }
+                else {
+                    rec.put(A[j], 1);
+                }
+                
+                if (A[i] > A[j]) {
+                    count++;
+                }
+            }
+            
+            long duplicate = 1;
+            
+            for (Integer value : rec.values()) {
+                long temp = 1;
+                
+                for (int x = 2; x <= value; x++) {
+                    temp *= x;
+                }
+                
+                duplicate *= temp;
+            }
+            
+            result += count * factorial / duplicate;
+            factorial *= len - i;
+        }
+        
+        return result + 1;
+    }
+}
