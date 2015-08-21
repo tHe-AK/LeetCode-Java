1530560fc56d683cc680e3fb5df4da716f80985d
Jason51122 <jason51122@users.noreply.github.com>
2015-08-20 23:42:59 -0700

    Create Ugly Number II.java

---
+++ Math/Number/Ugly Number II.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public int nthUglyNumber(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        
+        int[] rec = new int[n + 1];
+        rec[1] = 1;
+        int index2 = 1;
+        int index3 = 1;
+        int index5 = 1;
+        int factor2 = 2;
+        int factor3 = 3;
+        int factor5 = 5;
+        
+        for (int i = 2; i <= n; i++){
+            int min = Math.min(Math.min(factor2,factor3),factor5);
+            rec[i] = min;
+            
+            if (min == factor2) {
+                factor2 = 2 * rec[++index2];
+            }
+            
+            if (min == factor3) {
+                factor3 = 3 * rec[++index3];
+            }
+            
+            if (min == factor5) {
+                factor5 = 5 * rec[++index5];
+            }
+        }
+        
+        return rec[n];
+    }
+}
