5f4b8b79da850e4c3eff6e3ec5c3f63ec36ca0d2
Jason51122 <jason51122@users.noreply.github.com>
2015-08-23 19:45:35 -0700

    Update Find Peak Element II.java

--- Binary Search/Find Peak Element II.java
+++ Binary Search/Find Peak Element II.java
@@ -8,32 +8,62 @@ class Solution {
             throw new IllegalArgumentException();
         }
         
-        int low = 0;
-        int high = A.length - 1;
+        int iLow = 0;
+        int iHigh = A.length - 1;
+        int jLow = 0;
+        int jHigh = A[0].length - 1;
+        boolean flag = true;
         List<Integer> result = new ArrayList<Integer>();
         
-        while (low + 1 < high) {
-            int mid = low + (high - low) / 2;
-            int j = 0;
-            
-            for (int i = 1; i < A[0].length; i++) {
-                if (A[mid][i] > A[mid][j]) {
-                    j = i;
+        while (iLow + 1 < iHigh && jLow + 1 < jHigh) {
+            if (flag) {
+                int iMid = iLow + (iHigh - iLow) / 2;
+                int j = jLow;
+                
+                for (int x = jLow + 1; x <= jHigh; x++) {
+                    if (A[iMid][x] > A[iMid][j]) {
+                        j = x;
+                    }
+                }
+                
+                if (A[iMid - 1][j] < A[iMid][j] && A[iMid][j] > A[iMid + 1][j]) {
+                    result.add(iMid);
+                    result.add(j);
+                    
+                    return result;
+                }
+                else if (A[iMid - 1][j] > A[iMid][j]) {
+                    iHigh = iMid;
+                }
+                else {
+                    iLow = iMid;
+                } 
+            }
+            else {
+                int jMid = jLow + (jHigh - jLow) / 2;
+                int i = 0;
+                
+                for (int x = iLow + 1; x <= iHigh; x++) {
+                    if (A[x][jMid] > A[i][jMid]) {
+                        i = x;
+                    }
+                }
+                
+                if (A[i][jMid - 1] < A[i][jMid] && A[i][jMid] > A[i][jMid + 1]) {
+                    result.add(i);
+                    result.add(jMid);
+                    
+                    return result;
+                }
+                else if (A[i][jMid - 1] > A[i][jMid]) {
+                    jHigh = jMid;
+                }
+                else {
+                    jLow = jMid;
                 }
             }
             
-            if (A[mid - 1][j] < A[mid][j] && A[mid][j] > A[mid + 1][j]) {
-                result.add(mid);
-                result.add(j);
-                
-                return result;
-            }
-            else if (A[mid - 1][j] > A[mid][j]) {
-                high = mid;
-            }
-            else {
-                low = mid;
-            }
+            flag = !flag;
         }
         
         throw new IllegalArgumentException();
