6c29ad46913c7c28c319c26949e062fa0fc9a2e0
Jason51122 <jason51122@users.noreply.github.com>
2015-08-30 19:41:28 -0700

    Update Convert Number To Words.java

--- Math/Convert Number To Words.java
+++ Math/Convert Number To Words.java
@@ -1,66 +1,49 @@
-import java.io.*;
-import java.util.*;
-
-class Solution {
-    public String covertNum2Words(int n) {
-        if (n < 0) {
+public class Solution {
+    public String numberToWords(int num) {
+        if (num < 0) {
             throw new IllegalArgumentException();
         }
         
-        if (n == 0) {
-            return "zero";
+        if (num == 0) {
+            return "Zero";
         }
 
         int[] recNum = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40, 50, 60, 70, 80, 90};
-        String[] recStr = new String[] {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+        String[] recStr = new String[] {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
         int[] dividendNum = new int[] {1, 1000, 1000000, 1000000000};
-        String[] dividendStr = new String[] {"", "thousand", "million", "billion"};
-        StringJoiner sj = new StringJoiner(" ");
+        String[] dividendStr = new String[] {"", "Thousand", "Million", "Billion"};
+        StringBuilder sb = new StringBuilder();
         
         for (int level = dividendNum.length - 1; level >= 0; level--) {
-            int val = n / dividendNum[level] % 1000;
+            int val = num / dividendNum[level] % 1000;
             
-            if (level == 0 && n >= 1000 && val > 0 && val < 100) {
-                sj.add("and");
+            if (val > 0) {
+                helper(recNum, recStr, val, level, sb);
+                sb.append(dividendStr[level]);
+                sb.append(" ");
             }
-            
-            helper(recNum, recStr, dividendStr, val, level, sj);
         }
         
-   
-        return sj.toString();
+        return sb.toString().trim();
     }
     
-    private void helper(int[] recNum, String[] recStr, String[] dividendStr, int val, int level, StringJoiner sj) {    
-        if (val == 0) {
-            return;
-        }
-        
+    private void helper(int[] recNum, String[] recStr, int val, int level, StringBuilder sb) {    
         int digit = val / 100;
         
         if (digit > 0) {
-            sj.add(recStr[digit]);
-            sj.add("hundred");
+            sb.append(recStr[digit]);
+            sb.append(" ");
+            sb.append("Hundred ");
         }
         
         val %= 100;
         
-        if (digit > 0 && val > 0) {
-            sj.add("and");
-        }
-        
         for (int i = recNum.length - 1; i > 0 && val > 0; i--) {
             if (recNum[i] <= val) {                  
-                sj.add(recStr[i]);
+                sb.append(recStr[i]);
+                sb.append(" ");
                 val -= recNum[i];
             }
         }
-        
-        sj.add(dividendStr[level]);
-    }
-    
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println(s.covertNum2Words(1234567089));
     }
 }
