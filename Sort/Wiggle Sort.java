171c6e739ee8f6b0d6e76917fc9c0c9b0e0a479b
Jason51122 <jason51122@users.noreply.github.com>
2015-09-12 18:06:47 -0700

    Create Wiggle Sort.java

---
+++ Sort/Wiggle Sort.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public void wiggleSort(int[] nums) {
+        if (nums == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                if (nums[i - 1] < nums[i]) {
+                    swap(nums, i - 1, i);
+                }
+            } else {
+                if (nums[i - 1] > nums[i]) {
+                    swap(nums, i - 1, i);
+                }
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
