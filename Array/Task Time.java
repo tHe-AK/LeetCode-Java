6ce555dee4ab6fded0adf56451ea1cef9cdb93b0
Jason51122 <jason51122@users.noreply.github.com>
2015-09-13 00:28:03 -0700

    Create Task Time.java

---
+++ Array/Task Time.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+	public int taskTime(int[] task, int N){
+		if (task == null || N <= 0) {
+			throw new IllegalArgumentException();
+		}
+		
+	    int count = 0;
+	    Map<Integer, Integer> rec = new HashMap<Integer, Integer>();
+	    
+	    for (int i = 0; i < task.length; i++) {
+	    	int key = task[i];
+	    	
+	    	if (rec.containsKey(key)) {
+	    		int val = rec.get(key);
+	    		
+	    		if (count - val > N) {
+	    			rec.put(key, val + 1);
+	    			count++;
+	    		} else {
+	    			count = val + N + 1;
+	    			rec.put(key, count);
+	    			count++;
+	    		}
+	    	} else {
+	    		rec.put(key, count);
+	    		count++;
+	    	}
+	    }
+	    
+	    return count;
+	}
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(s.task(new int[] {1, 2, 1, 2, 3}, 2));
+	}
+
+}
