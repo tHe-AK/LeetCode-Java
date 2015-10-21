8b150035f41be6bf56974d053ee59d5f12b4762b
Jason51122 <jason51122@users.noreply.github.com>
2015-10-20 22:54:48 -0700

    Create Find the Duplicate Number.java

---
+++ Array/Find the Duplicate Number.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            throw new IllegalArgumentException();
+        }
+        
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        
+        slow = 0;
+        
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
+    }
+}
