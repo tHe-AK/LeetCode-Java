d29f2ecbc8fa4d1b16e4f6fdad15fb62c7b1e196
Jason51122 <jason51122@users.noreply.github.com>
2015-10-21 23:15:01 -0700

    Create Best Meeting Point.java

---
+++ Heap/Median/Best Meeting Point.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public int minTotalDistance(int[][] grid) {
+        if (grid == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        List<Integer> x = new ArrayList<Integer>();
+        List<Integer> y = new ArrayList<Integer>();
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    x.add(i);
+                    y.add(j);
+                }
+            }
+        }
+        
+        return helper(x) + helper(y);
+    }
+    
+    private int helper(List<Integer> list) {
+        Collections.sort(list);
+        
+        int dist = 0;
+        int i = 0;
+        int j = list.size() - 1;
+        
+        while (i < j) {
+            dist += list.get(j) - list.get(i);
+            i++;
+            j--;
+        }
+        
+        return dist;
+    }
+}
