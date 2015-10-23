bdf0fb438d79425aaa889c94ea8a60c7430c15c2
Jason51122 <jason51122@users.noreply.github.com>
2015-10-23 00:12:54 -0700

    Update Sudoku Solver.java

--- DFS/Sudoku Solver.java
+++ DFS/Sudoku Solver.java
@@ -4,56 +4,54 @@ public class Solution {
             throw new IllegalArgumentException();
         }
         
-        int len = 9;
-        boolean[][] row = new boolean[len][len];
-        boolean[][] col = new boolean[len][len];
-        boolean[][] sq = new boolean[len][len];
-        List<Integer> path = new ArrayList<Integer>();
-        
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len; j++) {
-                if (board[i][j] != '.') {
-                    int num = board[i][j] - '1';
-                    row[i][num] = true;
-                    col[j][num] = true;
-                    sq[i / 3 * 3 + j / 3][num] = true;
-                }
-                else {
-                    path.add(i * 9 + j);
-                }
-            }
-        }
-        
-        helper(board, row, col, sq, path, 0);
+        helper(board);
     }
     
-    private boolean helper(char[][] board, boolean[][] row, boolean[][] col, boolean[][] sq, List<Integer> path, int cur) {
-        if (cur == path.size()) {
-            return true;
-        }
-        
-        int val = path.get(cur);
-        int i = val / 9;
-        int j = val % 9;
-        int index = i / 3 * 3 + j / 3;
-        
-        for (int k = 0; k < 9; k++) {
-            if (!row[i][k] && !col[j][k] && !sq[index][k]) {
-                row[i][k] = true;
-                col[j][k] = true;
-                sq[index][k] = true;
-                board[i][j] = (char)('1' + k);
+    private boolean helper(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9.; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (valid(board, i, j, c)) {
+                            board[i][j] = c;
+                            
+                            if (helper(board)) {
+                                return true;
+                            }
+                            
+                            board[i][j] = '.';
+                        }
+                    }
                     
-                if (helper(board, row, col, sq, path, cur + 1)) {
-                    return true;
+                    return false;
                 }
-                    
-                row[i][k] = false;
-                col[j][k] = false;
-                sq[index][k] = false;
             }
         }
         
-        return false;
+        return true;
+    }
+    
+    private boolean valid(char[][] board, int x, int y, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][y] == c) {
+                return false;
+            }
+        }
+
+        for (int j = 0; j < 9; j++) {
+            if (board[x][j] == c) {
+                return false;
+            }
+        }
+
+        for (int i = (x / 3) * 3; i < (x / 3) * 3 + 3; i++) {
+            for (int j = (y / 3) * 3; j < (y / 3) * 3 + 3; j++) {
+                if (board[i][j] == c) {
+                    return false;
+                }
+            }
+        }
+                    
+        return true;
     }
 }
