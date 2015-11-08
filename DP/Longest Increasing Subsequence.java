c9302778ea6206bcc923b43d226a17fa827f0330
Jason51122 <jason51122@users.noreply.github.com>
2015-11-08 14:19:08 -0800

    Update Longest Increasing Subsequence.java

--- DP/Longest Increasing Subsequence.java
+++ DP/Longest Increasing Subsequence.java
@@ -4,6 +4,33 @@ public class Solution {
             throw new IllegalArgumentException();
         }
         
+        int[] rec = new int[nums.length];
+        int len = 0;
+
+        for (int num : nums) {
+            int index = Arrays.binarySearch(rec, 0, len, num);
+            
+            if (index < 0) {
+                index = -(index + 1);
+            }
+            
+            rec[index] = num;
+            
+            if (index == len) {
+                len++;
+            }
+        }
+
+        return len;
+    }
+}
+
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null) {
+            throw new IllegalArgumentException();
+        }
+        
         int len = nums.length;
         int[] result = new int[len];
         int max = 0;
@@ -15,60 +42,6 @@ public class Solution {
                 if (nums[j] < nums[i]) {
                     result[i] = Math.max(result[i], result[j] + 1);
                 }
-            }
-            
-            max = Math.max(max, result[i]);
-        }
-        
-        return max;
-    }
-}
-
-public class Solution {
-    public int lengthOfLIS(int[] nums) {
-        if (nums == null) {
-            throw new IllegalArgumentException();
-        }
-        
-        if (nums.length == 0) {
-            return 0;
-        }
-        
-        int len = nums.length;
-        int[] result = new int[len];
-        int[] min = new int[len];
-        int cur = 0;
-        int max = 1;
-        result[0] = 1;
-        min[0] = nums[0];
-
-        for (int i = 1; i < len; i++) {
-            if (nums[i] < min[0]) {
-                result[i] = 1;
-                min[0] = nums[i];
-            }
-            else if (nums[i] > min[cur]) {
-                cur++;
-                result[i] = cur + 1;
-                min[cur] = nums[i];
-            }
-            else {
-                int low = 0;
-                int high = cur;
-                
-                while (low <= high) {
-                    int mid = low + (high - low) / 2;
-                    
-                    if (min[mid] < nums[i]) {
-                        low = mid + 1;
-                    }
-                    else {
-                        high = mid - 1;
-                    }
-                }
-                
-                result[i] = low + 1;
-                min[low] = nums[i];
             }
             
             max = Math.max(max, result[i]);
