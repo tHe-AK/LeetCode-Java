70ee7bdb8a9a8cf9a3d003f7dceb8011e53acaac
Jason51122 <jason51122@users.noreply.github.com>
2015-11-10 22:22:54 -0800

    Update Peeking Iterator.java

--- Design/Peeking Iterator.java
+++ Design/Peeking Iterator.java
@@ -5,17 +5,17 @@ class PeekingIterator implements Iterator<Integer> {
     private Iterator<Integer> it;
     
     public PeekingIterator(Iterator<Integer> iterator) {
-	    // initialize any member here.
-	    if (iterator == null) {
-	        throw new IllegalArgumentException();
-	    }
-	        
-	    it = iterator;
-	    
-	    if (it.hasNext()) {
-	        next = it.next();
-	    }
-	}
+        // initialize any member here.
+        if (iterator == null) {
+            throw new IllegalArgumentException();
+        }
+            
+        it = iterator;
+        
+        if (it.hasNext()) {
+            next = it.next();
+        }
+    }
 
     // Returns the next element in the iteration without advancing the iterator.
     public Integer peek() {
@@ -24,16 +24,16 @@ class PeekingIterator implements Iterator<Integer> {
 
     // hasNext() and next() should behave the same as in the Iterator interface.
     // Override them if needed.
-	@Override
-	public Integer next() {
-	    Integer cur = next;
-	    next = it.hasNext() ? it.next() : null;
-	    
-	    return cur;
-	}
+    @Override
+    public Integer next() {
+        Integer cur = next;
+        next = it.hasNext() ? it.next() : null;
+        
+        return cur;
+    }
 
     @Override
-	public boolean hasNext() {
-	    return next != null;
-	}
+    public boolean hasNext() {
+        return next != null;
+    }
 }
