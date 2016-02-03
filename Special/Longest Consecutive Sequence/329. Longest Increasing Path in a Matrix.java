1fe01324bb6b1785115f6a8670cb350966ef24c3
Jason51122 <jason51122@users.noreply.github.com>
2016-02-03 13:52:10 -0800

    Update and rename Longest Increasing Continuous Subsequence II.java to 329. Longest Increasing Path in a Matrix.java

---
+++ Special/Longest Consecutive Sequence/329. Longest Increasing Path in a Matrix.java
@@ -0,0 +1,45 @@
+public class Solution {
+    public int longestIncreasingPath(int[][] matrix) {
+        if (matrix == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        int result = 0;
+        
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return result;
+        }
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] record = new int[row][col];
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                result = Math.max(result, helper(matrix, record, i, j));
+            }
+        }
+        
+        return result;
+    }
+    
+    private int helper(int[][] matrix, int[][] record, int i, int j) {
+        if (record[i][j] > 0) {
+            return record[i][j];
+        }
+        
+        int[] deltaX = new int[] {-1, -1, 1, 1};
+        int[] deltaY = new int[] {-1, 1, -1, 1};
+        
+        for (int k = 0; k < deltaX.length; k++) {
+            int x = i + deltaX[k];
+            int y = j + deltaY[k];
+            
+            if (x >= 0 && x < matrix.length && y >= 0 && y < matrix[0].length && matrix[x][y] >= matrix[i][j]) {
+                record[i][j] = Math.max(record[i][j], helper(matrix, record, x, y));
+            }
+        }
+        
+        return ++record[i][j];
+    }
+}
