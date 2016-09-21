aabdfc5d14e75ff7196b2c01cca3816837841bd7
Jason51122 <jason51122@users.noreply.github.com>
2016-09-21 12:23:55 -0700

    Create Binary Watch.java

---
+++ Bit Manipulation/Binary Watch.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public List<String> readBinaryWatch(int num) {
+        if (num < 0 || num > 11) {
+            throw new IllegalArgumentException();
+        }
+        
+        List<String> result = new ArrayList<String>();
+        
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 60; j++) {
+                if (Integer.bitCount((i << 6) + j) == num) {
+                    result.add(String.format("%d:%02d"), i, j);
+                }
+            }
+        }
+        
+        return result;
+    }
+}
