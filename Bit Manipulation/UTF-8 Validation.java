afbea54242d791ac769da63893a311b7dd58baef
Jason51122 <jason51122@users.noreply.github.com>
2016-09-22 21:37:57 -0700

    Create UTF-8 Validation.java

---
+++ Bit Manipulation/UTF-8 Validation.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public boolean validUtf8(int[] data) {
+        if (data == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        int count = 0;
+        
+        for (int num : data) {
+            if (count == 0) {
+                if (num >> 7 == 0b0) {
+                    continue;
+                } else if (num >> 5 == 0b110) {
+                    count = 1;
+                } else if (num >> 4 == 0b1110) {
+                    count = 2;
+                } else if (num >> 3 == 0b11110) {
+                    count = 3;
+                } else {
+                    return false;
+                }
+            } else {
+                if (num >> 6 != 0b10) {
+                    return false;
+                }
+                
+                count--;
+            }
+        }
+        
+        return count == 0;
+    }
+}
