92a7247a8e99a61720fe07632c342e97751423d7
Jason51122 <jason51122@users.noreply.github.com>
2016-09-29 18:11:27 -0700

    Update Decode String.java

--- String/Decode String.java
+++ String/Decode String.java
@@ -4,51 +4,42 @@ public class Solution {
             throw new IllegalArgumentException();
         }
         
+        
+        return helper(s, new int[] {0});
+    }
+    
+    private String helper(String s, int[] cur) {
         StringBuilder result = new StringBuilder();
-        Stack<Integer> counts = new Stack<Integer>();
-        Stack<StringBuilder> strs = new Stack<StringBuilder>();
-        
-        int i = 0;
-        
-        while (i < s.length()) {
-            char c = s.charAt(i);
-            
-            if (c == '[') {
-                strs.push(new StringBuilder());
-                i++;
-            } else if (c == ']') {
-                int count = counts.pop();
-                StringBuilder str = strs.pop();
 
-                for (int j = 0; j < count; j++) {
-                    if (strs.empty()) {
-                        result.append(str);
-                    } else {
-                        strs.peek().append(str);
-                    }
+        while (cur[0] < s.length()) {
+            char c = s.charAt(cur[0]);
+            
+            if (Character.isDigit(c)) {
+                int start = cur[0]++;
+                
+                while (cur[0] < s.length() && Character.isDigit(s.charAt(cur[0]))) {
+                    cur[0]++;
                 }
                 
-                i++;
-            } else if (Character.isDigit(c)) {
-                int start = i++;
+                int count = Integer.parseInt(s.substring(start, cur[0]));
                 
-                while (Character.isDigit(s.charAt(i)) && i < s.length()) {
-                    i++;
+                cur[0]++;
+                String str = helper(s, cur);
+                
+                for (int i = 0; i < count; i++) {
+                    result.append(str);
                 }
                 
-                counts.push(Integer.parseInt(s.substring(start, i)));
+            } else if (c == ']') {
+                cur[0]++;
+                return result.toString();
             } else {
-                if (strs.empty()) {
-                    result.append(c);
-                } else {
-                    strs.peek().append(c);
-                }
-                
-                i++;
+                cur[0]++;
+                result.append(c);
             }
         }
         
-        return result.toString();
+        return result.toString(); 
     }
 }
 
@@ -100,6 +91,60 @@ public class Solution {
             }
             
             i++;
+        }
+        
+        return result.toString();
+    }
+}
+
+public class Solution {
+    public String decodeString(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        StringBuilder result = new StringBuilder();
+        Stack<Integer> counts = new Stack<Integer>();
+        Stack<StringBuilder> strs = new Stack<StringBuilder>();
+        
+        int i = 0;
+        
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            
+            if (c == '[') {
+                strs.push(new StringBuilder());
+                i++;
+            } else if (c == ']') {
+                int count = counts.pop();
+                StringBuilder str = strs.pop();
+
+                for (int j = 0; j < count; j++) {
+                    if (strs.empty()) {
+                        result.append(str);
+                    } else {
+                        strs.peek().append(str);
+                    }
+                }
+                
+                i++;
+            } else if (Character.isDigit(c)) {
+                int start = i++;
+                
+                while (Character.isDigit(s.charAt(i)) && i < s.length()) {
+                    i++;
+                }
+                
+                counts.push(Integer.parseInt(s.substring(start, i)));
+            } else {
+                if (strs.empty()) {
+                    result.append(c);
+                } else {
+                    strs.peek().append(c);
+                }
+                
+                i++;
+            }
         }
         
         return result.toString();
