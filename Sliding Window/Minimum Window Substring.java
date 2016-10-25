9b6e50d0578c94a43d1bd4d69b1b0976832f2176
Jason51122 <jason51122@users.noreply.github.com>
2016-10-25 01:04:17 -0700

    Update Minimum Window Substring.java

--- Sliding Window/Minimum Window Substring.java
+++ Sliding Window/Minimum Window Substring.java
@@ -1,48 +1,34 @@
 public class Solution {
-    public String minWindow(String S, String T) {
-        if (S == null || T == null) {
+    public String minWindow(String s, String t) {
+        if (s == null || t == null) {
             throw new IllegalArgumentException();
         }
         
         int[] rec = new int[256];
-        int[] cur = new int[256];
-
-        for (int i = 0; i < T.length(); i++) {
-            rec[T.charAt(i)]++;
+        
+        for (char c : t.toCharArray()) {
+            rec[c]++;
         }
         
-        int low = 0;
-        int count = 0;
+        int start = 0;
+        int end = 0;
+        int count = t.length();
         int min = Integer.MAX_VALUE;
         String result = "";
         
-        for (int high = 0; high < S.length(); high++) {
-            int letter = S.charAt(high);
-            
-            if (rec[letter] == 0) {
-                continue;
+        while (end < s.length()) {
+            if (rec[s.charAt(end++)]-- > 0) {
+                count--;
             }
-            
-            if (cur[letter] < rec[letter]) {
-                count++;
-            }
-            
-            cur[letter]++;
-            
-            if (count == T.length()) {
-                while (rec[S.charAt(low)] == 0 || cur[S.charAt(low)] > rec[S.charAt(low)]) {
-                    letter = S.charAt(low);
-                    
-                    if (cur[letter] > rec[letter]) {
-                        cur[letter]--;
-                    }
-                    
-                    low++;
+
+            while (count == 0) {
+                if (end - start < min) {
+                    min = end - start;
+                    result = s.substring(start, end);
                 }
                 
-                if (high - low + 1 < min) {
-                    min = high - low + 1;
-                    result = S.substring(low, high + 1);
+                if (rec[s.charAt(start++)]++ >= 0) {
+                    count++;
                 }
             }
         }
