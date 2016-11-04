157b7664f140c4506719c65ef2ebc6e750b6c05d
Jason51122 <jason51122@users.noreply.github.com>
2016-11-03 22:42:48 -0700

    Create Find Right Interval.java

---
+++ Binary Search/Find Right Interval.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public int[] findRightInterval(Interval[] intervals) {
+        if (intervals == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        int len = intervals.length;
+        int[] result = new int[len];
+        TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
+        
+        for (int i = 0; i < len; i++) {
+            map.put(intervals[i].start, i);
+        }
+        
+        for (int i = 0; i < len; i++) {
+            Integer key = map.ceilingKey(intervals[i].end);
+            
+            if (key == null) {
+                result[i] = -1;
+            } else {
+                result[i] = map.get(key);
+            }
+        }
+        
+        return result;
+    }
+}
