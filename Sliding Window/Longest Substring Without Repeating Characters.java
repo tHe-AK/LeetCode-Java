861ecf3b46636e3862bba713a1195b86e75c81e4
Jason51122 <jason51122@users.noreply.github.com>
2016-12-01 21:43:05 +0800

    Update Longest Substring Without Repeating Characters.java

--- Sliding Window/Longest Substring Without Repeating Characters.java
+++ Sliding Window/Longest Substring Without Repeating Characters.java
@@ -1,24 +1,14 @@
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s == null) {
-            throw new IllegalArgumentException();
+        int ans = 0;
+        int[] index = new int[128];
+        
+        for (int j = 0, i = 0; j < s.length(); j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
         }
         
-        int result = 0;
-        int start = -1;
-        int[] rec = new int[256];
-        
-        for (int i = 0; i < 256; i++) {
-            rec[i] = -1;
-        }
-        
-        for (int i = 0; i < s.length(); i++) {
-            int cur = s.charAt(i);
-            start = Math.max(start, rec[cur] + 1);
-            rec[cur] = i;
-            result = Math.max(result, i - start + 1);
-        }
-        
-        return result;
+        return ans;
     }
 }
