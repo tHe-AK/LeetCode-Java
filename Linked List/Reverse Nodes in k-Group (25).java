6cd176b55cf4367015d63a8b807d1fd98dcdfc40
Jason51122 <jason51122@users.noreply.github.com>
2017-01-02 21:32:56 -0800

    Update and rename Reverse Nodes in k-Group.java to Reverse Nodes in k-Group (25).java

---
+++ Linked List/Reverse Nodes in k-Group (25).java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr = head;
+        int count = 0;
+        
+        while (curr != null && count != k) {
+            count++;
+            curr = curr.next;
+        }
+        
+        if (count == k) {
+            ListNode pre = reverseKGroup(curr, k);
+            
+            while (count-- > 0) {
+                ListNode next = head.next;
+                head.next = pre;
+                pre = head;
+                head = next;
+            }
+            
+            head = pre;
+        }
+        
+        return head;
+    }
+}
