2b546200868d5f774ba76a19e10d5e2a1b7e934c
Jason51122 <jason51122@users.noreply.github.com>
2017-01-02 18:06:44 -0800

    Update and rename Remove Nth Node From End of List.java to Remove Nth Node From End of List (19).java

--- Linked List/Remove Nth Node From End of List (19).java
+++ Linked List/Remove Nth Node From End of List (19).java
@@ -3,22 +3,11 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ *     ListNode(int x) { val = x; }
  * }
  */
 public class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException();
-        }
-        
-        if (head == null) {
-            return head;
-        }
-        
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode slow = dummy;
