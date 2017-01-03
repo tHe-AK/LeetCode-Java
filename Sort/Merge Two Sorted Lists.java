86e25e0327ec3e4014b684f24e698058469e4b78
Jason51122 <jason51122@users.noreply.github.com>
2017-01-02 18:33:37 -0800

    Update Merge Two Sorted Lists.java

--- Sort/Merge Two Sorted Lists.java
+++ Sort/Merge Two Sorted Lists.java
@@ -3,10 +3,7 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ *     ListNode(int x) { val = x; }
  * }
  */
 public class Solution {
@@ -19,30 +16,13 @@ public class Solution {
             return l1;
         }
         
-        ListNode dummy = new ListNode(0);
-        ListNode head = dummy;
-        
-        while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                head.next = l1;
-                l1 = l1.next;
-                head = head.next;
-            }
-            else {
-                head.next = l2;
-                l2 = l2.next;
-                head = head.next;
-            }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
         }
-        
-        if (l1 != null) {
-            head.next = l1;
+        else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
         }
-        
-        if (l2 != null) {
-            head.next = l2;
-        }
-        
-        return dummy.next;
     }
 }
