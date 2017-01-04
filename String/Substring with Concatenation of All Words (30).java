3c6e1bfe7deccd06eb4fcc4c4578db6d24e8311b
Jason51122 <jason51122@users.noreply.github.com>
2017-01-04 11:58:06 -0800

    Update and rename Substring with Concatenation of All Words.java to Substring with Concatenation of All Words (30).java

---
+++ String/Substring with Concatenation of All Words (30).java
@@ -0,0 +1,39 @@
+public class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        int len = words[0].length();
+        int count = len * words.length;
+        
+        for (String word : words) {
+            map.put(word, map.containsKey(word) ? map.get(word) + 1 : 1);
+        }
+        
+        for (int i = 0; i < s.length() - count + 1; i++) {
+            Map<String, Integer> copy = new HashMap<>(map);
+
+            for (int j = i; j < i + count; j += len) {
+                String str = s.substring(j, j + len);
+                
+                if (copy.containsKey(str)) {
+                    if (copy.get(str) == 1) {
+                        copy.remove(str);
+                    }
+                    else {
+                        copy.put(str, copy.get(str) - 1);
+                    }
+                    
+                    if (copy.isEmpty()) {
+                        result.add(i);
+                        break;
+                    }
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
