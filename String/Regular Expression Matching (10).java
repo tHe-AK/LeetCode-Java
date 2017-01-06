9efa952f7868007055a981868ae5641e911f3969
Jason51122 <jason51122@users.noreply.github.com>
2017-01-06 08:41:48 -0800

    Update Regular Expression Matching (10).java

--- String/Regular Expression Matching (10).java
+++ String/Regular Expression Matching (10).java
@@ -30,3 +30,37 @@ public class Solution {
         return rec[m][n];
     }
 }
+
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        return dfs(s, 0, p, 0);
+    }
+    
+    private boolean dfs(String s, int i, String p, int j) {
+        if (j == p.length()) {
+            return i == s.length();
+        }
+        else if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+            if (dfs(s, i, p, j + 2)) {
+                return true;
+            }
+            
+            while (i < s.length() && equal(s, i, p, j)) {
+                if (dfs(s, i + 1, p, j + 2)) {
+                    return true;
+                }
+                
+                i++;
+            }
+            
+            return false;
+        }
+        else {
+            return i < s.length() && equal(s, i, p, j) && dfs(s, i + 1, p, j + 1);
+        }
+    }
+    
+    private boolean equal(String s, int i, String p, int j) {
+        return p.charAt(j) == '.' || s.charAt(i) == p.charAt(j);
+    }
+}
