12d97f1c8b68377ae2204e9845c4668da83f6c9a
Jason51122 <jason51122@users.noreply.github.com>
2017-01-08 14:00:58 -0800

    Update Wildcard Matching (44).java

--- String/Wildcard Matching (44).java
+++ String/Wildcard Matching (44).java
@@ -1,5 +1,30 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] rec = new boolean[m + 1][n + 1];
+        rec[0][0] = true;
+        
+        for (int j = 1; j <= n && p.charAt(j - 1) == '*'; j++){
+            rec[0][j] = true;
+        }
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    rec[i][j] = rec[i][j - 1] || rec[i - 1][j];
+                } else if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    rec[i][j] = rec[i - 1][j - 1];
+                }
+            }
+        }
+        
+        return rec[m][n];
+    }
+}
+
+public class Solution {
+    public boolean isMatch(String s, String p) {
         int i = 0;
         int j = 0;
         int index = -1;
