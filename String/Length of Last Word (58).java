1ddad0705a3b438e2928d353271570fc63f9a03c
Jason51122 <jason51122@users.noreply.github.com>
2017-01-09 16:26:18 -0800

    Update and rename Length of Last Word.java to Length of Last Word (58).java

--- String/Length of Last Word (58).java
+++ String/Length of Last Word (58).java
@@ -1,9 +1,5 @@
 public class Solution {
     public int lengthOfLastWord(String s) {
-        if (s == null) {
-            throw new IllegalArgumentException();
-        }
-        
         int result = 0;
         int i = s.length() - 1;
         
