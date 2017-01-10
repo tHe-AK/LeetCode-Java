808086ffad99400ea2aca1f69e7edc40d1b1f7ac
Jason51122 <jason51122@users.noreply.github.com>
2017-01-09 16:50:49 -0800

    Update and rename Spiral Matrix II.java to Spiral Matrix II (59).java

--- Math/Spiral Matrix II (59).java
+++ Math/Spiral Matrix II (59).java
@@ -1,58 +1,51 @@
 public class Solution {
     public int[][] generateMatrix(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException();
+        int[][] matrix = new int[n][n];
+        
+        if (n == 0) {
+            return matrix;
         }
         
-        int[][] rec = new int[n][n];
         int left = 0;
         int right = n - 1;
         int top = 0;
         int bottom = n - 1;
-        int cur = 1;
+        int count = 1;
         
         while (true) {
-            if (left > right) {
-                break;
-            }
-            
             for (int j = left; j <= right; j++) {
-                rec[top][j] = cur;
-                cur++;
+                matrix[top][j] = count++;
             }
-            top++;
             
-            if (top > bottom) {
+            if (++top > bottom) {
                 break;
             }
             
             for (int i = top; i <= bottom; i++) {
-                rec[i][right] = cur;
-                cur++;
+                matrix[i][right] = count++;
             }
-            right--;
             
-            if (left > right) {
+            if (left > --right) {
                 break;
             }
             
             for (int j = right; j >= left; j--) {
-                rec[bottom][j] = cur;
-                cur++;
+                matrix[bottom][j] = count++;
             }
-            bottom--;
             
-            if (top > bottom) {
+            if (top > --bottom) {
                 break;
             }
             
             for (int i = bottom; i >= top; i--) {
-                rec[i][left] = cur;
-                cur++;
+                matrix[i][left] = count++;
             }
-            left++;
+            
+            if (++left > right) {
+                break;
+            }
         }
         
-        return rec;
+        return matrix;
     }
 }
