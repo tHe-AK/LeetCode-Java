8de1eaff407a0ebb0255f6141ec4ab360c16291d
Jason51122 <jason51122@users.noreply.github.com>
2017-01-12 11:31:42 -0800

    Update and rename Unique Paths.java to Unique Paths (62).java

--- DP/Unique Paths (62).java
+++ DP/Unique Paths (62).java
@@ -1,17 +1,12 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
-        if (m <= 0 || n <= 0) {
-            throw new IllegalArgumentException();
-        }
-        
         int[][] rec = new int[m][n];
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (i == 0 || j == 0) {
                     rec[i][j] = 1;
-                }
-                else {
+                } else {
                     rec[i][j] = rec[i][j - 1] + rec[i - 1][j];
                 }
             }
