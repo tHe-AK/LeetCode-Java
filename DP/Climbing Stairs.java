886edfb2e50ea8fd18a4f2d5d88ceb19fd3c285d
Jason51122 <jason51122@users.noreply.github.com>
2017-01-15 15:05:33 -0800

    Update Climbing Stairs.java

--- DP/Climbing Stairs.java
+++ DP/Climbing Stairs.java
@@ -1,23 +1,19 @@
 public class Solution {
     public int climbStairs(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException();
-        }
-        
         if (n <= 2) {
             return n;
         }
         
         int pre1 = 1;
         int pre2 = 2;
-        int cur = 3;
+        int curr = 3;
         
         for (int i = 3; i <= n; i++) {
-            cur = pre1 + pre2;
+            curr = pre1 + pre2;
             pre1 = pre2;
-            pre2 = cur;
+            pre2 = curr;
         }
         
-        return cur;
+        return curr;
     }
 }
