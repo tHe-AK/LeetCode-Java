112a76008cb59203aab88aa26c14a68458b22827
Jason51122 <jason51122@users.noreply.github.com>
2017-01-14 17:09:59 -0800

    Update and rename Plus One.java to Plus One (66).java

---
+++ Math/Plus One (66).java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        
+        for (int i = len - 1; i >= 0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        
+        int[] nums = new int[len + 1];
+        nums[0] = 1;
+        
+        return nums;
+    }
+}
