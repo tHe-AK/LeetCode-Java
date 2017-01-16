929ccacd57312b14592a2d6a0ccb09dd30783bc7
Jason51122 <jason51122@users.noreply.github.com>
2017-01-15 21:12:42 -0800

    Update and rename Edit Distance.java to Edit Distance (72).java

--- DP/Edit Distance (72).java
+++ DP/Edit Distance (72).java
@@ -1,25 +1,18 @@
 public class Solution {
     public int minDistance(String word1, String word2) {
-        if (word1 == null || word2 == null) {
-            throw new IllegalArgumentException();
-        }
-        
         int len1 = word1.length();
         int len2 = word2.length();
         int[][] rec = new int[len1 + 1][len2 + 1];
         
-        for (int i = 0; i < len1 + 1; i++) {
-            for (int j = 0; j < len2 + 1; j++) {
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
                 if (i == 0) {
                     rec[i][j] = j;
-                }
-                else if (j == 0) {
+                } else if (j == 0) {
                     rec[i][j] = i;
-                }
-                else if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                } else if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     rec[i][j] = rec[i - 1][j - 1];
-                }
-                else {
+                } else {
                     rec[i][j] = Math.min(Math.min(rec[i][j - 1], rec[i - 1][j]), rec[i - 1][j - 1]) + 1;
                 }
             }
