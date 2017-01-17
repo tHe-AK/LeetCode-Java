fe7f15084f48bf70af1be7a4ac1b1a0453be5b4d
Jason51122 <jason51122@users.noreply.github.com>
2017-01-16 16:46:30 -0800

    Update Set Matrix Zeroes.java

--- Math/Set Matrix Zeroes.java
+++ Math/Set Matrix Zeroes.java
@@ -1,67 +1,33 @@
 public class Solution {
     public void setZeroes(int[][] matrix) {
-        if (matrix == null) {
-            throw new IllegalArgumentException();
-        }
+        boolean row = false;
+        boolean col = false;
+        int m = matrix.length;
+        int n = matrix[0].length;
         
-        if (matrix.length == 0 || matrix[0].length == 0) {
-            return;
-        }
-        
-        int row = matrix.length;
-        int col = matrix[0].length;
-        
-        boolean rowZero = false;
-        boolean colZero = false;
-        
-        for (int i = 0; i < row; i++) {
-            if (matrix[i][0] == 0) {
-                colZero = true;
-                break;
-            }
-        }
-            
-        for (int j = 0; j < col; j++) {
-            if (matrix[0][j] == 0) {
-                rowZero = true;
-                break;
-            }
-        }
-        
-        for (int i = 1; i < row; i++) {
-            for (int j = 1; j < col; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
+                    if (i == 0) {
+                        row = true;
+                    } else if (j == 0) {
+                        col = true;
+                    } else {
+                        matrix[i][0] = matrix[0][j] = 0;
+                    }
+                }
+            }
+        }
+        
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i > 0 && j > 0 && matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                } else if (i == 0 && row) {
                     matrix[0][j] = 0;
+                } else if (j == 0 && col) {
+                    matrix[i][0] = 0;
                 }
-            }
-        }
-        
-        for (int i = 1; i < row; i++) {
-            if (matrix[i][0] == 0) {
-                for (int j = 1; j < col; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        
-        for (int j = 1; j < col; j++) {
-            if (matrix[0][j] == 0) {
-                for (int i = 1; i < row; i++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        
-        if (rowZero) {
-            for (int j = 0; j < col; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-        
-        if (colZero) {
-            for (int i = 0; i < row; i++) {
-                matrix[i][0] = 0;
             }
         }
     }
