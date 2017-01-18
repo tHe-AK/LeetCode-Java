ec7e8dac49dbc22aaf07092a7aa59c93609e091e
Jason51122 <jason51122@users.noreply.github.com>
2017-01-17 22:13:39 -0800

    Update and rename Largest Rectangle in Histogram.java to Largest Rectangle in Histogram (84).java

---
+++ Stack/Largest Rectangle in Histogram (84).java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int len = heights.length;
+        int max = 0;
+        
+        for (int i = 0; i <= len; i++) {
+            int curr = (i == len) ? -1 : heights[i];
+            
+            while (!stack.isEmpty() && heights[stack.peek()] >= curr) {
+                int heigth = heights[stack.pop()];
+                int width = stack.empty() ? i : i - stack.peek() - 1;
+                max = Math.max(max, heigth * width);
+            }
+            
+            stack.push(i);
+        }
+        
+        return max;
+    }
+}
