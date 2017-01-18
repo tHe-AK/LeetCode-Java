913fb0fe0928578c8be73f7cd9bb2cea4aa8b5c3
Jason51122 <jason51122@users.noreply.github.com>
2017-01-18 09:12:16 -0800

    Update and rename Partition List.java to Partition List (86).java

--- Linked List/Partition List (86).java
+++ Linked List/Partition List (86).java
@@ -3,38 +3,30 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ *     ListNode(int x) { val = x; }
  * }
  */
 public class Solution {
     public ListNode partition(ListNode head, int x) {
-        if (head == null) {
-            return head;
-        }
-        
         ListNode dummy1 = new ListNode(0);
         ListNode dummy2 = new ListNode(0);
-        ListNode cur1 = dummy1;
-        ListNode cur2 = dummy2;
+        ListNode curr1 = dummy1;
+        ListNode curr2 = dummy2;
 
         while (head != null) {
             if (head.val < x) {
-                cur1.next = head;
-                cur1 = head;
-            }
-            else {
-                cur2.next = head;
-                cur2 = head;
+                curr1.next = head;
+                curr1 = head;
+            } else {
+                curr2.next = head;
+                curr2 = head;
             }
             
             head = head.next;
         }
         
-        cur1.next = dummy2.next;
-        cur2.next = null;
+        curr1.next = dummy2.next;
+        curr2.next = null;
         
         return dummy1.next;
     }
