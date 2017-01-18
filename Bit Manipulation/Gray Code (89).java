7a592b27186ce5c36dafa4e082a90db160db3682
Jason51122 <jason51122@users.noreply.github.com>
2017-01-18 11:48:18 -0800

    Update and rename Gray Code.java to Gray Code (89).java

--- Bit Manipulation/Gray Code (89).java
+++ Bit Manipulation/Gray Code (89).java
@@ -1,13 +1,9 @@
 public class Solution {
     public List<Integer> grayCode(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException();
-        }
-        
         List<Integer> result = new ArrayList<Integer>();
         
-        for (int i = 0; i < 1 << n; i++) {
-            result.add(i ^ i >> 1);
+        for (int i = 0; i < (1 << n); i++) {
+            result.add(i ^ (i >> 1));
         }
         
         return result;
