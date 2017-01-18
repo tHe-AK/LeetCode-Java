c00b0115bdd571f823cada04905c6e9986b9814a
Jason51122 <jason51122@users.noreply.github.com>
2017-01-18 09:08:34 -0800

    Update Maximal Rectangle.java

--- DP/Maximal Rectangle.java
+++ DP/Maximal Rectangle.java
@@ -1,55 +1,42 @@
 public class Solution {
     public int maximalRectangle(char[][] matrix) {
-        if (matrix == null) {
-            throw new IllegalArgumentException();
-        }
-        
         int area = 0;
         
         if (matrix.length == 0 || matrix[0].length == 0) {
             return area;
         }
         
-        int row = matrix.length;
-        int col = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
         
-        int[] left = new int[col];
-        int[] right = new int[col];
-        int[] height = new int[col];
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] height = new int[n];
         
-        for (int i = 0; i < col; i++) {
-            left[i] = -1;
-        }
-        
-        for (int i = col - 1; i >= 0; i--) {
-            right[i] = col;
-        }
-        
-        for (int i = 0; i < row; i++) {
-            int start = -1;
+        for (int i = 0; i < m; i++) {
+            int curr = 0;
             
-            for (int j = 0; j < col; j++) {
+            for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == '1') {
                     height[j]++;
-                    left[j] = Math.max(left[j], start + 1);
+                    curr++;
+                    left[j] = left[j] == 0 ? curr : Math.min(left[j], curr);
                 }
                 else {
-                    height[j] = 0;
-                    start = j;
-                    left[j] = -1;
+                    height[j] = left[j] = curr = 0;
                 }
             }
             
-            int end = col;
+            curr = 0;
             
-            for (int j = col - 1; j >= 0; j--) {
+            for (int j = n - 1; j >= 0; j--) {
                 if (matrix[i][j] == '1') {
-                    right[j] = Math.min(right[j], end - 1);
-                    area = Math.max(area, height[j] * (right[j] - left[j] + 1));
+                    curr++;
+                    right[j] = right[j] == 0 ? curr : Math.min(right[j], curr);
+                    area = Math.max(area, height[j] * (left[j] + right[j] - 1));
                 }
                 else {
-                    end = j;
-                    right[j] = col;
+                    right[j] = curr = 0;
                 }
             }
         }
