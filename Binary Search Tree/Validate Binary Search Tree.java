05076537a7bc68286f5da7bd4b771c94007a8a4d
Jason51122 <jason51122@users.noreply.github.com>
2017-01-18 21:48:33 -0800

    Update Validate Binary Search Tree.java

--- Binary Search Tree/Validate Binary Search Tree.java
+++ Binary Search Tree/Validate Binary Search Tree.java
@@ -1,4 +1,31 @@
 /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean isValidBST(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+        
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        
+        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
+}
+
+/**
  * Definition for binary tree
  * public class TreeNode {
  *     int val;
@@ -35,5 +62,34 @@ public class Solution {
         }
         
         return true;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return inorder(root, new TreeNode[1]);
+    }
+    
+    private boolean inorder(TreeNode root, TreeNode[] pre) {
+        if (root == null) {
+            return true;
+        }
+        
+        if (!inorder(root.left, pre) || (pre[0] != null && pre[0].val >= root.val)) {
+            return false;
+        }
+        
+        pre[0] = root;
+        
+        return inorder(root.right, pre);
     }
 }
