3bd016e68567e9b76ff4b302c0e3cdddc6c2eb90
Jason51122 <jason51122@users.noreply.github.com>
2017-01-19 15:08:17 -0800

    Update and rename Construct Binary Tree from Inorder and Postorder Traversal.java to Construct Binary Tree from Inorder and Postorder Traversal (106).java

--- Binary Tree/Construct Binary Tree from Inorder and Postorder Traversal (106).java
+++ Binary Tree/Construct Binary Tree from Inorder and Postorder Traversal (106).java
@@ -1,5 +1,5 @@
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
@@ -9,14 +9,10 @@
  */
 public class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        if (inorder == null || postorder == null) {
-            throw new IllegalArgumentException();
-        }
-        
-        return helper(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+        return buildTree(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
     }
     
-    private TreeNode helper(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd) {
+    private TreeNode buildTree(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd) {
         if (inStart > inEnd || postStart > postEnd) {
             return null;
         }
@@ -29,8 +25,8 @@ public class Solution {
         }
         
         TreeNode root = new TreeNode(postorder[postEnd]);
-        root.left = helper(inorder, inStart, i - 1, postorder, postStart, postStart + i - 1 - inStart);
-        root.right = helper(inorder, i + 1, inEnd, postorder, postStart + i - inStart, postEnd - 1);
+        root.left = buildTree(inorder, inStart, i - 1, postorder, postStart, postStart + i - 1 - inStart);
+        root.right = buildTree(inorder, i + 1, inEnd, postorder, postStart + i - inStart, postEnd - 1);
         
         return root;
     }
