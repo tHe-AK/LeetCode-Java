8d6f2a0e010501f3da836b83aaa130e7d1e40af0
Jason51122 <jason51122@users.noreply.github.com>
2017-01-18 16:29:16 -0800

    Update Reverse Linked List II.java

--- Linked List/Reverse Linked List II.java
+++ Linked List/Reverse Linked List II.java
@@ -20,6 +20,45 @@ public class Solution {
             head = head.next;
         }
         
+        ListNode pre = null;
+        ListNode curr = head.next;
+        
+        for (int i = 0; i <= n - m; i++) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        
+        head.next.next = curr;
+        head.next = pre;
+        
+        return dummy.next;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null) {
+            return head;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        
+        for (int i = 1; i < m; i++) {
+            head = head.next;
+        }
+        
         ListNode pre = head.next;
         ListNode curr = head.next.next;
         
