30363ac3a315968d7d0f6d17dd6a5261c888f633
Jason51122 <jason51122@users.noreply.github.com>
2017-01-18 21:48:54 -0800

    Update Validate Binary Search Tree.java

--- Binary Search Tree/Validate Binary Search Tree.java
+++ Binary Search Tree/Validate Binary Search Tree.java
@@ -26,46 +26,6 @@ public class Solution {
 }
 
 /**
- * Definition for binary tree
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public class Solution {
-    public boolean isValidBST(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        
-        return helper(root, new TreeNode[1]);
-    }
-    
-    private boolean helper(TreeNode root, TreeNode[] pre) {
-        if (root == null) {
-            return true;
-        }
-        
-        if (!helper(root.left, pre)) {
-            return false;
-        }
-        
-        if (pre[0] != null && pre[0].val >= root.val) {
-            return false;
-        }
-        pre[0] = root;
-        
-        if (!helper(root.right, pre)) {
-            return false;
-        }
-        
-        return true;
-    }
-}
-
-/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
