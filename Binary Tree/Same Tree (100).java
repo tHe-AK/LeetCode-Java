0f10495e546b4dc539ebe9afd35de709bbf84945
Jason51122 <jason51122@users.noreply.github.com>
2017-01-18 21:58:52 -0800

    Update and rename Same Tree.java to Same Tree (100).java

--- Binary Tree/Same Tree (100).java
+++ Binary Tree/Same Tree (100).java
@@ -1,5 +1,5 @@
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
@@ -11,11 +11,9 @@ public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
             return true;
-        }
-        else if (p == null || q == null) {
+        } else if (p == null || q == null) {
             return false;
-        }
-        else {
+        } else {
             return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
     }
