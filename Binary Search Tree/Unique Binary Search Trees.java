d02bd87b597fb839d84b9b6cd3d789fd04f1027f
Jason51122 <jason51122@users.noreply.github.com>
2017-01-18 21:19:28 -0800

    Update Unique Binary Search Trees.java

--- Binary Search Tree/Unique Binary Search Trees.java
+++ Binary Search Tree/Unique Binary Search Trees.java
@@ -1,11 +1,7 @@
 public class Solution {
     public int numTrees(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException();
-        }
-        
-        if (n <= 1) {
-            return 1;
+        if (n == 0) {
+            return 0;
         }
         
         int[] rec = new int[n + 1];
