abceb0b8288568a587bd7651fae9a381ae645687
Jason51122 <jason51122@users.noreply.github.com>
2017-01-19 22:42:21 -0800

    Update and rename Minimum Depth of Binary Tree.java to Minimum Depth of Binary Tree (111).java

--- Binary Tree/Minimum Depth of Binary Tree (111).java
+++ Binary Tree/Minimum Depth of Binary Tree (111).java
@@ -1,5 +1,5 @@
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
@@ -13,9 +13,31 @@ public class Solution {
             return 0;
         }
         
-        int len = 1;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        
+        return (left == 0 || right == 0) ? left + right + 1: Math.min(left, right) + 1;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.offer(root);
+        int len = 1;
         
         while (!queue.isEmpty()) {
             int size = queue.size();
