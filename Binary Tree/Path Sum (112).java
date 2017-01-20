b06ccd0e3f0549e5f152206f9f6801377e947cf8
Jason51122 <jason51122@users.noreply.github.com>
2017-01-19 22:47:26 -0800

    Update and rename Path Sum.java to Path Sum (112).java

--- Binary Tree/Path Sum (112).java
+++ Binary Tree/Path Sum (112).java
@@ -1,5 +1,5 @@
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
@@ -11,11 +11,9 @@ public class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) {
             return false;
-        }
-        else if (root.left == null && root.right == null && root.val == sum) {
+        } else if (root.left == null && root.right == null && root.val == sum) {
             return true;
-        }
-        else {
+        } else {
             return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
         }
     }
