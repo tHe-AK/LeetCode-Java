96f080a1efb8531840bc4940a8848768dd1908cd
Jason51122 <jason51122@users.noreply.github.com>
2017-01-20 12:49:46 -0800

    Update Best Time to Buy and Sell Stock III (123).java

--- DP/Stock/Best Time to Buy and Sell Stock III (123).java
+++ DP/Stock/Best Time to Buy and Sell Stock III (123).java
@@ -5,13 +5,13 @@ public class Solution {
         int profit1 = 0;
         int profit2 = 0;
         
-		for (int price : prices) {
-			min1 = Math.min(min1, price);
-			profit1 = Math.max(profit1, price - min1);
-			min2 = Math.min(min2, price - profit1);
-			profit2 = Math.max(profit2, price - min2);
-		}
-		
-		return profit2;
+        for (int price : prices) {
+            min1 = Math.min(min1, price);
+            profit1 = Math.max(profit1, price - min1);
+            min2 = Math.min(min2, price - profit1);
+            profit2 = Math.max(profit2, price - min2);
+        }
+        
+        return profit2;
     }
 }
