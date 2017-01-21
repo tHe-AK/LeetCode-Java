bd44f24793d8e0181a69ad0d8eb2322103a7d279
Jason51122 <jason51122@users.noreply.github.com>
2017-01-21 15:36:31 -0800

    Update Word Ladder (127).java

--- Backtracking/Word Ladder (127).java
+++ Backtracking/Word Ladder (127).java
@@ -1,14 +1,82 @@
 public class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+        
+        if (!wordSet.contains(endWord)) {
+            return 0;
+        }
+        
+        Set<String> set1 = new HashSet<>();
+        Set<String> set2 = new HashSet<>();
+        set1.add(beginWord);
+        set2.add(endWord);
+        wordSet.remove(beginWord);
+        wordSet.remove(endWord);
+        int len = 1;
+
+        while (!set1.isEmpty()) {
+            Set<String> next = new HashSet<>();
+            len++;
+            
+            for (String str : set1) {
+                List<String> neighbors = getNeighbors(str);
+                
+                for (String neighbor : neighbors) {
+                    if (set2.contains(neighbor)) {
+                        return len;
+                    }
+                    
+                    if (wordSet.contains(neighbor)) {
+                        next.add(neighbor);
+                    }
+                }
+            }
+            
+            wordSet.removeAll(next);
+            set1 = next;
+            
+            if (set1.size() > set2.size()) {
+                Set<String> temp = new HashSet<>(set1);
+                set1 = set2;
+                set2 = temp;
+            }
+        }
+        
+        return 0;
+    }
+    
+    private List<String> getNeighbors(String str) {
+        List<String> neighbors = new ArrayList<>();
+        
+        for (int j = 0; j < str.length(); j++) {
+            StringBuilder sb = new StringBuilder(str);
+                    
+            for (char c = 'a'; c <= 'z'; c++) {
+                if (c != str.charAt(j)) {
+                    sb.setCharAt(j, c);
+                    String neighbor = sb.toString();
+                    neighbors.add(neighbor);
+                }
+            }
+        }
+        
+        return neighbors;
+    }
+}
+
+public class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         List<List<String>> result = new ArrayList<>();
 
         return bfs(beginWord, endWord, new HashSet<String>(wordList));
     }
     
-    private int bfs(String beginWord, String endWord, Set<String> wordList) {
+public class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
         Set<String> curr = new HashSet<>();
         curr.add(beginWord);
-        wordList.remove(beginWord);
+        wordSet.remove(beginWord);
         int len = 1;
         
         while (!curr.isEmpty()) {
@@ -19,7 +87,7 @@ public class Solution {
                 List<String> neighbors = getNeighbors(str);
                 
                 for (String neighbor : neighbors) {
-                    if (wordList.contains(neighbor)) {
+                    if (wordSet.contains(neighbor)) {
                         if (neighbor.equals(endWord)) {
                     	    return len;
                     	}
@@ -29,7 +97,7 @@ public class Solution {
                 }
             }
             
-            wordList.removeAll(next);
+            wordSet.removeAll(next);
             curr = next;
         }
         
