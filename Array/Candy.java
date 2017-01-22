142c16f6e45485f2921ffdf4093d6341ab5025de
Jason51122 <jason51122@users.noreply.github.com>
2017-01-21 22:23:51 -0800

    Update Candy.java

--- Array/Candy.java
+++ Array/Candy.java
@@ -1,34 +1,23 @@
 public class Solution {
     public int candy(int[] ratings) {
-        if (ratings == null) {
-            throw new IllegalArgumentException();
-        }
-        
         int len = ratings.length;
         int total = 0;
-        int[] increase = new int[len];
-        int[] decrease = new int[len];
-        
+        int[] rec = new int[len];
+
         for (int i = 0; i < len; i++) {
-            if (i != 0 && ratings[i - 1] < ratings[i]) {
-                increase[i] = increase[i - 1] + 1;
-            }
-            else {
-                increase[i] = 1;
+            if (i > 0 && ratings[i - 1] < ratings[i]) {
+                rec[i] = rec[i - 1] + 1;
+            } else {
+                rec[i] = 1;
             }
         }
         
         for (int i = len - 1; i >= 0; i--) {
-            if (i != len - 1 && ratings[i] > ratings[i + 1]) {
-                decrease[i] = decrease[i + 1] + 1;
+            if (i < len - 1 && ratings[i] > ratings[i + 1]) {
+                rec[i] = Math.max(rec[i], rec[i + 1] + 1);
             }
-            else {
-                decrease[i] = 1;
-            }
-        }
-        
-        for (int i = 0; i < len; i++) {
-            total += Math.max(increase[i], decrease[i]);
+            
+            total += rec[i];
         }
         
         return total;
