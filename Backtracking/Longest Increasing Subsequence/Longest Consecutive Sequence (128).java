92f42e5773a26f5dcf7150806cd9cf4bc48322f8
Jason51122 <jason51122@users.noreply.github.com>
2017-01-21 17:27:22 -0800

    Update Longest Consecutive Sequence (128).java

--- Backtracking/Longest Increasing Subsequence/Longest Consecutive Sequence (128).java
+++ Backtracking/Longest Increasing Subsequence/Longest Consecutive Sequence (128).java
@@ -1,6 +1,40 @@
 public class Solution {
     public int longestConsecutive(int[] nums) {
         int max = 0;
+        Set<Integer> set = new HashSet<>();
+        
+        for (int num : nums) {
+            set.add(num);
+        }
+        
+        for (int num : nums) {
+            if (set.remove(num)) {
+                int val = num;
+                int count = 1;
+                
+                while (set.remove(val - 1)) {
+                    count++;
+                    val--;
+                }
+                
+                val = num;
+                
+                while (set.remove(num + 1)) {
+                    count++;
+                    num++;
+                }
+                
+                max = Math.max(max, count);
+            }
+        }
+        
+        return max;
+    }
+}
+
+public class Solution {
+    public int longestConsecutive(int[] nums) {
+        int max = 0;
         Map<Integer, Integer> rec = new HashMap<>();
         
         for (int num : nums) {
