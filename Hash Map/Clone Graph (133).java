7f6e9911566d67497bb468501479471b6527f675
Jason51122 <jason51122@users.noreply.github.com>
2017-01-21 21:53:36 -0800

    Update Clone Graph (133).java

--- Hash Map/Clone Graph (133).java
+++ Hash Map/Clone Graph (133).java
@@ -8,6 +8,41 @@
  */
 public class Solution {
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+        
+        return cloneGraph(node, map);
+    }
+    
+    private UndirectedGraphNode cloneGraph(UndirectedGraphNode node, Map<UndirectedGraphNode, UndirectedGraphNode> map) {
+        if (node == null) {
+            return null;
+        }
+        
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+        
+        UndirectedGraphNode clone = new UndirectedGraphNode(node.label);
+        map.put(node, clone);
+        
+        for (UndirectedGraphNode neighbor : node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor, map));
+        }
+        
+        return clone;
+    }
+}
+
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
         if (node == null) {
             return null;
         }
