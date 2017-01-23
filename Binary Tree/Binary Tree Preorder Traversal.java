e50d18f4ef2e7258e465e9288c836ee851d4e06a
Jason51122 <jason51122@users.noreply.github.com>
2017-01-22 21:18:32 -0800

    Update Binary Tree Preorder Traversal.java

--- Binary Tree/Binary Tree Preorder Traversal.java
+++ Binary Tree/Binary Tree Preorder Traversal.java
@@ -1,5 +1,5 @@
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
@@ -9,28 +9,56 @@
  */
 public class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<Integer>();
+        List<Integer> result = new ArrayList<>();
         
         if (root == null) {
             return result;
         }
         
-        Stack<TreeNode> stack = new Stack<TreeNode>();
+        Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
         
         while (!stack.empty()) {
-            TreeNode cur = stack.pop();
-            result.add(cur.val);
+            TreeNode peek = stack.pop();
+            result.add(peek.val);
             
-            if (cur.right != null) {
-                stack.push(cur.right);
+            if (peek.right != null) {
+                stack.push(peek.right);
             }
             
-            if (cur.left != null) {
-                stack.push(cur.left);
+            if (peek.left != null) {
+                stack.push(peek.left);
             }
         }
         
         return result;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorder(root, result);
+        
+        return result;
+    }
+    
+    private void preorder(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        
+        result.add(root.val);
+        preorder(root.left, result);
+        preorder(root.right, result);
     }
 }
