6c7309b83ef9fdc3acbaa6fc61c3264e8e8dc0cb
Jason51122 <jason51122@users.noreply.github.com>
2017-01-22 22:05:59 -0800

    Update Binary Tree Postorder Traversal.java

--- Binary Tree/Binary Tree Postorder Traversal.java
+++ Binary Tree/Binary Tree Postorder Traversal.java
@@ -1,5 +1,5 @@
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
@@ -9,32 +9,48 @@
  */
 public class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<Integer>();
+        List<Integer> result = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
         
-        if (root == null) {
-            return result;
-        }
-        
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        TreeNode cur = root;
-        TreeNode pre = null;
-        
-        while (cur != null || !stack.empty()) {
-            if (cur != null) {
-                stack.add(cur);
-                cur = cur.left;
-            }
-            else if (stack.peek().right != null && stack.peek().right != pre) {
-                cur = stack.peek().right;
-            }
-            else {
+        while (root != null || !stack.empty()) {
+            if (root != null) {
+                result.add(0, root.val);
+                stack.add(root);
+                root = root.right;
+            } else {
                 TreeNode peek = stack.pop();
-                result.add(peek.val);
-                pre = peek;
-                cur = null;
+                root = peek.left;
             }
         }
         
         return result;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        postorder(root, result);
+        
+        return result;
+    }
+    
+    private void postorder(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        
+        postorder(root.left, result);
+        postorder(root.right, result);
+        result.add(root.val);
     }
 }
