8c02c02d9bf2977efd40997c65aa17fbbf2ab2e5
Jason51122 <jason51122@users.noreply.github.com>
2017-01-22 20:29:53 -0800

    Update and rename Word Break.java to Word Break (139).java

---
+++ DP/Word Break (139).java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet(wordDict);
+        int len = s.length();
+        boolean[] dp = new boolean[len + 1];
+        dp[0] = true;
+        
+        for (int i = 1; i <= len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[len];
+    }
+}
