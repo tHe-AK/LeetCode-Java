5b764512a09bb04d00cfb20aaf62a7159cb9afc8
Jason51122 <jason51122@users.noreply.github.com>
2017-01-22 20:49:59 -0800

    Update and rename Linked List Cycle.java to Linked List Cycle (141).java

--- Linked List/Linked List Cycle (141).java
+++ Linked List/Linked List Cycle (141).java
@@ -11,6 +11,35 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            } else {
+                set.add(head);
+            }
+            
+            head = head.next;
+        }
+        
+        return false;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
         if (head == null) {
             return false;
         }
