a0a41b07b745fa695716d25ee79e61bb82bcdbf3
Jason51122 <jason51122@users.noreply.github.com>
2017-01-22 20:51:45 -0800

    Update Word Break (139).java

--- DP/Word Break (139).java
+++ DP/Word Break (139).java
@@ -1,5 +1,5 @@
 public class Solution {
-    public List<String> wordBreak(String s, List<String> wordDict) {
+    public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> set = new HashSet(wordDict);
         int len = s.length();
         boolean[] dp = new boolean[len + 1];
@@ -14,27 +14,6 @@ public class Solution {
             }
         }
         
-        List<String> result = new ArrayList<>();
-        
-        if (dp[len]) {
-            dfs(s, set, 0, "", result);
-        }
-        
-        return result;
-    }
-    
-    private void dfs(String s, Set<String> wordDict, int start, String curr, List<String> result) {
-        if (start == s.length()) {
-            result.add(curr);
-            return;
-        }
-        
-        for (int i = start + 1; i <= s.length(); i++) {
-            String word = s.substring(start, i);
-            
-            if (wordDict.contains(word)) {
-                dfs(s, wordDict, i, curr + (curr.length() > 0 ? " " : "") + word, result);
-            }
-        }
+        return dp[len];
     }
 }
