6659fd689c8712d769799ac75e9fd7c392af0425
Jason51122 <jason51122@users.noreply.github.com>
2017-01-22 20:54:52 -0800

    Update and rename Linked List Cycle II.java to Linked List Cycle II (142).java

--- Linked List/Linked List Cycle II (142).java
+++ Linked List/Linked List Cycle II (142).java
@@ -11,25 +11,54 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        
+        while (head != null) {
+            if (set.contains(head)) {
+                return head;
+            } else {
+                set.add(head);
+            }
+            
+            head = head.next;
+        }
+        
+        return null;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
         if (head == null) {
             return head;
         }
         
         ListNode slow = head;
         ListNode fast = head;
-        boolean flag = false;
+        boolean isFound = false;
         
         while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
             
             if (slow == fast) {
-                flag = true;
+                isFound = true;
                 break;
             }
         }
         
-        if (!flag) {
+        if (!isFound) {
             return null;
         }
         
