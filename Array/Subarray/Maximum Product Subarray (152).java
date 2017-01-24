c4cd89c80108b747f02446a2fe504c1ed4a53243
Jason51122 <jason51122@users.noreply.github.com>
2017-01-24 14:09:48 -0800

    Update and rename Special/Subarray/Maximum Product Subarray.java to Array/Subarray/Maximum Product Subarray (152).java

---
+++ Array/Subarray/Maximum Product Subarray (152).java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int maxProduct(int[] nums) {
+        int max = nums[0];
+        int min = nums[0];
+        int result = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+            
+            max = Math.max(max * nums[i], nums[i]);
+            min = Math.min(min * nums[i], nums[i]);
+            
+            result = Math.max(result, max);
+        }
+        
+        return result;
+    }
+}
