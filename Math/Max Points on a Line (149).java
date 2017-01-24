7dcafefdc7a7a05fab0a2662b7dba9cb8ca4899c
Jason51122 <jason51122@users.noreply.github.com>
2017-01-23 23:20:10 -0800

    Update Max Points on a Line (149).java

--- Math/Max Points on a Line (149).java
+++ Math/Max Points on a Line (149).java
@@ -12,6 +12,66 @@ public class Solution {
         int max = 0;
         
         for (int i = 0; i < points.length; i++) {
+            Map<Integer, Map<Integer, Integer>> rec = new HashMap<>();
+            int duplicate = 1;
+            int curr = 0;
+            
+            for (int j = i + 1; j < points.length; j++) {
+                int diffX = points[j].x - points[i].x;
+                int diffY = points[j].y - points[i].y;
+                
+                if (diffX == 0 && diffY == 0) {
+                    duplicate++;
+                } else {
+                    int gcd = GCD(diffX, diffY);
+                    int x = diffX / gcd;
+                    int y = diffY / gcd;
+                    
+                    if (rec.containsKey(x)) {
+                        if (rec.get(x).containsKey(y)) {
+                            rec.get(x).put(y, rec.get(x).get(y) + 1);
+                        } else {
+                            rec.get(x).put(y, 1);
+                        }
+                    } else {
+                        Map<Integer, Integer> val = new HashMap<>();
+                        val.put(y, 1);
+                        rec.put(x, val);
+                    }
+                    
+                    curr = Math.max(curr, rec.get(x).get(y));
+                }
+            }
+            
+            max = Math.max(max, curr + duplicate);
+        }
+        
+        return max;
+    }
+    
+    private int GCD(int x, int y) {
+        if (y == 0) {
+            return x;
+        } else {
+            return GCD(y, x % y);
+        }
+    }
+}
+
+/**
+ * Definition for a point.
+ * class Point {
+ *     int x;
+ *     int y;
+ *     Point() { x = 0; y = 0; }
+ *     Point(int a, int b) { x = a; y = b; }
+ * }
+ */
+public class Solution {
+    public int maxPoints(Point[] points) {
+        int max = 0;
+        
+        for (int i = 0; i < points.length; i++) {
             Map<Double, Integer> rec = new HashMap<>();
             int duplicate = 1;
             int curr = 0;
