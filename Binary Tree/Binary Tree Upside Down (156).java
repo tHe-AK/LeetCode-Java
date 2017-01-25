bca8f02e20eae8e1e38e0dde55512164331fea1d
Jason51122 <jason51122@users.noreply.github.com>
2017-01-24 16:58:32 -0800

    Update Binary Tree Upside Down (156).java

--- Binary Tree/Binary Tree Upside Down (156).java
+++ Binary Tree/Binary Tree Upside Down (156).java
@@ -22,3 +22,31 @@ public class Solution {
         return newRoot;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        TreeNode next = null;
+        TreeNode temp = null;
+        TreeNode pre = null;
+        
+        while(root != null) {
+            next = root.left;
+            root.left = temp;
+            temp = root.right;
+            root.right = pre;
+            pre = root;
+            root = next;
+        }
+        
+        return pre;
+    }
+}
