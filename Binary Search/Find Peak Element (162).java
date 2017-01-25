d717658ccf9bff665137e8037758ce157c9bca2c
Jason51122 <jason51122@users.noreply.github.com>
2017-01-24 21:49:24 -0800

    Update and rename Find Peak Element.java to Find Peak Element (162).java

---
+++ Binary Search/Find Peak Element (162).java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        
+        while (low + 1 < high) {
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid - 1] > nums[mid]) {
+                high = mid;
+            } else {
+                low = mid;
+            }
+        }
+        
+        return -1;
+    }
+}
