e4ae13f5a524bef6b1ee73b5db6384529be3c982
Jason51122 <jason51122@users.noreply.github.com>
2017-01-24 19:41:31 -0800

    Update Maximum Subarray (53).java

--- Array/Subarray/Maximum Subarray (53).java
+++ Array/Subarray/Maximum Subarray (53).java
@@ -1,15 +1,13 @@
 public class Solution {
     public int maxSubArray(int[] nums) {
-        int len = nums.length;
-        int[] rec = new int[len];
-        rec[0] = nums[0];
         int max = nums[0];
+        int result = nums[0];
         
         for (int i = 1; i < len; i++) {
-            rec[i] = Math.max(rec[i - 1] + nums[i], nums[i]);
-            max = Math.max(max, rec[i]);
+            max = Math.max(max + rec[i], rec[i]);
+            result = Math.max(result, max);
         }
         
-        return max;
+        return result;
     }
 }
