839a4d87c36493b30d2f3cedeec38e959e79733a
Jason51122 <jason51122@users.noreply.github.com>
2017-01-25 23:03:24 -0800

    Update Excel Sheet Column Number (171).java

--- Math/Excel Sheet Column Number (171).java
+++ Math/Excel Sheet Column Number (171).java
@@ -1,5 +1,12 @@
 public class Solution {
     public int titleToNumber(String s) {
+        int len = s.length();
+        return len == 0 ? 0 : 26 * titleToNumber(s.substring(0, len - 1)) + s.charAt(len - 1) - 'A' + 1;
+    }
+}
+
+public class Solution {
+    public int titleToNumber(String s) {
         int result = 0;
         
         for (int i = 0; i < s.length(); i++) {
