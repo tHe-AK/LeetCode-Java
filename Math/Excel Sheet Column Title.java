51d88d72ba5b96b6ef5e88535f5756f325dd5ce9
Jason51122 <jason51122@users.noreply.github.com>
2017-01-25 22:19:50 -0800

    Update Excel Sheet Column Title.java

--- Math/Excel Sheet Column Title.java
+++ Math/Excel Sheet Column Title.java
@@ -1,24 +1,12 @@
 public class Solution {
     public String convertToTitle(int n) {
-        if (n < 1) {
-            throw new IllegalArgumentException();
-        }
-        
-        String result = "";
-        
+        StringBuilder sb = new StringBuilder();
+
         while (n > 0) {
-            int remaining = n % 26;
-            
-            if (remaining == 0) {
-                result = 'Z' + result;
-                n = n / 26 - 1;
-            }
-            else {
-                result = (char)('A' + remaining - 1) + result;
-                n /= 26;
-            }
+            sb.insert(0, (char) ('A' + --n % 26));
+            n /= 26;
         }
-        
-        return result;
+
+        return sb.toString();
     }
 }
