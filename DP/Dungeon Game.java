60e7051c59f29432feb3f8a98a38cce4bde1ba41
Jason51122 <jason51122@users.noreply.github.com>
2017-01-26 11:02:49 -0800

    Update Dungeon Game.java

--- DP/Dungeon Game.java
+++ DP/Dungeon Game.java
@@ -1,26 +1,19 @@
 public class Solution {
     public int calculateMinimumHP(int[][] dungeon) {
-        if (dungeon == null || dungeon.length == 0 || dungeon[0].length == 0) {
-            throw new IllegalArgumentException();
-        }
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+        int[][] rec = new int[m][n];
         
-        int row = dungeon.length;
-        int col = dungeon[0].length;
-        int[][] rec = new int[row][col];
-        
-        for (int i = row - 1; i >= 0; i--) {
-            for (int j = col - 1; j >= 0; j--) {
-                if (i == row - 1 && j == col - 1) {
-                    rec[i][j] = Math.max(1 - dungeon[i][j], 1);
-                }
-                else if (i == row - 1) {
-                    rec[i][j] = Math.max(rec[i][j + 1] - dungeon[i][j], 1);
-                }
-                else if (j == col - 1) {
-                    rec[i][j] = Math.max(rec[i + 1][j] - dungeon[i][j], 1);
-                }
-                else {
-                    rec[i][j] = Math.min(Math.max(rec[i][j + 1] - dungeon[i][j], 1), Math.max(rec[i + 1][j] - dungeon[i][j], 1));
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i == m - 1 && j == n - 1) {
+                    rec[i][j] = Math.max(1, 1 - dungeon[i][j]);
+                } else if (i == m - 1) {
+                    rec[i][j] = Math.max(1, rec[i][j + 1] - dungeon[i][j]);
+                } else if (j == n - 1) {
+                    rec[i][j] = Math.max(1, rec[i + 1][j] - dungeon[i][j]);
+                } else {
+                    rec[i][j] = Math.min(Math.max(1, rec[i][j + 1] - dungeon[i][j]), Math.max(1, rec[i + 1][j] - dungeon[i][j]));
                 }
             }
         }
