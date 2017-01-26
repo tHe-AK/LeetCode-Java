d7e4a3ec8dbe2d5a8629b5f0532d8b3b0dbbc1a5
Jason51122 <jason51122@users.noreply.github.com>
2017-01-25 22:20:52 -0800

    Update and rename Excel Sheet Column Title.java to Excel Sheet Column Title (168).java

--- Math/Excel Sheet Column Title (168).java
+++ Math/Excel Sheet Column Title (168).java
@@ -1,5 +1,11 @@
 public class Solution {
     public String convertToTitle(int n) {
+        return n == 0 ? "" : convertToTitle(--n / 26) + (char) ('A' + (n % 26));
+    }
+}
+
+public class Solution {
+    public String convertToTitle(int n) {
         StringBuilder sb = new StringBuilder();
 
         while (n > 0) {
