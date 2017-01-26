3a3ee8fb6b76b8f616b3a4169ccfa48c4e1280c5
Jason51122 <jason51122@users.noreply.github.com>
2017-01-26 15:24:07 -0800

    Update Repeated DNA Sequences.java

--- Bit Manipulation/Repeated DNA Sequences.java
+++ Bit Manipulation/Repeated DNA Sequences.java
@@ -1,33 +1,23 @@
 public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> result = new ArrayList<String>();
-
-        if (s == null || s.length() < 10) {
-            return result;
-        }
-
-        HashSet<Integer> rec = new HashSet<Integer>();
-        HashSet<Integer> add = new HashSet<Integer>();
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        List<String> result = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> duplicate = new HashSet<>();
+        Map<Character, Integer> map = new HashMap<>();
         map.put('A', 0);
         map.put('C', 1);
         map.put('G', 2);
         map.put('T', 3);
 
-        int cur = 0;
+        int num = 0;
+        
         for (int i = 0; i < s.length(); i++) {
-            cur <<= 2;
-            cur += map.get(s.charAt(i));
-            cur &= (1 << 20) - 1;
+            num <<= 2;
+            num += map.get(s.charAt(i));
+            num &= 0xFFFFF;
 
-            if (i >= 9) {
-                if (!rec.contains(cur)) {
-                    rec.add(cur);
-                } else if (!add.contains(cur)) {
-                    add.add(cur);
-                    result.add(s.substring(i - 9, i + 1));
-                } else {
-                }
+            if (i >= 9 && !set.add(num) && duplicate.add(num)) {
+                result.add(s.substring(i - 9, i + 1));
             }
         }
 
