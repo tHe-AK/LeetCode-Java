3e7050228923326c82aca8be842963432f56ba6e
Jason51122 <jason51122@users.noreply.github.com>
2017-01-27 11:22:26 -0800

    Update Best Time to Buy and Sell Stock IV (188).java

--- DP/Stock/Best Time to Buy and Sell Stock IV (188).java
+++ DP/Stock/Best Time to Buy and Sell Stock IV (188).java
@@ -1,35 +1,26 @@
 public class Solution {
     public int maxProfit(int k, int[] prices) {
-        if (prices == null) {
-            throw new IllegalArgumentException();
-        }
-        
-        if (prices.length <= 1) {
-            return 0;
-        }
-        
         int len = prices.length;
         
         if (k >= len - 1) {
-            return helper(prices);
+            return getMaxProfit(prices);
         }
         
-        int[][] local = new int[len][k + 1];
-        int[][] global = new int[len][k + 1];
-         
-        for (int i = 1; i < len; i++) {
-            int diff = prices[i] - prices[i - 1];
-        
-            for (int j = 1; j <= k; j++) {
-                local[i][j] = Math.max(global[i - 1][j - 1] + Math.max(diff, 0), local[i - 1][j] + diff);
-                global[i][j] = Math.max(global[i - 1][j], local[i][j]);
+        int[] min = new int[k + 1];
+        Arrays.fill(min, Integer.MAX_VALUE);
+        int[] profit = new int[k + 1];
+
+        for (int price : prices) {
+            for (int i = 1; i <= k; i++) {
+                min[i] = Math.min(min[i], price - profit[i - 1]);
+                profit[i] = Math.max(profit[i], price - min[i]);
             }
         }
         
-        return global[len - 1][k];
+        return profit[k];
     }
     
-    private int helper(int[] prices) {
+    private int getMaxProfit(int[] prices) {
         int sum = 0;
         
         for (int i = 1; i < prices.length; i++) {
