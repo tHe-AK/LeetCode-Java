b26f4e3f2e26d7d1e6c9d15182cfa7826dad5ebc
Jason51122 <jason51122@users.noreply.github.com>
2017-01-27 14:48:26 -0800

    Update House Robber.java

--- DP/House Robber.java
+++ DP/House Robber.java
@@ -1,22 +1,19 @@
 public class Solution {
     public int rob(int[] nums) {
-        if (nums == null) {
-            throw new IllegalArgumentException();
-        }
-        
-        if (nums.length == 0) {
+        int len = nums.length;
+
+        if (len == 0) {
             return 0;
         }
         
-        int size = nums.length;
-        int[] rec = new int[size + 1];
+        int[] rec = new int[len + 1];
         rec[0] = 0;
         rec[1] = nums[0];
         
-        for (int i = 2; i <= size; i++) {
+        for (int i = 2; i <= len; i++) {
             rec[i] = Math.max(rec[i - 2] + nums[i - 1], rec[i - 1]);
         }
         
-        return rec[size];
+        return rec[len];
     }
 }
