50f74c8380fd727eb6abef6f64982d724dc07249
Jason Wu <jason51122@hotmail.com>
2017-01-27 16:13:27 -0800

    Update

--- Binary Tree/Binary Tree Right Side View (199).java
+++ Binary Tree/Binary Tree Right Side View (199).java
@@ -9,6 +9,38 @@
  */
 public class Solution {
     public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        
+        preorder(root, 0, result);
+        
+        return result;
+    }
+    
+    private void preorder(TreeNode root, int level, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        
+        if (level == result.size()) {
+            result.add(root.val);
+        }
+        
+        preorder(root.right, level + 1, result);
+        preorder(root.left, level + 1, result);
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<Integer>();
         
         if (root == null) {
