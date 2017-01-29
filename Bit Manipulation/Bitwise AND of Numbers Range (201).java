6174b35135f6afa0f9a39f2b3b29b6e98d6f9a77
Jason51122 <jason51122@users.noreply.github.com>
2017-01-28 18:35:54 -0800

    Update and rename Bitwise AND of Numbers Range.java to Bitwise AND of Numbers Range (201).java

--- Bit Manipulation/Bitwise AND of Numbers Range (201).java
+++ Bit Manipulation/Bitwise AND of Numbers Range (201).java
@@ -1,13 +1,9 @@
 public class Solution {
     public int rangeBitwiseAnd(int m, int n) {
-        if (m > n) {
-            throw new IllegalArgumentException();
-        }
-        
         while (m < n) {
             n &= n - 1;
         }
         
-        return m & n;
+        return n;
     }
 }
