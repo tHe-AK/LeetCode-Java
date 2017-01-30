372088bc6af92b1195f0915bade3786910348ecb
Jason51122 <jason51122@users.noreply.github.com>
2017-01-29 21:51:41 -0800

    Update Word Search II (212).java

--- Binary Tree/Trie/Word Search II (212).java
+++ Binary Tree/Trie/Word Search II (212).java
@@ -1,138 +1,139 @@
 class TrieNode {
-    char c;
-    HashMap<Character, TrieNode> children = new HashMap<Character, TrieNode>();
-    boolean isLeaf;
-        
-    // Initialize your data structure here.
+    // R links to node children
+    private TrieNode[] links;
+
+    private final int R = 26;
+
+    private boolean isEnd;
+
     public TrieNode() {
+        links = new TrieNode[R];
+    }
+
+    public boolean containsKey(char ch) {
+        return links[ch - 'a'] != null;
     }
     
-    public TrieNode(char c) {
-        this.c = c;
+    public TrieNode get(char ch) {
+        return links[ch - 'a'];
+    }
+    
+    public void put(char ch, TrieNode node) {
+        links[ch - 'a'] = node;
+    }
+    
+    public void setEnd() {
+        isEnd = true;
+    }
+    
+    public boolean isEnd() {
+        return isEnd;
     }
 }
 
 class Trie {
     private TrieNode root;
-
+    
+    /** Initialize your data structure here. */
     public Trie() {
         root = new TrieNode();
     }
-
-    // Inserts a word into the trie.
+    
+    /** Inserts a word into the trie. */
     public void insert(String word) {
-        TrieNode cur = root;
+        TrieNode node = root;
         
         for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
+            char currentChar = word.charAt(i);
             
-            if (cur.children.containsKey(c)) {
-                cur = cur.children.get(c);
-            }
-            else {
-                TrieNode next = new TrieNode(c);
-                cur.children.put(c, next);
-                cur = next;
+            if (!node.containsKey(currentChar)) {
+                node.put(currentChar, new TrieNode());
             }
             
-            if (i == word.length() - 1) {
-                cur.isLeaf = true;
-            }
+            node = node.get(currentChar);
         }
-    }
-
-    // Returns if the word is in the trie.
-    public boolean search(String word) {
-        TrieNode node = searchNode(word);
         
-        if (node != null && node.isLeaf) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    // Returns if there is any word in the trie
-    // that starts with the given prefix.
-    public boolean startsWith(String prefix) {
-        TrieNode node = searchNode(prefix);
-        
-        if (node != null) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        node.setEnd();
     }
     
-    public TrieNode searchNode(String word) {
-        TrieNode cur = root;
+    /** search a prefix or whole key in trie and returns the node where search ends. */
+    private TrieNode searchPrefix(String word) {
+        TrieNode node = root;
         
         for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            
-            if (!cur.children.containsKey(c)) {
-                return null;
-            }
-            
-            cur = cur.children.get(c);
+           char curLetter = word.charAt(i);
+           
+           if (node.containsKey(curLetter)) {
+               node = node.get(curLetter);
+           } else {
+               return null;
+           }
         }
         
-        return cur;
+        return node;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        
+        return node != null && node.isEnd();
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        
+        return node != null;
     }
 }
 
 public class Solution {
     public List<String> findWords(char[][] board, String[] words) {
-        List<String> result = new ArrayList<String>();
-        
-        if (board == null || board.length == 0 || board[0].length == 0 || words == null || words.length == 0) {
-            return result;
-        }
-        
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int[][] delta = new int[][] { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
         Trie trie = new Trie();
+        List<String> result = new ArrayList<>();
         
         for (String word : words) {
             trie.insert(word);
         }
         
-        int row = board.length;
-        int col = board[0].length;
-        boolean[][] visited = new boolean[row][col];
-        
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                helper(i, j, board, visited, "", trie, result);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dfs(board, i, j, visited, "", delta, trie, result);
             }
         }
         
         return result;
     }
     
-    private void helper(int i, int j, char[][] board, boolean[][] visited, String cur, Trie trie, List<String> result) {
+    private void dfs(char[][] board, int i, int j, boolean[][] visited, String word, int[][] delta, Trie trie, List<String> result) {
         if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j]) {
             return;
         }
         
-        cur += board[i][j];
+        word += board[i][j];
         
-        if (!trie.startsWith(cur)) {
+        if (!trie.startsWith(word)) {
             return;
         }
         
-        if (trie.search(cur)) {
-            trie.searchNode(cur).isLeaf = false;
-            result.add(cur);
+        if (trie.search(word) && !result.contains(word)) {
+            result.add(word);
         }
         
         visited[i][j] = true;
-        
-        helper(i - 1, j, board, visited, cur, trie, result);
-        helper(i + 1, j, board, visited, cur, trie, result);
-        helper(i, j - 1, board, visited, cur, trie, result);
-        helper(i, j + 1, board, visited, cur, trie, result);
-        
+            
+        for (int k = 0; k < delta.length; k++) {
+            int x = i + delta[k][0];
+            int y = j + delta[k][1];
+                
+            dfs(board, x, y, visited, word, delta, trie, result);
+        }
+            
         visited[i][j] = false;
     }
 }
