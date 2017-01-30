15cc38e667a0cf89c8d6c574446825bc8df36af7
Jason51122 <jason51122@users.noreply.github.com>
2017-01-29 17:01:46 -0800

    Update Implement Trie (Prefix Tree) (208).java

--- Binrary Tree/Trie/Implement Trie (Prefix Tree) (208).java
+++ Binrary Tree/Trie/Implement Trie (Prefix Tree) (208).java
@@ -1,5 +1,4 @@
 class TrieNode {
-
     // R links to node children
     private TrieNode[] links;
 
@@ -14,15 +13,19 @@ class TrieNode {
     public boolean containsKey(char ch) {
         return links[ch -'a'] != null;
     }
+    
     public TrieNode get(char ch) {
         return links[ch -'a'];
     }
+    
     public void put(char ch, TrieNode node) {
         links[ch -'a'] = node;
     }
+    
     public void setEnd() {
         isEnd = true;
     }
+    
     public boolean isEnd() {
         return isEnd;
     }
