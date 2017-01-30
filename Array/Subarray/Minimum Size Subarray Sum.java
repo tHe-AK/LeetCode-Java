d715165b70df556b6b6c511088372be707a2ab0a
Jason51122 <jason51122@users.noreply.github.com>
2017-01-29 17:15:52 -0800

    Update Minimum Size Subarray Sum.java

--- Array/Subarray/Minimum Size Subarray Sum.java
+++ Array/Subarray/Minimum Size Subarray Sum.java
@@ -1,9 +1,5 @@
 public class Solution {
     public int minSubArrayLen(int s, int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        
         int start = 0;
         int sum = 0;
         int min = Integer.MAX_VALUE;
@@ -11,13 +7,12 @@ public class Solution {
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
             
-            while (start <= i && sum >= s) {
+            while (sum >= s) {
                 min = Math.min(min, i - start + 1);
-                sum -= nums[start];
-                start++;
+                sum -= nums[start++];
             }
         }
         
-        return min != Integer.MAX_VALUE ? min : 0;
+        return min == Integer.MAX_VALUE ? 0 : min;
     }
 }
