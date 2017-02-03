b2c6443244b1c0f22122390b494086d20b5f54ab
Jason51122 <jason51122@users.noreply.github.com>
2017-02-02 16:21:00 -0800

    Update Basic Calculator (224).java

--- Stack/Expression/Basic Calculator (224).java
+++ Stack/Expression/Basic Calculator (224).java
@@ -3,32 +3,32 @@ public class Solution {
         int len = s.length();
         int sign = 1;
         int result = 0;
-    	Stack<Integer> stack = new Stack<>();
-    	
-    	for (int i = 0; i < len; i++) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < len; i++) {
             if (Character.isDigit(s.charAt(i))) {
-    			int sum = s.charAt(i) - '0';
-    			
-    			while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
-    				sum = sum * 10 + s.charAt(i + 1) - '0';
-    				i++;
-    			}
-    			
-    			result += sum * sign;
-    		} else if (s.charAt(i) == '+') {
-    			sign = 1;
-    		} else if (s.charAt(i) == '-') {
-    			sign = -1;
-    		} else if (s.charAt(i) == '(') {
-    			stack.push(result);
-    			stack.push(sign);
-    			result = 0;
-    			sign = 1;
-    		} else if (s.charAt(i) == ')') {
-    			result = result * stack.pop() + stack.pop();
-    		}
-    	}
-    	
-    	return result;
+                int sum = s.charAt(i) - '0';
+                
+                while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
+                    sum = sum * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+                
+                result += sum * sign;
+            } else if (s.charAt(i) == '+') {
+                sign = 1;
+            } else if (s.charAt(i) == '-') {
+                sign = -1;
+            } else if (s.charAt(i) == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (s.charAt(i) == ')') {
+                result = result * stack.pop() + stack.pop();
+            }
+        }
+        
+        return result;
     }
 }
