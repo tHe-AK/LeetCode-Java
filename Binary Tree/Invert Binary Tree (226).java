c0103e30954bfae17e736ed81ecf4b726bc59889
Jason51122 <jason51122@users.noreply.github.com>
2017-02-02 16:49:15 -0800

    Update Invert Binary Tree (226).java

--- Binary Tree/Invert Binary Tree (226).java
+++ Binary Tree/Invert Binary Tree (226).java
@@ -21,3 +21,40 @@ public class Solution {
         return root;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            TreeNode peek = queue.poll();
+            TreeNode temp = peek.left;
+            peek.left = peek.right;
+            peek.right = temp;
+            
+            if (peek.left != null) {
+                queue.add(peek.left);
+            }
+            
+            if (peek.right != null) {
+                queue.add(peek.right);
+            }
+        }
+        
+        return root;
+    }
+}
