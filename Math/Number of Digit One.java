15bcfefafa83a75d0fa1d2a667c58646917b0091
Jason51122 <jason51122@users.noreply.github.com>
2017-02-03 14:11:04 -0800

    Update Number of Digit One.java

--- Math/Number of Digit One.java
+++ Math/Number of Digit One.java
@@ -1,26 +1,18 @@
 public class Solution {
     public int countDigitOne(int n) {
         int count = 0;
-        
-        if (n < 0) {
-            return count;
-        }
-        
         long val = 1;
         
         while (n / val > 0) {
             long high = n / (val * 10);
-            long cur = (n / val) % 10;
+            long curr = (n / val) % 10;
             long low = n % val;
+            count += high * val;
             
-            if (cur == 1) {
-                count += high * val + low + 1;
-            }
-            else if (cur < 1) {
-                count += high * val;
-            }
-            else {
-                count += (high + 1) * val;
+            if (curr == 1) {
+                count += low + 1;
+            } else if (curr > 1) {
+                count += val;
             }
             
             val *= 10;
