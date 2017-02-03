680c3634b2f10aad721a125c712c2e0992eb940c
Jason51122 <jason51122@users.noreply.github.com>
2017-02-02 16:19:44 -0800

    Update Basic Calculator.java

--- Stack/Expression/Basic Calculator.java
+++ Stack/Expression/Basic Calculator.java
@@ -1,61 +1,34 @@
 public class Solution {
     public int calculate(String s) {
-        if (s == null) {
-            throw new IllegalArgumentException();
-        }
-        
-        Stack<Integer> num = new Stack<Integer>();
-        Stack<Character> opr = new Stack<Character>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char cur = s.charAt(i);
-            
-            if (Character.isDigit(cur)) {
-                int val = s.charAt(i) - '0';
-                
-                while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
-                    val = val * 10 + s.charAt(i + 1) - '0';
-                    i++;
-                }
-                
-                num.push(val);
-            }
-            else if (cur == '(') {
-                opr.push(cur);
-            }
-            else if (cur == ')') {
-                while (opr.peek() != '(') {
-                    num.push(helper(num.pop(), num.pop(), opr.pop()));
-                }
-                
-                opr.pop();
-            }
-            else if (cur == '+' || cur == '-') {
-                while (!opr.empty() && opr.peek() != '(') {
-                    num.push(helper(num.pop(), num.pop(), opr.pop()));
-                }
-                
-                opr.push(cur);
-            }
-            else {
-            }
-        }
-        
-        while (!opr.empty()) {
-            num.push(helper(num.pop(), num.pop(), opr.pop()));
-        }
-                
-        return num.peek();
-    }
-    
-    private int helper(int b, int a, char sign) {
-        switch (sign) {
-            case '+':
-                return a + b;
-            case '-':
-                return a - b;
-            default:
-                throw new RuntimeException();
-        }
+        int len = s.length();
+        int sign = 1;
+        int result = 0;
+    	Stack<Integer> stack = new Stack<>();
+    	
+    	for (int i = 0; i < len; i++) {
+    		if (Character.isDigit(s.charAt(i))) {
+    			int sum = s.charAt(i) - '0';
+    			
+    			while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
+    				sum = sum * 10 + s.charAt(i + 1) - '0';
+    				i++;
+    			}
+    			
+    			result += sum * sign;
+    		} else if (s.charAt(i) == '+') {
+    			sign = 1;
+    		} else if (s.charAt(i) == '-') {
+    			sign = -1;
+    		} else if (s.charAt(i) == '(') {
+    			stack.push(result);
+    			stack.push(sign);
+    			result = 0;
+    			sign = 1;
+    		} else if (s.charAt(i) == ')') {
+    			result = result * stack.pop() + stack.pop();
+    		}
+    	}
+    	
+    	return result;
     }
 }
