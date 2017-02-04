7b1ae9b252d96432741a181f33d53fef281e7e18
Jason51122 <jason51122@users.noreply.github.com>
2017-02-03 16:51:02 -0800

    Update and rename Lowest Common Ancestor of a Binary Tree.java to Lowest Common Ancestor of a Binary Tree (236).java

--- Binary Tree/Lowest Common Ancestor of a Binary Tree (236).java
+++ Binary Tree/Lowest Common Ancestor of a Binary Tree (236).java
@@ -12,17 +12,15 @@ public class Solution {
         if (root == null || root == p || root == q) {
             return root;
         }
-
+        
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
         
         if (left == null) {
             return right;
-        }
-        else if (right == null) {
+        } else if (right == null) {
             return left;
-        }
-        else {
+        } else {
             return root;
         }
     }
