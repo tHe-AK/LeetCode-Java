01f3ebf68ba782dae1410a0dd74e87d628b83732
Jason51122 <jason51122@users.noreply.github.com>
2017-02-04 12:55:55 -0800

    Update and rename Search a 2D Matrix II.java to Search a 2D Matrix II (240).java

--- Binary Search/Search a 2D Matrix II (240).java
+++ Binary Search/Search a 2D Matrix II (240).java
@@ -1,20 +1,20 @@
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
             return false;
         }
         
-        int i = matrix.length - 1;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int i = row - 1;
         int j = 0;
         
-        while (i >= 0 && j < matrix[0].length) {
+        while (i >= 0 && j < col) {
             if (matrix[i][j] == target) {
                 return true;
-            }
-            else if (matrix[i][j] < target) {
+            } else if (matrix[i][j] < target) {
                 j++;
-            }
-            else {
+            } else {
                 i--;
             }
         }
