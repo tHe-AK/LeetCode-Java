2acfc5e3828e81150de2a6b45cdbfff82822c9bf
Jason51122 <jason51122@users.noreply.github.com>
2017-02-04 20:06:59 -0800

    Update Strobogrammatic Number II (247).java

--- Math/Number/Strobogrammatic Number II (247).java
+++ Math/Number/Strobogrammatic Number II (247).java
@@ -6,26 +6,24 @@ public class Solution {
     private List<String> findStrobogrammatic(int n, int m) {
         if (m == 0) {
             return Arrays.asList("");
-        }
-        
-        if (m == 1) {
+        } else if (m == 1) {
             return Arrays.asList("0", "1", "8");
-        }
-        
-        List<String> result = new ArrayList<String>();
-        List<String> list = findStrobogrammatic(n, m - 2);
-        
-        for (String str : list) {
-            if (n != m) {
-                result.add("0" + str + "0");
+        } else {
+            List<String> result = new ArrayList<String>();
+            List<String> list = findStrobogrammatic(n, m - 2);
+
+            for (String str : list) {
+                if (n != m) {
+                    result.add("0" + str + "0");
+                }
+
+                result.add("1" + str + "1");
+                result.add("8" + str + "8");
+                result.add("6" + str + "9");
+                result.add("9" + str + "6");
             }
-            
-            result.add("1" + str + "1");
-            result.add("8" + str + "8");
-            result.add("6" + str + "9");
-            result.add("9" + str + "6");
+
+            return result;
         }
-        
-        return result;
     }
 }
