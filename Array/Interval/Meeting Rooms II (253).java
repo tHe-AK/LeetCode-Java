5c16a37a184950a62e50a50e81f69ecb285684a3
Jason51122 <jason51122@users.noreply.github.com>
2017-02-04 23:09:06 -0800

    Update Meeting Rooms II (253).java

--- Array/Interval/Meeting Rooms II (253).java
+++ Array/Interval/Meeting Rooms II (253).java
@@ -49,3 +49,39 @@ public class Solution {
         return result;
     }
 }
+
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+
+public class Solution {
+    public int minMeetingRooms(Interval[] intervals) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int count = 0;
+        int result = 0;
+
+        for (Interval i : intervals) {
+            updateMap(map, i.start, 1);
+            updateMap(map, i.end, -1);
+        }
+        
+        for (int key : map.keySet()) {
+            count += map.get(key);
+            result = Math.max(result, count);
+        }
+        
+        return result;
+    }
+    
+    private void updateMap(TreeMap<Integer, Integer> map, int key, int diff) {
+        int val = map.containsKey(key) ? map.get(key) : 0;
+        map.put(key, val + diff);
+    }
+}
+
