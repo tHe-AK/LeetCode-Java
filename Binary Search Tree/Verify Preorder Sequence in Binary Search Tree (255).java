c4e924231cc9ad0c4f49861b8474a943eb4369ce
Jason51122 <jason51122@users.noreply.github.com>
2017-02-05 12:19:51 -0800

    Update and rename Stack/Verify Preorder Sequence in Binary Search Tree.java to Binary Search Tree/Verify Preorder Sequence in Binary Search Tree (255).java

---
+++ Binary Search Tree/Verify Preorder Sequence in Binary Search Tree (255).java
@@ -0,0 +1,41 @@
+public class Solution {
+    public boolean verifyPreorder(int[] preorder) {
+        long min = Long.MIN_VALUE;
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int val : preorder) {
+            if (val <= min) {
+                return false;
+            }
+            
+            while (!stack.empty() && val > stack.peek()) {
+                min = stack.pop();
+            }
+            
+            stack.push(val);
+        }
+        
+        return true;
+    }
+}
+
+public class Solution {
+    public boolean verifyPreorder(int[] preorder) {
+        long min = Long.MIN_VALUE;
+        int curr = -1;
+        
+        for (int val : preorder) {
+            if (val <= min) {
+                return false;
+            }
+            
+            while (curr >= 0 && val > preorder[curr]) {
+                min = preorder[curr--];
+            }
+            
+            preorder[++curr] = val;
+        }
+        
+        return true;
+    }
+}
