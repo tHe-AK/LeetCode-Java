da95d94e6be8ab60c18a6e9306b40e3bc8c6f6c4
Jason51122 <jason51122@users.noreply.github.com>
2017-02-05 15:43:02 -0800

    Update Palindrome Permutation II (267).java

--- String/Palindrome/Palindrome Permutation II (267).java
+++ String/Palindrome/Palindrome Permutation II (267).java
@@ -2,7 +2,7 @@ public class Solution {
     public List<String> generatePalindromes(String s) {
         int[] rec = new int[256];
         int count = 0;
-        String str = "";
+        StringBuilder sb = new StringBuilder();
         List<String> result = new ArrayList<>();
         
         for (int i = 0; i < s.length(); i++) {
@@ -13,8 +13,8 @@ public class Solution {
             count += rec[i];
             
             if (rec[i] % 2 == 1) {
-                if (str.length() == 0) {
-                    str += (char) i;
+                if (sb.length() == 0) {
+                    sb.append((char) i);
                     rec[i]--;
                 } else {
                     return result;
@@ -22,24 +22,28 @@ public class Solution {
             }
         }
         
-        dfs(rec, count, str, result);
+        dfs(rec, count, sb, result);
         
         return result;
     }
     
-    private void dfs(int[] rec, int count, String str, List<String> result) {
-        if (str.length() == count) {
-            result.add(str);
+    private void dfs(int[] rec, int count, StringBuilder sb, List<String> result) {
+        if (sb.length() == count) {
+            result.add(sb.toString());
             return;
         }
         
         for (int i = 0; i < rec.length; i++) {
             if (rec[i] > 0) {
                 char ch = (char) i;
+                sb.insert(0, ch);
+                sb.append(ch);
                 rec[i] -= 2;
                 
-                dfs(rec, count, ch + str + ch, result);
+                dfs(rec, count, sb, result);
                 
+                sb.deleteCharAt(0);
+                sb.deleteCharAt(sb.length() - 1);
                 rec[i] += 2;
             }
         }
