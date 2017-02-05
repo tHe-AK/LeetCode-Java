2e9baa06f0fe5f224f62c53f5f82af907d626bd0
Jason51122 <jason51122@users.noreply.github.com>
2017-02-04 21:37:37 -0800

    Update Flatten 2D Vector.java

--- Design/Flatten 2D Vector.java
+++ Design/Flatten 2D Vector.java
@@ -1,29 +1,61 @@
-public class Vector2D {
-    private Iterator<List<Integer>> x;
-    private Iterator<Integer> y;
-
+public class Vector2D implements Iterator<Integer> {
+    private int idx1;
+    private int idx2;
+    private List<List<Integer>> vec2d;
+    
     public Vector2D(List<List<Integer>> vec2d) {
-        if (vec2d == null) {
-            throw new IllegalArgumentException();
-        }
-        
-        x = vec2d.iterator();
+        idx1 = 0;
+        idx2 = 0;
+        this.vec2d = vec2d;
     }
 
-    public int next() {
-        if (hasNext()) {
-            return y.next();
-        } else {
-            throw new RuntimeException();
-        }
+    @Override
+    public Integer next() {
+        return vec2d.get(idx1).get(idx2++);
     }
 
+    @Override
     public boolean hasNext() {
-        while ((y == null || !y.hasNext()) && x.hasNext()) {
-            y = x.next().iterator();
+        while (idx1 < vec2d.size()) {
+            if (idx2 < vec2d.get(idx1).size()) {
+                return true;
+            } else {
+                idx1++;
+                idx2 = 0;
+            }
         }
         
-        return y != null && y.hasNext();
+        return false;
+    }
+}
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D i = new Vector2D(vec2d);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+
+public class Vector2D implements Iterator<Integer> {
+    private Iterator<List<Integer>> itr1;
+    private Iterator<Integer> itr2;
+    
+    public Vector2D(List<List<Integer>> vec2d) {
+        itr1 = vec2d.iterator();
+        itr2 = null;
+    }
+
+    @Override
+    public Integer next() {
+        return itr2.next();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while ((itr2 == null || !itr2.hasNext()) && itr1.hasNext()) {
+            itr2 = itr1.next().iterator();
+        }
+        
+        return itr2 != null && itr2.hasNext();
     }
 }
 
