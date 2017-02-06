929be70cd22b2f1e339783d2fe7f47132644b208
Jason51122 <jason51122@users.noreply.github.com>
2017-02-05 16:02:20 -0800

    Update Add Digits.java

--- Math/Add Digits.java
+++ Math/Add Digits.java
@@ -1,9 +1,5 @@
 public class Solution {
     public int addDigits(int num) {
-        if (num < 0) {
-            throw new IllegalArgumentException();
-        }
-        
         return (num - 1) % 9 + 1;
     }
 }
