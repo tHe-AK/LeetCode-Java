5af4b95e7f5f1b43a0c3da3fd5ffb6b802c174b5
Jason51122 <jason51122@users.noreply.github.com>
2017-02-06 20:27:09 -0800

    Update Closest Binary Search Tree Value.java

--- Binary Search Tree/Closest Binary Search Tree Value.java
+++ Binary Search Tree/Closest Binary Search Tree Value.java
@@ -9,19 +9,51 @@
  */
 public class Solution {
     public int closestValue(TreeNode root, double target) {
-        if (root == null) {
-            throw new IllegalArgumentException();
+        int result = root.val;
+        
+        while (root != null) {
+            if (root.val == target) {
+                return root.val;
+            }
+            
+            if (Math.abs(root.val - target) < Math.abs(result - target)) {
+                result = root.val;
+            }
+            
+            root = root.val > target ? root.left: root.right;
         }
         
+        return result;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int closestValue(TreeNode root, double target) {
         int rootVal = root.val;
-        TreeNode child = target < rootVal ? root.left : root.right;
+        TreeNode child = null;
+        
+        if (rootVal == target) {
+            return rootVal;
+        } else if (rootVal > target) {
+            child = root.left;
+        } else {
+            child = root.right;
+        }
         
         if (child == null) {
             return rootVal;
+        } else {
+            int childVal = closestValue(child, target);
+            return Math.abs(rootVal - target) < Math.abs(childVal - target) ? rootVal : childVal;
         }
-        
-        int childVal = closestValue(child, target);
-        
-        return Math.abs(rootVal - target) < Math.abs(childVal - target) ? rootVal : childVal;
     }
 }
