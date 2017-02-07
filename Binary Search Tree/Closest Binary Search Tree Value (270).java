56f2512b52f42253c1631b49ea50b784cb035e91
Jason51122 <jason51122@users.noreply.github.com>
2017-02-06 21:48:51 -0800

    Update Closest Binary Search Tree Value (270).java

--- Binary Search Tree/Closest Binary Search Tree Value (270).java
+++ Binary Search Tree/Closest Binary Search Tree Value (270).java
@@ -9,6 +9,41 @@
  */
 public class Solution {
     public int closestValue(TreeNode root, double target) {
+        Integer[] result = new Integer[1];
+
+        inorder(root, target, result);
+        
+        return result[0];
+    }
+    
+    private void inorder(TreeNode root, double target, Integer[] result) {
+        if (root == null) {
+            return;
+        }
+        
+        inorder(root.left, target, result);
+        
+        if (result[0] == null || Math.abs(root.val - target) < Math.abs(result[0] - target)) {
+            result[0] = root.val;
+        } else {
+            return;
+        }
+        
+        inorder(root.right, target, result);
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int closestValue(TreeNode root, double target) {
         int result = root.val;
         
         while (root != null) {
