c55479735f9f24285f6f7d4a069594336b80cc4d
Jason51122 <jason51122@users.noreply.github.com>
2017-02-07 15:11:47 -0800

    Update Basic Calculator II (227).java

--- Stack/Expression/Basic Calculator II (227).java
+++ Stack/Expression/Basic Calculator II (227).java
@@ -2,6 +2,46 @@ public class Solution {
     public int calculate(String s) {
         int len = s.length();
         char sign = '+';
+        int prev = 0;
+        int result = 0;
+
+        for (int i = 0; i < len; i++) {
+            char ch = s.charAt(i);
+            
+            if (Character.isDigit(ch)) {
+                int curr = s.charAt(i) - '0';
+                
+                while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
+                    curr = curr * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+                
+                if (sign == '+') {
+                    result += curr;
+                    prev = curr;
+                } else if (sign == '-') {
+                    result -= curr;
+                    prev = -curr;
+                } else if (sign == '*') {
+                    result = result - prev + prev * curr;
+                    prev = prev * curr;
+                } else {
+                    result = result - prev + prev / curr;
+                    prev = prev / curr;
+                }
+            } else if (ch != ' ') {
+                sign = ch;
+            }
+        }
+        
+        return result;
+    }
+}
+
+public class Solution {
+    public int calculate(String s) {
+        int len = s.length();
+        char sign = '+';
         int result = 0;
         Stack<Integer> stack = new Stack<>();
         
