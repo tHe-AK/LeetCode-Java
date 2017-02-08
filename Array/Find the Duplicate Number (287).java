4f0c86332ed8ba4a2a2d92d6a36b88c86d856c23
Jason51122 <jason51122@users.noreply.github.com>
2017-02-07 17:16:07 -0800

    Update and rename Find the Duplicate Number.java to Find the Duplicate Number (287).java

--- Array/Find the Duplicate Number (287).java
+++ Array/Find the Duplicate Number (287).java
@@ -1,28 +1,17 @@
 public class Solution {
     public int findDuplicate(int[] nums) {
-        if (nums == null || nums.length < 2) {
-            throw new IllegalArgumentException();
-        }
-        
         int slow = 0;
         int fast = 0;
-        int len = nums.length;
-        boolean cycle = false;
-        
-        while (slow >= 0 && slow < len && fast >= 0 && fast < len) {
+
+        while (true) {
             slow = nums[slow];
             fast = nums[nums[fast]];
             
             if (slow == fast) {
-                cycle = true;
                 break;
             }
         }
-        
-        if (!cycle) {
-            throw new IllegalArgumentException();
-        }
-        
+
         slow = 0;
         
         while (slow != fast) {
