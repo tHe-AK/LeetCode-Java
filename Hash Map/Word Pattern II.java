d24995fb044b83bde91785cfa62143f4fcc392a4
Jason51122 <jason51122@users.noreply.github.com>
2017-02-07 22:37:41 -0800

    Update Word Pattern II.java

--- Hash Map/Word Pattern II.java
+++ Hash Map/Word Pattern II.java
@@ -1,16 +1,12 @@
 public class Solution {
     public boolean wordPatternMatch(String pattern, String str) {
-        if (pattern == null || str == null) {
-            throw new IllegalArgumentException();
-        }
-        
         Map<Character, String> map = new HashMap<Character, String>();
         Set<String> set = new HashSet<String>();
         
-        return helper(pattern, 0, str, 0, map, set);
+        return dfs(pattern, str, 0, 0, map, set);
     }
     
-    private boolean helper(String pattern, int i, String str, int j, Map<Character, String> map, Set<String> set) {
+    private boolean dfs(String pattern, String str, int i, int j, Map<Character, String> map, Set<String> set) {
         if (i == pattern.length() && j == str.length()) {
             return true;
         }
@@ -19,36 +15,36 @@ public class Solution {
             return false;
         }
         
-        char c = pattern.charAt(i);
+        char ch = pattern.charAt(i);
         
-        if (map.containsKey(c)) {
-            String word = map.get(c);
+        if (map.containsKey(ch)) {
+            String word = map.get(ch);
             
             if (str.startsWith(word, j)) {
-                return helper(pattern, i + 1, str, j + word.length(), map, set);
+                return dfs(pattern, str, i + 1, j + word.length(), map, set);
             } else {
                 return false;
             }
-        }
-
-        for (int x = j + 1; x <= str.length(); x++) {
-            String word = str.substring(j, x);
-
-            if (set.contains(word)) {
-                continue;
+        } else {
+            for (int end = j + 1; end <= str.length(); end++) {
+                String word = str.substring(j, end);
+    
+                if (set.contains(word)) {
+                    continue;
+                }
+                
+                map.put(ch, word);
+                set.add(word);
+                
+                if (dfs(pattern, str, i + 1, end, map, set)) {
+                    return true;
+                }
+                
+                map.remove(ch);
+                set.remove(word);
             }
             
-            map.put(c, word);
-            set.add(word);
-            
-            if (helper(pattern, i + 1, str, x, map, set)) {
-                return true;
-            }
-            
-            map.remove(c);
-            set.remove(word);
+            return false;
         }
-        
-        return false;
     }
 }
