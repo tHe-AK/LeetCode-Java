86e348051a25447d699543b4164c4afff943e618
Jason51122 <jason51122@users.noreply.github.com>
2017-02-07 23:19:56 -0800

    Update and rename Flip Game II.java to Flip Game II (294).java

--- Backtracking/Game/Flip Game II (294).java
+++ Backtracking/Game/Flip Game II (294).java
@@ -1,9 +1,5 @@
 public class Solution {
     public boolean canWin(String s) {
-        if (s == null) {
-            throw new IllegalArgumentException();
-        }
-        
         for (int i = 0; i < s.length() - 1; i++) {
             if (s.startsWith("++", i)) {
                 String str = s.substring(0, i) + "--" + s.substring(i + 2);
