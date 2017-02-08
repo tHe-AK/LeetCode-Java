a1e887d893a912ee119a92fc3d80b0170a98fa5f
Jason51122 <jason51122@users.noreply.github.com>
2017-02-08 00:15:34 -0800

    Update Best Meeting Point (296).java

--- Math/Median/Best Meeting Point (296).java
+++ Math/Median/Best Meeting Point (296).java
@@ -1,5 +1,50 @@
 public class Solution {
     public int minTotalDistance(int[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        
+        int row = grid.length;
+        int col = grid[0].length;
+        int[] x = new int[row];
+        int[] y = new int[col];
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    x[i]++;
+                    y[j]++;
+                }
+            }
+        }
+        
+        return getDist(x) + getDist(y);
+    }
+    
+    private int getDist(int[] rec) {
+        int dist = 0;
+        int i = 0;
+        int j = rec.length - 1;
+        
+        while (i < j) {
+            if (rec[i] == 0) {
+                i++;
+            } else if (rec[j] == 0) {
+                j--;
+            } else {
+                int min = Math.min(rec[i], rec[j]);
+                dist += (j - i) * min;
+                rec[i] -= min;
+                rec[j] -= min;
+            }
+        }
+        
+        return dist;
+    }
+}
+
+public class Solution {
+    public int minTotalDistance(int[][] grid) {
         List<Integer> x = new ArrayList<Integer>();
         List<Integer> y = new ArrayList<Integer>();
         
