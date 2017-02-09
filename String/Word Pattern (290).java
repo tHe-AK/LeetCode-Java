1e49ef2dcbd4455984557160531100f66ab1a4fa
Jason51122 <jason51122@users.noreply.github.com>
2017-02-08 23:08:14 -0800

    Update Word Pattern (290).java

--- String/Word Pattern (290).java
+++ String/Word Pattern (290).java
@@ -7,6 +7,28 @@ public class Solution {
             return false;
         }
         
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<String, Integer> map2 = new HashMap<>();
+
+        for (Integer i = 0; i < len; i++) {
+            if (map1.put(pattern.charAt(i), i) != map2.put(words[i], i)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
+
+public class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        int len = words.length;
+        
+        if (pattern.length() != len) {
+            return false;
+        }
+        
         Map<Character, String> map = new HashMap<>();
         Set<String> set = new HashSet<>();
         
