e91ee37f95b653e0bd2375fcbd1327231517c934
Jason51122 <jason51122@users.noreply.github.com>
2017-02-08 21:48:29 -0800

    Update Word Pattern (290).java

--- String/Word Pattern (290).java
+++ String/Word Pattern (290).java
@@ -1,38 +1,33 @@
 public class Solution {
     public boolean wordPattern(String pattern, String str) {
-        if (pattern == null || str == null) {
-            throw new IllegalArgumentException();
-        }
-        
-        int len1 = pattern.length();
         String[] words = str.split(" ");
-        int len2 = words.length;
+        int len = words.length;
         
-        if (len1 != len2) {
+        if (pattern.length() != len) {
             return false;
         }
         
-        Map<Character, String> map = new HashMap<Character, String>();
-        Set<String> set = new HashSet<String>();
+        Map<Character, String> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
         
-        for (int i = 0; i < len1; i++) {
-            char c = pattern.charAt(i);
+        for (int i = 0; i < len; i++) {
+            char ch = pattern.charAt(i);
             String word = words[i];
             
-            if (map.containsKey(c)) {
-                if (map.get(c).equals(word)) {
+            if (map.containsKey(ch)) {
+                if (map.get(ch).equals(word)) {
                     continue;
                 } else {
                     return false;
                 }
+            } else {
+                if (set.contains(word)) {
+                    return false;
+                }
+                
+                map.put(ch, word);
+                set.add(word);
             }
-            
-            if (set.contains(word)) {
-                return false;
-            }
-            
-            map.put(c, word);
-            set.add(word);
         }
         
         return true;
