e2138293a2cd5e72b7b547b33f668b7d636c95c6
Jason51122 <jason51122@users.noreply.github.com>
2017-02-08 21:09:25 -0800

    Update and rename Game of Life.java to Game of Life (289).java

--- Backtracking/Game/Game of Life (289).java
+++ Backtracking/Game/Game of Life (289).java
@@ -1,21 +1,20 @@
 public class Solution {
     public void gameOfLife(int[][] board) {
-        if (board == null) {
-            throw new IllegalArgumentException();
+        if (board.length == 0 || board[0].length == 0) {
+            return;
         }
         
         int row = board.length;
         int col = board[0].length;
-        int[] offsetX = new int[] {-1, 1, 0, 0, -1, 1, 1, -1};
-        int[] offsetY = new int[] {0, 0, -1, 1, -1, 1, -1, 1};
-        
+        int[][] delta = new int[][] { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 } };
+
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 int count = 0;
                 
-                for (int k = 0; k < offsetX.length; k++) {
-                    int x = i + offsetX[k];
-                    int y = j + offsetY[k];
+                for (int[] diff : delta) {
+                    int x = i + diff[0];
+                    int y = j + diff[1];
                     
                     if (x >= 0 && x < row && y >= 0 && y < col && (board[x][y] == 1 || board[x][y] == 2)) {
                         count++;
@@ -24,9 +23,7 @@ public class Solution {
                 
                 if (board[i][j] == 0 && count == 3) {
                     board[i][j] = 3;
-                }
-                    
-                if (board[i][j] == 1 && (count < 2 || count > 3)) {
+                } else if (board[i][j] == 1 && (count < 2 || count > 3)) {
                     board[i][j] = 2;
                 }
             }
