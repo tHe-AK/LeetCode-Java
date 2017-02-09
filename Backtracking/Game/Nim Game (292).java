25f1a923486340fd50e96c53e31fb6a2143202af
Jason51122 <jason51122@users.noreply.github.com>
2017-02-08 23:23:29 -0800

    Update Nim Game (292).java

--- Backtracking/Game/Nim Game (292).java
+++ Backtracking/Game/Nim Game (292).java
@@ -3,3 +3,15 @@ public class Solution {
         return n % 4 != 0;
     }
 }
+
+public class Solution {
+    public boolean canWinNim(int n) {
+        for (int i = 1; i <= 3 && i <= n; i++) {
+            if (!canWinNim(n - i)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
