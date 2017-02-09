3470fc6893edc9e3f2a8b229eeee9b97fab02ecb
Jason51122 <jason51122@users.noreply.github.com>
2017-02-08 23:14:48 -0800

    Update and rename Nim Game.java to Nim Game (292).java

--- Backtracking/Game/Nim Game (292).java
+++ Backtracking/Game/Nim Game (292).java
@@ -1,9 +1,5 @@
 public class Solution {
     public boolean canWinNim(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException();
-        }
-        
         return n % 4 != 0;
     }
 }
