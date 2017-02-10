aebaac0c2075d970c68192a65d8d0c24569e78e9
Jason51122 <jason51122@users.noreply.github.com>
2017-02-10 00:04:56 -0800

    Update and rename Binary Tree Longest Consecutive Sequence.java to Binary Tree Longest Consecutive Sequence (298).java

--- String/Longest Increasing Sequence/Binary Tree Longest Consecutive Sequence (298).java
+++ String/Longest Increasing Sequence/Binary Tree Longest Consecutive Sequence (298).java
@@ -9,16 +9,16 @@
  */
 public class Solution {
     public int longestConsecutive(TreeNode root) {
-        return helper(root, null, 0);
+        return longestConsecutive(root, null, 0);
     }
 
-    private int helper(TreeNode root, TreeNode parent, int len) {
+    private int longestConsecutive(TreeNode root, TreeNode parent, int len) {
         if (root == null) {
             return len;
         }
         
         len = (parent != null && root.val == parent.val + 1) ? len + 1 : 1;
         
-        return Math.max(len, Math.max(helper(root.left, root, len), helper(root.right, root, len)));
+        return Math.max(len, Math.max(longestConsecutive(root.left, root, len), longestConsecutive(root.right, root, len)));
     }
 }
