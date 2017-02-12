905ea6f1feff641be301a44ce0c63754b83e2a95
Jason51122 <jason51122@users.noreply.github.com>
2017-02-12 00:13:40 -0800

    Update Range Sum Query - Mutable (307).java

--- Binary Tree/Segment Tree/Range Sum Query - Mutable (307).java
+++ Binary Tree/Segment Tree/Range Sum Query - Mutable (307).java
@@ -1,3 +1,55 @@
+public class NumArray {
+    private int[] nums;
+    private int[] tree;
+
+    public NumArray(int[] nums) {
+        this.nums = nums;
+        tree = new int[nums.length + 1];
+        
+        for (int i = 0; i < nums.length; i++) {
+            updateDiff(i, nums[i]);
+        }
+    }
+    
+    public void update(int i, int val) {
+        int diff = val - nums[i];
+        nums[i] = val;
+        updateDiff(i, diff);
+    }
+    
+    private void updateDiff(int i, int diff) {
+        i++;
+        
+        while (i < tree.length) {
+            tree[i] += diff;
+            i += i & -i;
+        }
+    }
+    
+    public int sumRange(int i, int j) {
+        return getSum(j) - getSum(i - 1);
+    }
+    
+    private int getSum(int i) {
+        i++;
+        int sum = 0;
+        
+        while (i > 0) {
+            sum += tree[i];
+            i -= i & -i;
+        }
+        
+        return sum;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(i,val);
+ * int param_2 = obj.sumRange(i,j);
+ */
+
 class SegmentTreeNode {
     public int start;
     public int end;
