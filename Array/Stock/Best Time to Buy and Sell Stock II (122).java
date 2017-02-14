1c14d27bce167b715cbe7e1e54dac40eb59cadc5
Jason51122 <jason51122@users.noreply.github.com>
2017-02-13 19:47:02 -0800

    Update Best Time to Buy and Sell Stock II (122).java

--- Array/Stock/Best Time to Buy and Sell Stock II (122).java
+++ Array/Stock/Best Time to Buy and Sell Stock II (122).java
@@ -1,5 +1,19 @@
 public class Solution {
     public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        
+        for (int price : prices) {
+            min = Math.min(min, price - profit);
+            profit = Math.max(profit, price - min);
+        }
+        
+        return profit;
+    }
+}
+
+public class Solution {
+    public int maxProfit(int[] prices) {
         int profit = 0;
         
         for (int i = 1; i < prices.length; i++) {
