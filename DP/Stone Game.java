d4ddabd6fef4d900205287c76bd5e62ca80afc96
Jason51122 <jason51122@users.noreply.github.com>
2017-02-13 21:42:39 -0800

    Update Stone Game.java

--- DP/Stone Game.java
+++ DP/Stone Game.java
@@ -1,18 +1,18 @@
 public class Solution {
-    public int stoneGame(int[] num) {
-        if (num.length == 0) {
+    public int stoneGame(int[] nums) {
+        if (nums.length == 0) {
             return 0;
         }
         
-        int len = num.length;
+        int len = nums.length;
         int[][] sum = new int[len][len];
         int[][] dp = new int[len][len];
 
         for (int i = 0; i < len; i++) {
-            sum[i][j] = num[j];
+            sum[i][i] = nums[i];
             
             for (int j = i + 1; j < len; j++) {
-                sum[i][j] += sum[i][j - 1];
+                sum[i][j] += sum[i][j - 1] + nums[j];
             }
         }
         
