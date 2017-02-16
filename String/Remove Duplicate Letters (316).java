0d469d4d15e956c2a272d07741f2751683a31d21
Jason51122 <jason51122@users.noreply.github.com>
2017-02-15 23:22:36 -0800

    Update Remove Duplicate Letters (316).java

--- String/Remove Duplicate Letters (316).java
+++ String/Remove Duplicate Letters (316).java
@@ -1,30 +1,32 @@
 public class Solution {
     public String removeDuplicateLetters(String s) {
-        if (s.length() == 0) {
-            return "";
-        }
-        
-        int len = s.length();
-        int[] rec = new int[26];
-        int idx = 0;
         String result = "";
         
-        for (int i = 0; i < len; i++) {
-            rec[s.charAt(i) - 'a']++;
-        }
-        
-        for (int i = 0; i < len; i++) {
-            char ch = s.charAt(i);
+        while (s.length() > 0) {
+            int len = s.length();
+            int[] rec = new int[26];
+            int idx = 0;
             
-            if (ch < s.charAt(idx)) {
-                idx = i;
+            for (int i = 0; i < len; i++) {
+                rec[s.charAt(i) - 'a']++;
             }
             
-            if (--rec[ch - 'a'] == 0) {
-                break;
+            for (int i = 0; i < len; i++) {
+                char ch = s.charAt(i);
+                
+                if (ch < s.charAt(idx)) {
+                    idx = i;
+                }
+                
+                if (--rec[ch - 'a'] == 0) {
+                    break;
+                }
             }
+            
+            result += s.charAt(idx);
+            s = s.substring(idx + 1).replaceAll(s.charAt(idx) + "", "");
         }
         
-        return s.charAt(idx) + removeDuplicateLetters(s.substring(idx + 1).replaceAll(s.charAt(idx) + "", ""));
+        return result;
     }
 }
