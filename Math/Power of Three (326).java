064912ffbdaed9e1729d5669aa644cb6e7f23e1a
Jason51122 <jason51122@users.noreply.github.com>
2017-02-18 23:14:45 -0800

    Update Power of Three (326).java

--- Math/Power of Three (326).java
+++ Math/Power of Three (326).java
@@ -11,3 +11,9 @@ public class Solution {
         return n == 1;
     }
 }
+
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        return n > 0 && Math.pow(3, 19) % n == 0;
+    }
+}
