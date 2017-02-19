60c844a712eba9526f5a46a4b68d1b5496fbe8a5
Jason51122 <jason51122@users.noreply.github.com>
2017-02-18 22:10:14 -0800

    Update Power of Two (231).java

--- Bit Manipulation/Power of Two (231).java
+++ Bit Manipulation/Power of Two (231).java
@@ -1,9 +1,5 @@
 public class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n <= 0) {
-            return false;
-        }
-        
-        return (n & (n - 1)) == 0;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
