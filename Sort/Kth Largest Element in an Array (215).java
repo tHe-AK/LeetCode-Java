f15acae84ac4737e69d66598d3fa1064770ec875
Jason51122 <jason51122@users.noreply.github.com>
2017-02-18 21:40:38 -0800

    Update Kth Largest Element in an Array (215).java

--- Sort/Kth Largest Element in an Array (215).java
+++ Sort/Kth Largest Element in an Array (215).java
@@ -21,27 +21,26 @@ public class Solution {
     
     private int findKthLargest(int[] nums, int k, int low, int high) {
         int pivot = nums[low];
-        int curr = low + 1;
+        int idx = low + 1;
         
-        for (int i = low + 1; i <= high; i++) {
+        for (int i = idx; i <= high; i++) {
             if (nums[i] > pivot) {
                 int temp = nums[i];
-                nums[i] = nums[curr];
-                nums[curr++] = temp;
+                nums[i] = nums[idx];
+                nums[idx++] = temp;
             }
         }
-        
-        nums[low] = nums[--curr];
-        nums[curr] = pivot;
-        
-        int count = curr - low + 1;
+  
+        int count = idx - low;
+        nums[low] = nums[--idx];
+        nums[idx] = pivot;
         
         if (count == k) {
-            return nums[curr];
+            return nums[idx];
         } else if (count < k) {
-            return findKthLargest(nums, k - count, curr + 1, high);
+            return findKthLargest(nums, k - count, idx + 1, high);
         } else {
-            return findKthLargest(nums, k, low, curr - 1);
+            return findKthLargest(nums, k, low, idx - 1);
         }
     }
 }
