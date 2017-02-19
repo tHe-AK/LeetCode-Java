a2f92c7fe9950cb81cf6fbae4f4800f752d69021
Jason51122 <jason51122@users.noreply.github.com>
2017-02-19 15:44:40 -0800

    Update Largest BST Subtree (333).java

--- Binary Search Tree/Largest BST Subtree (333).java
+++ Binary Search Tree/Largest BST Subtree (333).java
@@ -10,10 +10,10 @@
  
 class SubTree {
     public int size;
-    public long min;
-    public long max;
+    public Long min;
+    public Long max;
      
-    public SubTree(int size, long min, long max) {
+    public SubTree(int size, Long min, Long max) {
         this.size = size;
         this.min = min;
         this.max = max;
@@ -30,19 +30,19 @@ public class Solution {
     
     private SubTree traverse(TreeNode root, int[] max) {
         if (root == null) {
-            return new SubTree(0, Long.MAX_VALUE, Long.MIN_VALUE);
+            return new SubTree(0, null, null);
         }
         
         SubTree left = traverse(root.left, max);
         SubTree right = traverse(root.right, max);
         
-        if (left.size == -1 || right.size == -1 || root.val <= left.max || root.val >= right.min) {
-            return new SubTree(-1, 0, 0);
+        if (left.size == -1 || right.size == -1 || (left.max != null && root.val <= left.max) || (right.min != null && root.val >= right.min)) {
+            return new SubTree(-1, null, null);
         }
         
         int size = left.size + right.size + 1;
         max[0] = Math.max(max[0], size);
         
-        return new SubTree(size, Math.min(left.min, root.val), Math.max(right.max, root.val));
+        return new SubTree(size, left.min != null ? left.min : root.val, right.max != null ? right.max : root.val);
     }
 }
