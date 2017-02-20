2fdcfee0972a488b3acb747fc24000e3ed069a2e
Jason51122 <jason51122@users.noreply.github.com>
2017-02-20 14:42:03 -0800

    Create Data Stream as Disjoint Intervals (352).java

---
+++ Array/Interval/Data Stream as Disjoint Intervals (352).java
@@ -0,0 +1,49 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }                                                              
+ */
+public class SummaryRanges {
+    private TreeMap<Integer, Interval> map;
+    
+    /** Initialize your data structure here. */                      
+    public SummaryRanges() {
+        map = new TreeMap<>();
+    }
+    
+    public void addNum(int val) {
+        if (map.containsKey(val)) {
+            return;
+        }
+        
+        Integer low = map.lowerKey(val);
+        Integer high = map.higherKey(val);
+        
+        if (low != null && high != null && val == map.get(low).end + 1 && val == high - 1) {
+            map.get(low).end = map.get(high).end;
+            map.remove(high);
+        } else if (low != null && val <= map.get(low).end + 1) {
+            map.get(low).end = Math.max(map.get(low).end, val);
+        } else if (high != null && val == high - 1) {
+            map.put(val, new Interval(val, map.get(high).end));
+            map.remove(high);
+        } else {
+            map.put(val, new Interval(val, val));
+        }
+    }
+    
+    public List<Interval> getIntervals() {
+        return new ArrayList<>(map.values());
+    }
+}
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * SummaryRanges obj = new SummaryRanges();
+ * obj.addNum(val);
+ * List<Interval> param_2 = obj.getIntervals();
+ */
