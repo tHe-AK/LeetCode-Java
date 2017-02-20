47dec47894bc8f9e3ea07c6569369ec2026ae23d
Jason51122 <jason51122@users.noreply.github.com>
2017-02-19 17:02:30 -0800

    Update and rename Self Crossing.java to Self Crossing (335).java

--- Math/Self Crossing (335).java
+++ Math/Self Crossing (335).java
@@ -1,19 +1,15 @@
 public class Solution {
     public boolean isSelfCrossing(int[] x) {
-        if (x == null) {
-            throw new IllegalArgumentException();
-        }
-        
         for (int i = 3; i < x.length; i++) {
             if (x[i] >= x[i - 2] && x[i - 1] <= x[i - 3]) {
                 return true;
             }
               
-            if (i >= 4 && x[i - 1] == x[i - 3] && x[i] >= x[i - 2] - x[i - 4]) {
+            if (i >= 4 && x[i] >= x[i - 2] - x[i - 4] && x[i - 1] == x[i - 3]) {
                 return true;
             }
               
-            if (i >= 5 && x[i - 2] >= x[i - 4] && x[i] >= x[i - 2] - x[i - 4] && x[i - 3] >= x[i - 1] && x[i - 5] >= x[i - 3] - x[i - 1]) {
+            if (i >= 5 && x[i] >= x[i - 2] - x[i - 4] && x[i - 2] >= x[i - 4] && x[i - 3] >= x[i - 1] && x[i - 5] >= x[i - 3] - x[i - 1]) {
                 return true;
             }
         }
