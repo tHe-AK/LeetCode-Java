fa90bc6a871deda8262fd7f4a04a2d4189911ea6
Jason51122 <jason51122@users.noreply.github.com>
2017-02-19 18:18:15 -0800

    Update and rename Longest Substring with At Most K Distinct Characters.java to Longest Substring with At Most K Distinct Characters (340).java

---
+++ String/Window Substring/Longest Substring with At Most K Distinct Characters (340).java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int result = 0;
+        int[] rec = new int[256];
+        int start = 0;
+        int end = 0;
+        int count = 0;
+        
+        while (end < s.length()) {
+            if (rec[s.charAt(end++)]++ == 0) {
+                count++;
+            }
+            
+            while (count > k) {
+                if (rec[s.charAt(start++)]-- == 1) {
+                    count--;
+                }
+            }
+            
+            result = Math.max(result, end - start);
+        }
+        
+        return result;
+    }
+}
