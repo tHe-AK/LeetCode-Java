5455e1c6a9ae7eac5510f3137df2901848331e47
Jason51122 <jason51122@users.noreply.github.com>
2017-02-19 18:01:26 -0800

    Update Counting Bits (338).java

--- Bit Manipulation/Counting Bits (338).java
+++ Bit Manipulation/Counting Bits (338).java
@@ -1,18 +1,21 @@
 public class Solution {
     public int[] countBits(int num) {
-        if (num < 0) {
-            throw new IllegalArgumentException();
+        int[] rec = new int[num + 1];
+
+        for (int i = 1; i <= num; i++) {
+            rec[i] = rec[i >> 1] + (i & 1);
         }
         
+        return rec;
+    }
+}
+
+public class Solution {
+    public int[] countBits(int num) {
         int[] rec = new int[num + 1];
-        rec[0] = 0;
-        
+
         for (int i = 1; i <= num; i++) {
-            rec[i] = rec[i / 2];
-            
-            if (i % 2 == 1) {
-                rec[i]++;
-            }
+            rec[i] = rec[i & (i - 1)] + 1;
         }
         
         return rec;
