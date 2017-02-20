7d2be5308d7ab135259ee9e9ccb9d7c11b551e2a
Jason51122 <jason51122@users.noreply.github.com>
2017-02-19 18:12:54 -0800

    Update Nested List Weight Sum (339).java

--- Math/Nested List Weight Sum (339).java
+++ Math/Nested List Weight Sum (339).java
@@ -34,3 +34,46 @@ public class Solution {
         return sum;
     }
 }
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        int sum = 0;
+        int level = 0;
+        
+        Queue<NestedInteger> queue = new LinkedList<NestedInteger>(nestedList);
+        
+        while (!queue.isEmpty()) {
+            level++;
+            int size = queue.size();
+            
+            for (int i = 0; i < size; i++) {
+                NestedInteger ni = queue.poll();
+                
+                if (ni.isInteger()) {
+                    sum += ni.getInteger() * level;
+                } else {
+                    queue.addAll(ni.getList());
+                }
+            }
+        }
+        
+        return sum;
+    }
+}
