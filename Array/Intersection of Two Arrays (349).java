ad7b3c8dd06cc4328e9990b9ec3d8ef727e71090
Jason51122 <jason51122@users.noreply.github.com>
2017-02-19 23:52:36 -0800

    Update Intersection of Two Arrays (349).java

--- Array/Intersection of Two Arrays (349).java
+++ Array/Intersection of Two Arrays (349).java
@@ -23,3 +23,41 @@ public class Solution {
         return result;
     }
 }
+
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> intersect = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                intersect.add(nums1[i]);
+                i++;
+                j++;
+                
+                while (i < nums1.length && nums1[i] == nums1[i - 1]) {
+                    i++;
+                }
+                
+                while (j < nums2.length && nums2[j] == nums2[j - 1]) {
+                    j++;
+                }
+            }
+        }
+    
+       int[] result = new int[intersect.size()];
+       
+       for (i = 0; i < result.length; i++) {
+           result[i] = intersect.get(i);
+       }
+    
+       return result;
+    }
+}
