7353493e9790a6573608909cb2d429650bef00f0
Jason51122 <jason51122@users.noreply.github.com>
2017-02-19 20:34:10 -0800

    Create Power of Four (342).java

---
+++ Bit Manipulation/Power of Four (342).java
@@ -0,0 +1,25 @@
+public class Solution {
+    public boolean isPowerOfFour(int num) {
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n % 4 == 0) {
+            n /= 4;
+        }
+
+        return n == 1;
+    }
+}
+
+public class Solution {
+    public boolean isPowerOfFour(int num) {
+        return num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0;
+    }
+}
+
+public class Solution {
+    public boolean isPowerOfFour(int num) {
+        return num > 0 && (num & (num - 1)) == 0 && (num & 0x55555555) != 0;
+    }
+}
