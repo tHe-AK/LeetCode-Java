e687262ea839c0a734ab2b0537d1b371914b0416
Jason51122 <jason51122@users.noreply.github.com>
2017-02-19 20:54:48 -0800

    Update Integer Break (343).java

--- DP/Integer Break (343).java
+++ DP/Integer Break (343).java
@@ -17,3 +17,18 @@ public class Solution {
         }
     }
 }
+
+public class Solution {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j, dp[j]) * (i - j));
+            }
+        }
+        
+        return dp[n];
+    }
+}
