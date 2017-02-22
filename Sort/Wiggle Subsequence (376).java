43fcbe3c111e933fd6e783b687fc0a2e1111e7ea
Jason51122 <jason51122@users.noreply.github.com>
2017-02-21 23:57:01 -0800

    Update Wiggle Subsequence (376).java

--- Sort/Wiggle Subsequence (376).java
+++ Sort/Wiggle Subsequence (376).java
@@ -1,5 +1,11 @@
 public class Solution {
     public int wiggleMaxLength(int[] nums) {
+        int len = nums.length;
+        
+        if (len < 2) {
+            return len;
+        }
+        
         int down = 1;
         int up = 1;
         
@@ -12,5 +18,29 @@ public class Solution {
         }
         
         return Math.max(down, up);
+    }
+}
+
+public class Solution {
+    public int wiggleMaxLength(int[] nums) {
+        int len = nums.length;
+        
+        if (len < 2) {
+            return len;
+        }
+        
+        int prevdiff = nums[1] - nums[0];
+        int count = prevdiff != 0 ? 2 : 1;
+        
+        for (int i = 2; i < nums.length; i++) {
+            int diff = nums[i] - nums[i - 1];
+            
+            if ((diff > 0 && prevdiff <= 0) || (diff < 0 && prevdiff >= 0)) {
+                count++;
+                prevdiff = diff;
+            }
+        }
+        
+        return count;
     }
 }
