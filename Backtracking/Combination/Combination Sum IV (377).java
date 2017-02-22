9a9b948fa10de24f6ae2703adf96859aaad89345
Jason51122 <jason51122@users.noreply.github.com>
2017-02-22 00:25:20 -0800

    Update Combination Sum IV (377).java

--- Backtracking/Combination/Combination Sum IV (377).java
+++ Backtracking/Combination/Combination Sum IV (377).java
@@ -1,13 +1,62 @@
 public class Solution {
     public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                int k = i - nums[j];
+                
+                if (k >= 0) {
+                    dp[i] += dp[k];
+                }
+            }
+        }
+        
+        return dp[target];
+    }
+}
+
+public class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 1;
+        
+        return dfs(nums, target, dp);
+    }
+    
+    private int dfs(int[] nums, int target, int[] dp) {
+        if (dp[target] >= 0) {
+            return dp[target];
+        }
+        
+        int count = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= target) {
+                count += dfs(nums, target - nums[i], dp);
+            }
+        }
+        
+        dp[target] = count;
+        
+        return count;
+    }
+}
+
+public class Solution {
+    public int combinationSum4(int[] nums, int target) {
         if (target == 0) {
             return 1;
         }
         
         int count = 0;
         
-        for (int i = 0; i < nums.length && nums[i] <= target; i++) {
-            count += combinationSum4(nums, target - nums[i]);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= target) {
+                count += combinationSum4(nums, target - nums[i]);
+            }
         }
         
         return count;
