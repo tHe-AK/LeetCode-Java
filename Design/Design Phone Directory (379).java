b8fe12d47543cce1dc64eaf9998a1e3c9d171e85
Jason51122 <jason51122@users.noreply.github.com>
2017-02-23 21:45:57 -0800

    Update Design Phone Directory (379).java

--- Design/Design Phone Directory (379).java
+++ Design/Design Phone Directory (379).java
@@ -29,10 +29,6 @@ public class PhoneDirectory {
     
     /** Check if a number is available or not. */
     public boolean check(int number) {
-        if (number < 0 || number >= maxNumbers) {
-            return false;
-        }
-        
         return !used.contains(number);
     }
     
@@ -40,6 +36,56 @@ public class PhoneDirectory {
     public void release(int number) {
         if (used.remove(number)) {
             available.offer(number);
+        }
+    }
+}
+
+/**
+ * Your PhoneDirectory object will be instantiated and called as such:
+ * PhoneDirectory obj = new PhoneDirectory(maxNumbers);
+ * int param_1 = obj.get();
+ * boolean param_2 = obj.check(number);
+ * obj.release(number);
+ */
+
+public class PhoneDirectory {
+    private BitSet bitSet;
+    private int maxNumbers;
+    private int next;
+    
+    /** Initialize your data structure here
+        @param maxNumbers - The maximum numbers that can be stored in the phone directory. */
+    public PhoneDirectory(int maxNumbers) {
+        this.maxNumbers = maxNumbers;
+        bitSet = new BitSet(maxNumbers);
+        next = 0;
+    }
+    
+    /** Provide a number which is not assigned to anyone.
+        @return - Return an available number. Return -1 if none is available. */
+    public int get() {
+        if (next == maxNumbers) {
+            return -1;
+        }
+        
+        int num = next;
+        bitSet.set(next);
+        next = bitSet.nextClearBit(next);
+
+        return num;
+    }
+    
+    /** Check if a number is available or not. */
+    public boolean check(int number) {
+        return !bitSet.get(number);
+    }
+    
+    /** Recycle or release a number. */
+    public void release(int number) {
+        bitSet.clear(number);
+            
+        if (number < next) {
+            next = number;
         }
     }
 }
