007e3e481258adb5360e34ea1743d2a7c186c218
Jason51122 <jason51122@users.noreply.github.com>
2017-02-24 11:45:10 -0800

    Create Shuffle an Array (384).java

---
+++ Math/Probability/Shuffle an Array (384).java
@@ -0,0 +1,39 @@
+public class Solution {
+    private int[] nums;
+    private Random random;
+    
+    public Solution(int[] nums) {
+        this.nums = nums;
+        random = new Random();
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return nums;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        int[] shuffle = nums.clone();
+        
+        for (int i = 0; i < shuffle.length; i++) {
+            int j = random.nextInt(shuffle.length - i);
+            swap(shuffle, i, i + j);
+        }
+        
+        return shuffle;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
