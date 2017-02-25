61fb520eeec25aadc4773c603d32305086462e00
Jason51122 <jason51122@users.noreply.github.com>
2017-02-25 15:42:13 -0800

    Create Find the Difference (389).java

---
+++ Bit Manipulation/Find the Difference (389).java
@@ -0,0 +1,13 @@
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        int len = s.length();
+	    char ch = t.charAt(len);
+	    
+	    for (int i = 0; i < len; i++) {
+		    ch ^= s.charAt(i);
+		    ch ^= t.charAt(i);
+	    }
+	    
+	    return ch;
+    }
+}
