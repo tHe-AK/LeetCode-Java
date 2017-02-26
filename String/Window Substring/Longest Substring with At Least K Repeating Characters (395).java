5e0aca1375fdad6522c1bb07a7962087522135de
Jason51122 <jason51122@users.noreply.github.com>
2017-02-26 00:34:15 -0800

    Update and rename Longest Substring with At Least K Repeating Characters.java to Longest Substring with At Least K Repeating Characters (395).java

--- String/Window Substring/Longest Substring with At Least K Repeating Characters (395).java
+++ String/Window Substring/Longest Substring with At Least K Repeating Characters (395).java
@@ -1,13 +1,5 @@
 public class Solution {
     public int longestSubstring(String s, int k) {
-        if (s == null || k < 0) {
-            throw new IllegalArgumentException();
-        }
-        
-        if (s.length() < k) {
-            return 0;
-        }
-        
         int len = s.length();
         int[] counts = new int[26];
         
@@ -17,8 +9,8 @@ public class Solution {
         
         for (int i = 0; i < 26; i++) {
             if (counts[i] > 0 && counts[i] < k) {
-                char c = (char) ('a' + i);
-                String[] strs = s.split(Character.toString(c));
+                char ch = (char) ('a' + i);
+                String[] strs = s.split(Character.toString(ch));
                 int result = 0;
                 
                 for (String str : strs) {
