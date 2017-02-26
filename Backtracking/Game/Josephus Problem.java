f3c743ec45bb4282772c13ed8e64a8de5f5660b8
Jason51122 <jason51122@users.noreply.github.com>
2017-02-25 16:55:06 -0800

    Update Josephus Problem.java

--- Backtracking/Game/Josephus Problem.java
+++ Backtracking/Game/Josephus Problem.java
@@ -1,5 +1,5 @@
 public class Solution {
     public int josephus(int n, int k) {
-        return n == 1 ? 1 : (josephus(n - 1, k) + 1) % n + 1;
+        return n == 1 ? 1 : (josephus(n - 1, k) + k - 1) % n + 1;
     }
 }
