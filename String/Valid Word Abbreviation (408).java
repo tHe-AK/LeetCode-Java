2915b441728144708d4daf661ee39336fd20b646
Jason51122 <jason51122@users.noreply.github.com>
2017-02-27 00:44:28 -0800

    Update and rename Valid Word Abbreviation.java to Valid Word Abbreviation (408).java

--- String/Valid Word Abbreviation (408).java
+++ String/Valid Word Abbreviation (408).java
@@ -1,28 +1,23 @@
 public class Solution {
     public boolean validWordAbbreviation(String word, String abbr) {
-        if (word == null || abbr == null) {
-            throw new IllegalArgumentException();
-        }
-        
         int i = 0;
         int j = 0;
         int len1 = word.length();
         int len2 = abbr.length();
         
         while (i < len1 && j < len2) {
-            char c1 = word.charAt(i);
-            char c2 = abbr.charAt(j);
+            char ch1 = word.charAt(i);
+            char ch2 = abbr.charAt(j);
             
-            if (Character.isDigit(abbr.charAt(j)) && c2 != '0') {
+            if (Character.isDigit(abbr.charAt(j)) && ch2 != '0') {
                 int start = j++;
             
                 while (j < len2 && Character.isDigit(abbr.charAt(j))) {
                     j++;
                 }
                 
-                int count = Integer.parseInt(abbr.substring(start, j));
-                i += count;
-            } else if (c1 == c2) {
+                i += Integer.parseInt(abbr.substring(start, j));
+            } else if (ch1 == ch2) {
                 i++;
                 j++;
             } else {
