7c3ce462248e15fad65333367131fb371554d659
Jason51122 <jason51122@users.noreply.github.com>
2017-02-27 00:20:31 -0800

    Update and rename Trapping Rain Water II.java to Trapping Rain Water II (407).java

--- Heap/Trapping Rain Water II (407).java
+++ Heap/Trapping Rain Water II (407).java
@@ -1,9 +1,9 @@
 class Cell {
-    int x;
-    int y;
-    int height;
+    public int x;
+    public int y;
+    public int height;
     
-    Cell(int x, int y, int height) {
+    public Cell(int x, int y, int height) {
         this.x = x;
         this.y = y;
         this.height = height;
@@ -12,24 +12,15 @@ class Cell {
 
 public class Solution {
     public int trapRainWater(int[][] heightMap) {
-        if (heightMap == null) {
-            throw new IllegalArgumentException();
-        }
-        
-        int sum = 0;
-        
         if (heightMap.length == 0 || heightMap[0].length == 0) {
-            return sum;
+            return 0;
         }
         
+        int volume = 0;
         int row = heightMap.length;
         int col = heightMap[0].length;
         boolean[][] visited = new boolean[row][col];
-        PriorityQueue<Cell> pq = new PriorityQueue<Cell>(new Comparator<Cell>() {
-            public int compare(Cell c1, Cell c2) {
-                return c1.height - c2.height;
-            }
-        });
+        PriorityQueue<Cell> pq = new PriorityQueue<>((a, b) -> a.height - b.height);
         
         for (int i = 0; i < row; i++) {
             pq.offer(new Cell(i, 0, heightMap[i][0]));
@@ -45,26 +36,26 @@ public class Solution {
             visited[row - 1][j] = true;
         }
         
-        int[][] delta = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
         while (!pq.isEmpty()) {
             Cell peek = pq.poll();
             
-            for (int i = 0; i < delta.length; i++) {
-                int x = peek.x + delta[i][0];
-                int y = peek.y + delta[i][1];
+            for (int[] diff : delta) {
+                int x = peek.x + diff[0];
+                int y = peek.y + diff[1];
                 
                 if (x >= 0 && x < row && y >= 0 && y < col && !visited[x][y]) {
                     pq.offer(new Cell(x, y, Math.max(peek.height, heightMap[x][y])));
                     visited[x][y] = true;
                     
                     if (peek.height > heightMap[x][y]) {
-                        sum += peek.height - heightMap[x][y];
+                        volume += peek.height - heightMap[x][y];
                     }
                 }
             }
         }
         
-        return sum;
+        return volume;
     }
 }
