1a8a1ef00b2106d437cd49ac513ec3909f7e23c1
Jason51122 <jason51122@users.noreply.github.com>
2017-02-26 23:44:10 -0800

    Update and rename Convert a Number to Hexadecimal.java to Convert a Number to Hexadecimal (405).java

--- Bit Manipulation/Convert a Number to Hexadecimal (405).java
+++ Bit Manipulation/Convert a Number to Hexadecimal (405).java
@@ -5,7 +5,7 @@ public class Solution {
         }
         
         String result = "";
-        String[] map = new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
+        String[] map = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
         
         while (num != 0) {
             result = map[num & 0b1111] + result;
