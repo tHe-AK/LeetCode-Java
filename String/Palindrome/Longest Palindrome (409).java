d5a0ff215dac4985d05fec329f74b4d2366e1e50
Jason51122 <jason51122@users.noreply.github.com>
2017-02-27 00:46:33 -0800

    Update and rename Longest Palindrome.java to Longest Palindrome (409).java

--- String/Palindrome/Longest Palindrome (409).java
+++ String/Palindrome/Longest Palindrome (409).java
@@ -1,20 +1,15 @@
 public class Solution {
     public int longestPalindrome(String s) {
-        if (s == null) {
-            throw new IllegalArgumentException();
-        }
-        
         int count = 0;
-        Set<Character> set = new HashSet<Character>();
+        Set<Character> set = new HashSet<>();
         
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+            char ch = s.charAt(i);
             
-            if (set.contains(c)) {
-                set.remove(c);
+            if (set.remove(ch)) {
                 count++;
             } else {
-                set.add(c);
+                set.add(ch);
             }
         }
         
