3a4e85664f5cf5828d03314611f67d9f4746a8ae
Jason51122 <jason51122@users.noreply.github.com>
2017-02-26 23:15:19 -0800

    Update Frog Jump (403).java

--- Backtracking/Frog Jump (403).java
+++ Backtracking/Frog Jump (403).java
@@ -1,5 +1,30 @@
 public class Solution {
     public boolean canCross(int[] stones) {
+        int len = stones.length;
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        
+        for (int i = 0; i < len; i++) {
+            map.put(stones[i], new HashSet<Integer>());
+        }
+        
+        map.get(0).add(0);
+        
+        for (int i = 0; i < len; i++) {
+            for (int k : map.get(stones[i])) {
+                for (int step = k - 1; step <= k + 1; step++) {
+                    if (step > 0 && map.containsKey(stones[i] + step)) {
+                        map.get(stones[i] + step).add(step);
+                    }
+                }
+            }
+        }
+        
+        return map.get(stones[len - 1]).size() > 0;
+    }
+}
+
+public class Solution {
+    public boolean canCross(int[] stones) {
         return dfs(stones, 0, 0, new boolean[stones.length][stones.length]);
     }
     
