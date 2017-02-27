23b60d1586f700cedad3e7ad139c6cf9f7002b09
Jason51122 <jason51122@users.noreply.github.com>
2017-02-26 23:40:17 -0800

    Update Sum of Left Leaves (404).java

--- Binary Tree/Sum of Left Leaves (404).java
+++ Binary Tree/Sum of Left Leaves (404).java
@@ -22,3 +22,42 @@ public class Solution {
         }
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int result = 0;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            TreeNode peek = queue.poll();
+            
+            if (peek.left != null && peek.left.left == null && peek.left.right == null) {
+                result += peek.left.val;
+            }
+                
+            if (peek.left != null) {
+                queue.offer(peek.left);
+            }
+                
+            if (peek.right != null) {
+                queue.offer(peek.right);
+            }
+        }
+        
+        return result;
+    }
+}
