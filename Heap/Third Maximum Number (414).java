9513cfc8cd2f3bd3bfad38953b852f82f54b1674
Jason51122 <jason51122@users.noreply.github.com>
2017-02-27 22:00:25 -0800

    Update Third Maximum Number (414).java

--- Heap/Third Maximum Number (414).java
+++ Heap/Third Maximum Number (414).java
@@ -1,5 +1,30 @@
 public class Solution {
     public int thirdMax(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i : nums) {
+            if (set.add(i)) {
+                pq.offer(i);
+
+                if (pq.size() > 3) {
+                    set.remove(pq.poll());
+                }
+            }
+        }
+        
+        if (pq.size() < 3) {
+            while (pq.size() > 1) {
+                pq.poll();
+            }
+        }
+        
+        return pq.peek();
+    }
+}
+
+public class Solution {
+    public int thirdMax(int[] nums) {
         Integer max1 = null;
         Integer max2 = null;
         Integer max3 = null;
