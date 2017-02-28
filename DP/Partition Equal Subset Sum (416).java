4c6890e49f4ee793567702c2c497476acce7943a
Jason51122 <jason51122@users.noreply.github.com>
2017-02-27 22:38:30 -0800

    Create Partition Equal Subset Sum (416).java

---
+++ DP/Partition Equal Subset Sum (416).java
@@ -0,0 +1,30 @@
+public class Solution {
+    public boolean canPartition(int[] nums) {
+        if (nums.length <= 1) {
+            return false;
+        }
+
+        int sum = 0;
+        
+        for (int num : nums) {
+            sum += num;
+        }
+        
+        if (sum % 2 != 0) {
+            return false;
+        }
+        
+        sum /= 2;
+
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
+        
+        for (int num : nums) {
+            for (int j = sum; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+        
+        return dp[sum];
+    }
+}
