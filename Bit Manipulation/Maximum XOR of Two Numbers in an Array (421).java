e4f4b827f76c8f51b957483108829924a89ded3a
Jason51122 <jason51122@users.noreply.github.com>
2017-03-01 00:12:36 -0800

    Update Maximum XOR of Two Numbers in an Array (421).java

--- Bit Manipulation/Maximum XOR of Two Numbers in an Array (421).java
+++ Bit Manipulation/Maximum XOR of Two Numbers in an Array (421).java
@@ -24,3 +24,53 @@ public class Solution {
         return max;
     }
 }
+
+class TrieNode {
+    public TrieNode[] links;
+    
+    public TrieNode() {
+        links = new TrieNode[2];
+    }
+}
+    
+public class Solution {
+    public int findMaximumXOR(int[] nums) {
+        TrieNode root = new TrieNode();
+        
+        for (int num: nums) {
+            TrieNode node = root;
+            
+            for (int i = 31; i >= 0; i--) {
+                int bit = (num >>> i) & 1;
+                
+                if (node.links[bit] == null) {
+                    node.links[bit] = new TrieNode();
+                }
+                
+                node = node.links[bit];
+            }
+        }
+        
+        int max = 0;
+        
+        for (int num: nums) {
+            TrieNode node = root;
+            int curr = 0;
+            
+            for (int i = 31; i >= 0; i--) {
+                int bit = (num >>> i) & 1;
+                
+                if (node.links[bit ^ 1] != null) {
+                    curr += (1 << i);
+                    node = node.links[bit ^ 1];
+                } else {
+                    node = node.links[bit];
+                }
+            }
+            
+            max = Math.max(max, curr);
+        }
+        
+        return max;
+    }
+}
