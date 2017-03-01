24f944f7534dc7747878d0b286e6e3bc23801d54
Jason51122 <jason51122@users.noreply.github.com>
2017-03-01 00:33:10 -0800

    Update Valid Word Square (422).java

--- Array/Valid Word Square (422).java
+++ Array/Valid Word Square (422).java
@@ -1,6 +1,8 @@
 public class Solution {
     public boolean validWordSquare(List<String> words) {
-        for (int i = 0; i < words.size(); i++) {
+        int n = words.size();
+        
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < words.get(i).length(); j++){
                 if (j >= n || i >= words.get(j).length() || words.get(i).charAt(j) != words.get(j).charAt(i)) {
                     return false;
