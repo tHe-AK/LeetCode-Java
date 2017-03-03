2ecf14994d93d8a33072d024ab43d270a37f0036
Jason51122 <jason51122@users.noreply.github.com>
2017-03-03 00:10:46 -0800

    Update Find Right Interval (436).java

--- Array/Interval/Find Right Interval (436).java
+++ Array/Interval/Find Right Interval (436).java
@@ -30,3 +30,46 @@ public class Solution {
         return result;
     }
 }
+
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public int[] findRightInterval(Interval[] intervals) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> starts = new ArrayList<>();
+        int len = intervals.length;
+        int[] result = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            int start = intervals[i].start;
+            map.put(start, i);
+            starts.add(start);
+        }
+        
+        Collections.sort(starts);
+        
+        for (int i = 0; i < len; i++) {
+            int end = intervals[i].end;
+            int idx = Collections.binarySearch(starts, end);
+            
+            if (idx < 0) {
+                idx = -(idx + 1);
+            }
+            
+            if (idx == len) {
+                result[i] = -1;
+            } else {
+                result[i] = map.get(starts.get(idx));
+            }
+        }
+        
+        return result;
+    }
+}
