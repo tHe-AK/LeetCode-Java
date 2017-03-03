696e7cdd168209903293746ec84437ac8d4d2e81
Jason51122 <jason51122@users.noreply.github.com>
2017-03-02 21:54:21 -0800

    Update and rename All O`one Data Structure.java to All O`one Data Structure (432).java

--- Design/All O`one Data Structure (432).java
+++ Design/All O`one Data Structure (432).java
@@ -1,53 +1,55 @@
-class Node {
-    int val;
-    Set<String> keySet;
-    Node pre;
-    Node next;
+class ListNode {
+    public int val;
+    public Set<String> keySet;
+    public ListNode pre;
+    public ListNode next;
     
-    public Node(int val, Set<String> keySet) {
+    public ListNode(int val, Set<String> keySet) {
         this.val = val;
         this.keySet = keySet;
     }
 }
 
-
 public class AllOne {
-    private Node head;
-    private Node tail;
-    private Map<String, Node> keyMap;
+    private ListNode head;
+    private ListNode tail;
+    private Map<String, ListNode> keyMap;
 
     /** Initialize your data structure here. */
     public AllOne() {
-        head = new Node(0, new HashSet<String>());
-        tail = new Node(Integer.MAX_VALUE, new HashSet<String>());
+        head = new ListNode(0, new HashSet<String>());
+        tail = new ListNode(0, new HashSet<String>());
         head.next = tail;
         tail.pre = head;
-        keyMap = new HashMap<String, Node>();
+        keyMap = new HashMap<String, ListNode>();
     }
     
     /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
     public void inc(String key) {
         if (keyMap.containsKey(key)) {
-            Node cur = keyMap.get(key);
-            insertNext(cur, key, cur.val + 1);
-            remove(cur, key);
+            ListNode curr = keyMap.get(key);
+            insertNext(curr, key, curr.val + 1);
+            keyMap.put(key, curr.next);
+            remove(curr, key);
         } else {
             insertNext(head, key, 1);
+            keyMap.put(key, head.next);
         }
     }
     
     /** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
     public void dec(String key) {
         if (keyMap.containsKey(key)) {
-            Node cur = keyMap.get(key);
+            ListNode curr = keyMap.get(key);
             
-            if (cur.val > 1) {
-                insertPre(cur, key, cur.val - 1);
+            if (curr.val > 1) {
+                insertPre(curr, key, curr.val - 1);
+                keyMap.put(key, curr.pre);
             } else {
                 keyMap.remove(key);
             }
             
-            remove(cur, key);
+            remove(curr, key);
         }
     }
     
@@ -61,45 +63,42 @@ public class AllOne {
         return head.next == tail ? "" : head.next.keySet.iterator().next();
     }
 
-    private void insertPre(Node next, String key, int val) {
-        Node pre = next.pre;
+    private void insertPre(ListNode curr, String key, int val) {
+        ListNode pre = curr.pre;
         
         if (pre.val == val) {
             pre.keySet.add(key);
-            keyMap.put(key, pre);
         } else {
-            insert(pre, next, key, val);
+            insert(pre, curr, key, val);
         }
     }
     
-    private void insertNext(Node pre, String key, int val) {
-        Node next = pre.next;
+    private void insertNext(ListNode curr, String key, int val) {
+        ListNode next = curr.next;
         
         if (next.val == val) {
             next.keySet.add(key);
-            keyMap.put(key, next);
         } else {
-            insert(pre, next, key, val);
+            insert(curr, next, key, val);
         }
     }
     
-    private void insert(Node pre, Node next, String key, int val) {
+    private void insert(ListNode pre, ListNode next, String key, int val) {
         Set<String> keySet = new HashSet<String>();
         keySet.add(key);
-        Node cur = new Node(val, keySet);
-        pre.next = cur;
-        cur.next = next;
-        next.pre = cur;
-        cur.pre = pre;
-        keyMap.put(key, cur);
+        ListNode curr = new ListNode(val, keySet);
+        pre.next = curr;
+        curr.next = next;
+        next.pre = curr;
+        curr.pre = pre;
     }
     
-    private void remove(Node cur, String key) {
-        cur.keySet.remove(key);
+    private void remove(ListNode curr, String key) {
+        curr.keySet.remove(key);
         
-        if (cur.keySet.isEmpty()) {
-            Node pre = cur.pre;
-            Node next = cur.next;
+        if (curr.keySet.isEmpty()) {
+            ListNode pre = curr.pre;
+            ListNode next = curr.next;
             pre.next = next;
             next.pre = pre;
         }
