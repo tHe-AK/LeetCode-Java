200712083e35a48c1541d8a20a86518e78fac678
Jason51122 <jason51122@users.noreply.github.com>
2017-03-03 22:40:21 -0800

    Update and rename Arranging Coins.java to Arranging Coins (441).java

--- Binary Search/Arranging Coins (441).java
+++ Binary Search/Arranging Coins (441).java
@@ -1,9 +1,5 @@
 public class Solution {
     public int arrangeCoins(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException();
-        }
-        
         int low = 0;
         int high = n;
         
@@ -11,9 +7,7 @@ public class Solution {
             int mid = low + (high - low) / 2;
             long sum = (long) mid * (mid + 1) / 2;
             
-            if (sum == n) {
-                return mid;
-            } else if (sum <= n) {
+            if (sum <= n) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
@@ -26,10 +20,6 @@ public class Solution {
 
 public class Solution {
     public int arrangeCoins(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException();
-        }
-        
         return (int) ((-1 + Math.sqrt(1 + 8 * (long) n)) / 2);
     }
 }
