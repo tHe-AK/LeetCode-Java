85b709b0795bf84757dcaa65f60803b4c044be5e
Jason51122 <jason51122@users.noreply.github.com>
2017-03-06 17:09:05 -0800

    Create Minimum Moves to Equal Array Elements (453).java

---
+++ Array/Minimum Moves to Equal Array Elements (453).java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int minMoves(int[] nums) {
+        int moves = 0;
+        int min = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < nums.length; i++) {
+            min = Math.min(min, nums[i]);
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            moves += nums[i] - min;
+        }
+        
+        return moves;
+    }
+}
