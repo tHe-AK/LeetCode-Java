16377bc162c82c9ae2dfd66005d4fda2c8fb8e15
Jason51122 <jason51122@users.noreply.github.com>
2017-03-10 22:26:49 -0800

    Update Find Median from Data Stream (295).java

--- Math/Median/Find Median from Data Stream (295).java
+++ Math/Median/Find Median from Data Stream (295).java
@@ -9,6 +9,44 @@ public class MedianFinder {
     }
     
     public void addNum(int num) {
+        if (max.isEmpty() || num <= max.peek()) {
+            max.offer(num);
+            
+            if (max.size() > min.size() + 1) {
+                min.offer(max.poll());
+            }
+        } else {
+            min.offer(num);
+            
+            if (max.size() < min.size()) {
+                max.offer(min.poll());
+            }
+        }
+    }
+    
+    public double findMedian() {
+        return max.size() == min.size() ? ((double) max.peek() + min.peek()) / 2 : max.peek();
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
+
+public class MedianFinder {
+    Queue<Integer> max; 
+    Queue<Integer> min;
+    
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        max = new PriorityQueue<Integer>((a, b) -> b.compareTo(a));
+        min = new PriorityQueue<Integer>();
+    }
+    
+    public void addNum(int num) {
         max.offer(num);
         min.offer(max.poll());
             
