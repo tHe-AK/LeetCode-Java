b515d06de7170ff03785c2024329f773781351e9
Jason51122 <jason51122@users.noreply.github.com>
2017-03-13 00:12:07 -0700

    Update The Maze (490).java

--- Backtracking/The Maze (490).java
+++ Backtracking/The Maze (490).java
@@ -1,7 +1,52 @@
 public class Solution {
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        if (start[0] == destination[0] && start[1] == destination[1]) {
+            return true;
+        }
+        
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(start);
         boolean[][] visited = new boolean[maze.length][maze[0].length];
-		    int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+        visited[start[0]][start[1]] = true;
+        int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+		
+        while (!queue.isEmpty()) {
+            int[] peek = queue.poll();
+            
+            for (int[] diff : delta) {
+                int[] next = Arrays.copyOf(peek, peek.length);
+                
+                while (isValid(maze, next, diff)) {
+                    next[0] += diff[0];
+                    next[1] += diff[1];
+                }
+                
+                if (next[0] == destination[0] && next[1] == destination[1]) {
+                    return true;
+                }
+                
+                if (!visited[next[0]][next[1]]) {
+                    queue.offer(next);
+                    visited[next[0]][next[1]] = true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean isValid(int[][] maze, int[] start, int[] diff) {
+        int i = start[0] + diff[0];
+        int j = start[1] + diff[1];
+        
+        return i >= 0 && i < maze.length && j >= 0 && j < maze[0].length && maze[i][j] == 0;
+    }
+}
+
+public class Solution {
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 		
         return dfs(maze, start, destination, visited, delta);
     }
@@ -21,14 +66,14 @@ public class Solution {
         visited[i][j] = true;
         
         for (int[] diff : delta) {
-            int[] nextStart = Arrays.copyOf(start, start.length);
+            int[] next = Arrays.copyOf(start, start.length);
             
-            while (isValid(maze, nextStart, diff)) {
-                nextStart[0] += diff[0];
-                nextStart[1] += diff[1];
+            while (isValid(maze, next, diff)) {
+                next[0] += diff[0];
+                next[1] += diff[1];
             }
             
-            if (dfs(maze, nextStart, destination, visited, delta)) {
+            if (dfs(maze, next, destination, visited, delta)) {
                 return true;
             }
         }
