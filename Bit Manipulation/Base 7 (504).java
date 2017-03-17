fc80ab5ed731bbd96b1926c1b02f1c3fee51f007
Jason51122 <jason51122@users.noreply.github.com>
2017-03-17 13:56:23 -0700

    Update Base 7 (504).java

--- Bit Manipulation/Base 7 (504).java
+++ Bit Manipulation/Base 7 (504).java
@@ -1,5 +1,17 @@
 public class Solution {
     public String convertToBase7(int num) {
+        if (num < 0) {
+            return '-' + convertToBase7(-num);
+        } else if (num < 7) {
+            return num + "";
+        } else {
+            return convertToBase7(num / 7) + num % 7;
+        }
+    }
+}
+
+public class Solution {
+    public String convertToBase7(int num) {
         if (num == 0) {
             return "0";
         }
