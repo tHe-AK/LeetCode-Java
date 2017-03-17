36e35821185320d9d19e68b3a2df4550430eb21c
Jason51122 <jason51122@users.noreply.github.com>
2017-03-16 20:59:53 -0700

    Update The Maze III (499).java

--- Backtracking/Maze/The Maze III (499).java
+++ Backtracking/Maze/The Maze III (499).java
@@ -58,3 +58,77 @@ public class Solution {
         return x >= 0 && x < maze.length && y >= 0 && y < maze[0].length && maze[x][y] == 0;
     }
 }
+
+class Point {
+    public int i;
+    public int j;
+    public int step;
+    public String path;
+    
+    public Point(int i, int j, int step, String path) {
+        this.i = i;
+        this.j = j;
+        this.step = step;
+        this.path = path;
+    }
+}
+
+public class Solution {
+    public String findShortestWay(int[][] maze, int[] ball, int[] hole) {
+        String[] dir = { "u", "d", "l", "r" };
+        int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+        String result = null;
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(new Point(ball[0], ball[1], 0, ""));
+        int[][] rec = new int[maze.length][maze[0].length];
+
+        for (int i = 0; i < rec.length; i++) {
+            Arrays.fill(rec[i], Integer.MAX_VALUE);
+        }
+	
+	    rec[ball[0]][ball[1]] = 0;
+        
+        while (!queue.isEmpty()) {
+            Point peek = queue.poll();
+            
+            for (int k = 0; k < delta.length; k++) {
+                int x = peek.i;
+                int y = peek.j;
+                int step = peek.step;
+                String path = peek.path + dir[k];
+    
+                while (isValid(maze, x, y, delta[k])) {
+                    step++;
+                    x += delta[k][0];
+                    y += delta[k][1];
+                            
+                    if (x == hole[0] && y == hole[1] && step <= rec[x][y]) {
+                        if (step < rec[x][y]) {
+                            rec[x][y] = step;
+                            result = path;
+                        } else if (path.compareTo(result) < 0) {
+                            result = path;
+                        }
+                        
+                        break;
+                    }
+                    
+                    rec[x][y] = Math.min(rec[x][y], step);
+                }
+                
+                if (step > peek.step && step <= rec[hole[0]][hole[1]] && step <= rec[x][y]) {
+                    queue.offer(new Point(x, y, step, path));
+                }
+            }
+        }
+        
+        return result == null ? "impossible" : result;
+    }
+    
+    private boolean isValid(int[][] maze, int i, int j, int[] diff) {
+        int x = i + diff[0];
+        int y = j + diff[1];
+        
+        return x >= 0 && x < maze.length && y >= 0 && y < maze[0].length && maze[x][y] == 0;
+    }
+}
