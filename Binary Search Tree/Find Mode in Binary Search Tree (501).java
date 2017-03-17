6c25168bede05fa5197f42dbaa226db7f9ff9898
Jason51122 <jason51122@users.noreply.github.com>
2017-03-16 23:35:11 -0700

    Update Find Mode in Binary Search Tree (501).java

--- Binary Search Tree/Find Mode in Binary Search Tree (501).java
+++ Binary Search Tree/Find Mode in Binary Search Tree (501).java
@@ -9,6 +9,57 @@
  */
 public class Solution {
     public int[] findMode(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        
+        inorder(root, new Integer[1], new int[1], new int[1], list);
+        
+        int[] result = new int[list.size()];
+        
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+        
+        return result;
+    }
+    
+    private void inorder(TreeNode root, Integer[] pre, int[] count, int[] max, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        
+        inorder(root.left, pre, count, max, list);
+        
+        if (pre[0] == null || root.val != pre[0]) {
+            count[0] = 1;
+        } else {
+            count[0]++;
+        }
+        
+        if (count[0] == max[0]) {
+            list.add(root.val);
+        } else if (count[0] > max[0]) {
+            list.clear();
+            list.add(root.val);
+            max[0] = count[0];
+        }
+            
+        pre[0] = root.val;
+        
+        inorder(root.right, pre, count, max, list);
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int[] findMode(TreeNode root) {
         Map<Integer, Integer> map = new HashMap<>();
         int[] max = new int[1];
         List<Integer> list = new ArrayList<>();
