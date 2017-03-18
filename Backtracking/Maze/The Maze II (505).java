a2a14cd577715d2c589d4c44517d8d23d085bbbe
Jason51122 <jason51122@users.noreply.github.com>
2017-03-17 22:13:03 -0700

    Update The Maze II (505).java

--- Backtracking/Maze/The Maze II (505).java
+++ Backtracking/Maze/The Maze II (505).java
@@ -45,3 +45,60 @@ public class Solution {
         return x >= 0 && x < maze.length && y >= 0 && y < maze[0].length && maze[x][y] == 0;
     }
 }
+
+class Point {
+    public int i;
+    public int j;
+    public int step;
+
+    public Point(int i, int j, int step) {
+        this.i = i;
+        this.j = j;
+        this.step = step;
+    }
+}
+
+public class Solution {
+    public int shortestDistance(int[][] maze, int[] start, int[] destination) {
+        int[][] delta = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(new Point(start[0], start[1], 0));
+        int[][] rec = new int[maze.length][maze[0].length];
+
+        for (int i = 0; i < rec.length; i++) {
+            Arrays.fill(rec[i], Integer.MAX_VALUE);
+        }
+        
+        rec[start[0]][start[1]] = 0;
+        
+        while (!queue.isEmpty()) {
+            Point peek = queue.poll();
+            
+            for (int k = 0; k < delta.length; k++) {
+                int x = peek.i;
+                int y = peek.j;
+                int step = peek.step;
+
+                while (isValid(maze, x, y, delta[k])) {
+                    x += delta[k][0];
+                    y += delta[k][1];
+                    step++;
+                }
+                
+                if (step < rec[destination[0]][destination[1]] && step < rec[x][y]) {
+                    rec[x][y] = Math.min(rec[x][y], step);
+                    queue.offer(new Point(x, y, step));
+                }
+            }
+        }
+        
+        return rec[destination[0]][destination[1]] == Integer.MAX_VALUE ? -1 : rec[destination[0]][destination[1]];
+    }
+    
+    private boolean isValid(int[][] maze, int i, int j, int[] diff) {
+        int x = i + diff[0];
+        int y = j + diff[1];
+        
+        return x >= 0 && x < maze.length && y >= 0 && y < maze[0].length && maze[x][y] == 0;
+    }
+}
