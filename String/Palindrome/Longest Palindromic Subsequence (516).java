85e36d6c80f485fa3cb1369d2e2126188f585484
Jason51122 <jason51122@users.noreply.github.com>
2017-03-18 22:37:14 -0700

    Update Longest Palindromic Subsequence (516).java

--- String/Palindrome/Longest Palindromic Subsequence (516).java
+++ String/Palindrome/Longest Palindromic Subsequence (516).java
@@ -3,6 +3,29 @@ public class Solution {
         int n = s.length();
         int[][] dp = new int[n][n];
         
+        for (int k = 1; k <= n; k++) {
+            for (int i = 0; i < n - k + 1; i++) {
+                int j = i + k - 1;
+                
+                if (i == j) {
+                    dp[i][j] = 1;
+                } else if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        return dp[0][n - 1];
+    }
+}
+
+public class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        
         for (int i = n - 1; i >= 0; i--) {
             for (int j = i; j < n; j++) {
                 if (i == j) {
