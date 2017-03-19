25cf1551cb0c7d1887d9521eaf64c3362cfc3909
Jason51122 <jason51122@users.noreply.github.com>
2017-03-18 22:06:39 -0700

    Update and rename Backtracking/Freedom Trail (514).java to DP/Freedom Trail (514).java

--- DP/Freedom Trail (514).java
+++ DP/Freedom Trail (514).java
@@ -1,5 +1,29 @@
 public class Solution {
     public int findRotateSteps(String ring, String key) {
+        int m = key.length();
+        int n = ring.length();
+        int[][] dp = new int[m + 1][n];
+        
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+                
+                for (int k = 0; k < n; k++) {
+                    if (ring.charAt(k) == key.charAt(i)) {
+                        int diff = Math.abs(j - k);
+                        int step = Math.min(diff, ring.length() - diff);
+                        dp[i][j] = Math.min(dp[i][j], step + dp[i + 1][k]);
+                    }
+                }
+            }
+        }
+        
+        return dp[0][0] + key.length();
+    }
+}
+
+public class Solution {
+    public int findRotateSteps(String ring, String key) {
         Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
         
         return dfs(ring, key, 0, 0, map) + key.length();
