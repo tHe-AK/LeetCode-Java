9f143f0874e76b2935b0c96b96831275c7919b15
Jason51122 <jason51122@users.noreply.github.com>
2017-03-18 21:55:28 -0700

    Update Freedom Trail (514).java

--- Backtracking/Freedom Trail (514).java
+++ Backtracking/Freedom Trail (514).java
@@ -1,45 +1,34 @@
 public class Solution {
     public int findRotateSteps(String ring, String key) {
-        Map<String, Map<Integer, Integer>> map = new HashMap<>();
+        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
         
-        return dfs(ring, key, 0, map) + key.length();
+        return dfs(ring, key, 0, 0, map) + key.length();
     }
     
-    private int dfs(String ring, String key, int idx, Map<String, Map<Integer, Integer>> map){
+    private int dfs(String ring, String key, int start, int idx, Map<Integer, Map<Integer, Integer>> map){
         if (idx == key.length()) {
             return 0;
         }
         
-        if (map.containsKey(ring) && map.get(ring).containsKey(idx)) {
-            return map.get(ring).get(idx);
+        if (map.containsKey(start) && map.get(start).containsKey(idx)) {
+            return map.get(start).get(idx);
         }
         
         char ch = key.charAt(idx);
-        int clockIdx = ring.indexOf(ch);
-        int antiIdx = getAntiIdx(ring, ch);
+        int result = Integer.MAX_VALUE;
         
-        int clock = clockIdx + dfs(ring.substring(clockIdx) + ring.substring(0, clockIdx), key, idx + 1, map);
-        int anti = ring.length() - antiIdx + dfs(ring.substring(antiIdx) + ring.substring(0, antiIdx), key, idx + 1, map);
-        int result = Math.min(clock, anti);
-        
-        Map<Integer, Integer> val = map.getOrDefault(ring, new HashMap<>());
+        for (int i = 0; i < ring.length(); i++) {
+            if (ring.charAt(i) == ch) {
+                int diff = Math.abs(start - i);
+                int step = Math.min(diff, ring.length() - diff);
+                result = Math.min(result, step + dfs(ring, key, i, idx + 1, map));
+            }
+        }
+
+        Map<Integer, Integer> val = map.getOrDefault(start, new HashMap<>());
         val.put(idx, result);
-        map.put(ring, val);
+        map.put(start, val);
         
         return result;
-    }
-    
-    private int getAntiIdx(String ring, char ch) {
-        if (ring.charAt(0) == ch) {
-            return 0;
-        } else {
-            for (int i = ring.length() - 1; i > 0; i--) {
-                if (ring.charAt(i) == ch) {
-                    return i;
-                }
-            }
-            
-            return -1;
-        }
     }
 }
