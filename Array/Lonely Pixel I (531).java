c2242760dc15ac9ec519c32f2f71de9dc71c58ed
Jason51122 <jason51122@users.noreply.github.com>
2017-03-19 22:56:58 -0700

    Create Lonely Pixel I (531).java

---
+++ Array/Lonely Pixel I (531).java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int findLonelyPixel(char[][] picture) {
+        int m = picture.length;
+        int n = picture[0].length;
+    
+        int[] row = new int[m];
+        int[] col = new int[n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (picture[i][j] == 'B') {
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+    
+        int count = 0;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (picture[i][j] == 'B' && row[i] == 1 && col[j] == 1) {
+                    count++;
+                }
+            }
+        }
+                    
+        return count;
+    }
+}
