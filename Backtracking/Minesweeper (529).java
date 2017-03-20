3ac8046fe87ec19f2e362e2a3439c64745e2e1aa
Jason51122 <jason51122@users.noreply.github.com>
2017-03-19 21:34:14 -0700

    Update Minesweeper (529).java

--- Backtracking/Minesweeper (529).java
+++ Backtracking/Minesweeper (529).java
@@ -53,3 +53,67 @@ public class Solution {
         return neighbors;
     }
 }
+
+public class Solution {
+    public char[][] updateBoard(char[][] board, int[] click) {
+        int m = board.length;
+        int n = board[0].length;
+        int row = click[0];
+        int col = click[1];
+        
+        if (board[row][col] == 'M') {
+            board[row][col] = 'X';
+        } else {
+            Queue<int[]> queue = new LinkedList<>();
+            board[row][col] = 'B';
+            queue.add(click);
+            
+            while (!queue.isEmpty()) {
+                int[] peek = queue.poll();
+                row = peek[0];
+                col = peek[1];
+                int count = 0;
+                
+                List<int[]> neighbors = getNeighbors(board, row, col);
+                    
+                for (int[] neighbor : neighbors) {
+                    if (board[neighbor[0]][neighbor[1]] == 'M') {
+                        count++;
+                    }
+                }
+                    
+                if (count > 0) {
+                    board[row][col] = (char) ('0' + count);
+                } else {
+                    for (int[] neighbor : neighbors) {
+                        if (board[neighbor[0]][neighbor[1]] == 'E') {
+                            board[neighbor[0]][neighbor[1]] = 'B';
+                            queue.offer(neighbor);
+                        }
+                    }
+                }
+            }
+        }
+        
+        return board;
+    }
+    
+    private List<int[]> getNeighbors(char[][] board, int row, int col) {
+        List<int[]> neighbors = new ArrayList<>();
+        
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i != 0 || j != 0) {
+                    int x = row + i;
+                    int y = col + j;
+                            
+                    if (x >= 0 && x < board.length && y >= 0 && y < board[0].length) {
+                        neighbors.add(new int[] { x, y });
+                    }
+                }
+            }
+        }
+        
+        return neighbors;
+    }
+}
