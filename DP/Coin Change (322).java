f67bbf354e488aa150c4b3d7501c67ddf291cdb7
Jason51122 <jason51122@users.noreply.github.com>
2017-03-24 16:45:02 -0700

    Update Coin Change (322).java

--- DP/Coin Change (322).java
+++ DP/Coin Change (322).java
@@ -1,19 +1,17 @@
 public class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] rec = new int[amount + 1];
-        Arrays.fill(rec, amount + 1);
-        rec[0] = 0;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
         
         for (int i = 1; i <= amount; i++) {
             for (int coin : coins) {
-                int j = i - coin;
-                
-                if (j >= 0 && rec[j] <= amount) {
-                    rec[i] = Math.min(rec[i], rec[j] + 1);
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
         
-        return rec[amount] <= amount ? rec[amount] : -1;
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
