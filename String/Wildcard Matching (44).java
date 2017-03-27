86a5d9322f56e63f703ec3d55013a4dbdb121dc1
Jason51122 <jason51122@users.noreply.github.com>
2017-03-26 22:31:34 -0700

    Update Wildcard Matching (44).java

--- String/Wildcard Matching (44).java
+++ String/Wildcard Matching (44).java
@@ -56,3 +56,35 @@ public class Solution {
         return j == p.length();
     }
 }
+
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        return dfs(s, 0, p, 0);
+    }
+    
+    private boolean dfs(String s, int i, String p, int j) {
+        if (j == p.length()) {
+            return i == s.length();
+        } else if (p.charAt(j) == '*') {
+            if (dfs(s, i, p, j + 1)) {
+                return true;
+            }
+            
+            while (i < s.length()) {
+                if (dfs(s, i + 1, p, j + 1)) {
+                    return true;
+                }
+                
+                i++;
+            }
+            
+            return false;
+        } else {
+            return i < s.length() && isEqual(s, i, p, j) && dfs(s, i + 1, p, j + 1);
+        }
+    }
+    
+    private boolean isEqual(String s, int i, String p, int j) {
+        return s.charAt(i) == p.charAt(j) || p.charAt(j) == '?';
+    }
+}
