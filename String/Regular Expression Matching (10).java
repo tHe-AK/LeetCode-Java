3f52eef76d1145b3c7d6cbae76dd843145fee2d3
Jason51122 <jason51122@users.noreply.github.com>
2017-03-26 22:06:21 -0700

    Update Regular Expression Matching (10).java

--- String/Regular Expression Matching (10).java
+++ String/Regular Expression Matching (10).java
@@ -2,32 +2,32 @@ public class Solution {
     public boolean isMatch(String s, String p) {
         int m = s.length();
         int n = p.length();
-        boolean[][] rec = new boolean[m + 1][n + 1];
-        rec[0][0] = true;
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
         
         for (int j = 1; j <= n; j++) {
             if (p.charAt(j - 1) == '*') {
-                rec[0][j] = rec[0][j - 2];
+                dp[0][j] = dp[0][j - 2];
             }
         }
         
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                char c = p.charAt(j - 1);
+                char ch = p.charAt(j - 1);
                 
-                if (c == s.charAt(i - 1) || c == '.') {
-                    rec[i][j] = rec[i - 1][j - 1];
-                } else if (c == '*') {
+                if (ch == s.charAt(i - 1) || ch == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (ch == '*') {
                     if (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') {
-                        rec[i][j] = rec[i][j - 2] || rec[i - 1][j];
+                        dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
                     } else {
-                        rec[i][j] = rec[i][j - 2];
+                        dp[i][j] = dp[i][j - 2];
                     }
                 }
             }
         }
         
-        return rec[m][n];
+        return dp[m][n];
     }
 }
 
@@ -39,13 +39,12 @@ public class Solution {
     private boolean dfs(String s, int i, String p, int j) {
         if (j == p.length()) {
             return i == s.length();
-        }
-        else if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+        } else if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
             if (dfs(s, i, p, j + 2)) {
                 return true;
             }
             
-            while (i < s.length() && equal(s, i, p, j)) {
+            while (i < s.length() && isEqual(s, i, p, j)) {
                 if (dfs(s, i + 1, p, j + 2)) {
                     return true;
                 }
@@ -54,13 +53,12 @@ public class Solution {
             }
             
             return false;
-        }
-        else {
-            return i < s.length() && equal(s, i, p, j) && dfs(s, i + 1, p, j + 1);
+        } else {
+            return i < s.length() && isEqual(s, i, p, j) && dfs(s, i + 1, p, j + 1);
         }
     }
     
-    private boolean equal(String s, int i, String p, int j) {
+    private boolean isEqual(String s, int i, String p, int j) {
         return p.charAt(j) == '.' || s.charAt(i) == p.charAt(j);
     }
 }
