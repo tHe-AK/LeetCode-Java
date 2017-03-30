4afa9950c154f1a27072522d26371872e82c97f2
Jason51122 <jason51122@users.noreply.github.com>
2017-03-30 13:58:51 -0700

    Update Boundary of Binary Tree (545).java

--- Binary Tree/Boundary of Binary Tree (545).java
+++ Binary Tree/Boundary of Binary Tree (545).java
@@ -72,3 +72,80 @@ public class Solution {
         return t.left == null && t.right == null;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        List<Integer> left_boundary = new LinkedList<>();
+        List<Integer> right_boundary = new LinkedList<>();
+        List<Integer> leaves = new LinkedList<>();
+        
+        preorder(root, left_boundary, right_boundary, leaves, 0);
+        
+        left_boundary.addAll(leaves);
+        left_boundary.addAll(right_boundary);
+        
+        return left_boundary;
+    }
+
+    private void preorder(TreeNode cur, List<Integer> left_boundary, List<Integer> right_boundary, List<Integer> leaves, int flag) {
+        if (cur == null) {
+            return;
+        }
+        
+        if (isRoot(flag) || isLeftBoundary(flag)) {
+            left_boundary.add(cur.val);
+        } else if (isRightBoundary(flag)) {
+            right_boundary.add(0, cur.val);
+        } else if (isLeaf(cur)) {
+            leaves.add(cur.val);
+        }
+        
+        preorder(cur.left, left_boundary, right_boundary, leaves, leftChildFlag(cur, flag));
+        preorder(cur.right, left_boundary, right_boundary, leaves, rightChildFlag(cur, flag));
+    }
+    
+        private boolean isLeaf(TreeNode cur) {
+        return cur.left == null && cur.right == null;
+    }
+
+    private boolean isRightBoundary(int flag) {
+        return flag == 2;
+    }
+
+    private boolean isLeftBoundary(int flag) {
+        return flag == 1;
+    }
+
+    private boolean isRoot(int flag) {
+        return flag == 0;
+    }
+
+    private int leftChildFlag(TreeNode cur, int flag) {
+        if (isLeftBoundary(flag) || isRoot(flag)) {
+            return 1;
+        } else if (isRightBoundary(flag) && cur.right == null) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+
+    private int rightChildFlag(TreeNode cur, int flag) {
+        if (isRightBoundary(flag) || isRoot(flag)) {
+            return 2;
+        } else if (isLeftBoundary(flag) && cur.left == null) {
+            return 1;
+        } else {
+            return 3;
+        }
+    }
+}
