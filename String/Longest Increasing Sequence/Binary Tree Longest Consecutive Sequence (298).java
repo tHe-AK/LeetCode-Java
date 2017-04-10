6aab18327e8041e590ea83f33210527bd2d421d2
Jason51122 <jason51122@users.noreply.github.com>
2017-04-09 22:54:18 -0700

    Update Binary Tree Longest Consecutive Sequence (298).java

--- String/Longest Increasing Sequence/Binary Tree Longest Consecutive Sequence (298).java
+++ String/Longest Increasing Sequence/Binary Tree Longest Consecutive Sequence (298).java
@@ -9,6 +9,44 @@
  */
 public class Solution {
     public int longestConsecutive(TreeNode root) {
+        int[] max = new int[1];
+        postorder(root, max);
+        return max[0];
+    }
+    
+    private int postorder(TreeNode root, int[] max) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int inc = 1;
+        int left = postorder(root.left, max);
+        int right = postorder(root.right, max);
+        
+        if (root.left != null && root.val == root.left.val - 1) {
+            inc = left + 1;
+        }
+        
+        if (root.right != null && root.val == root.right.val - 1) {
+            inc = Math.max(inc, right + 1);
+        }
+        
+        max[0] = Math.max(max[0], inc);
+        return inc;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int longestConsecutive(TreeNode root) {
         return longestConsecutive(root, null, 0);
     }
 
