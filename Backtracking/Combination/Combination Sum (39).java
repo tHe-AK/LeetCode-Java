8b263f202ad4ab1f55cf274b7c5e0b09cbc7be2b
Jason51122 <jason51122@users.noreply.github.com>
2017-04-22 16:09:14 -0700

    Update Combination Sum (39).java

--- Backtracking/Combination/Combination Sum (39).java
+++ Backtracking/Combination/Combination Sum (39).java
@@ -1,6 +1,5 @@
 public class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         
         dfs(candidates, target, 0, new ArrayList<Integer>(), result);
@@ -14,10 +13,36 @@ public class Solution {
             return;
         }
         
-        for (int i = start; i < candidates.length && target >= candidates[i]; i++) {
-            curr.add(candidates[i]);
-            dfs(candidates, target - candidates[i], i, curr, result);
-            curr.remove(curr.size() - 1);
+        for (int i = start; i < candidates.length; i++) {
+            if (candidates[i] <= target) {
+                curr.add(candidates[i]);
+                dfs(candidates, target - candidates[i], i, curr, result);
+                curr.remove(curr.size() - 1);
+            }
         }
+    }
+}
+
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>>[] rec = new List[target + 1];
+        
+        for (int i = 0; i < rec.length; i++) {
+            rec[i] = new ArrayList<>();
+        }
+        
+        rec[0].add(new ArrayList<>());
+        
+        for (int candidate : candidates) {
+            for (int i = candidate; i < rec.length; i++) {
+                for (List<Integer> list : rec[i - candidate]) {
+                    List<Integer> next = new ArrayList<>(list);
+                    next.add(candidate);
+                    rec[i].add(next);
+                }
+            }
+        }
+        
+        return rec[target];
     }
 }
