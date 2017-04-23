0904d91f2c1417e4d6498c6662e090662e3ffe5e
Jason51122 <jason51122@users.noreply.github.com>
2017-04-22 21:43:13 -0700

    Update Array Partitioning I (561).java

--- Array/Array Partitioning I (561).java
+++ Array/Array Partitioning I (561).java
@@ -10,3 +10,29 @@ public class Solution {
         return sum;
     }
 }
+
+public class Solution {
+    public int arrayPairSum(int[] nums) {
+        int max = 10000;
+        int[] rec = new int[max * 2 + 1];
+        int sum = 0;
+        boolean flag = true;
+        
+        for (int num : nums) {
+            rec[num + max]++;
+        }
+        
+        for (int i = 0; i < rec.length; i++) {
+            if (rec[i] > 0) {
+                sum += (i - max) * (rec[i] / 2);
+                
+                if (rec[i] % 2 == 1) {
+                    sum += flag ? i - max : 0;
+                    flag = !flag;
+                }
+            }
+        }
+        
+        return sum;
+    }
+}
