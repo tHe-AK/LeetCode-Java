a35972c1a4d30086edabfa7961f316d8e92d083f
Jason51122 <jason51122@users.noreply.github.com>
2017-04-24 22:29:37 -0700

    Update Product of Array Except Self (238).java

--- Math/Product of Array Except Self (238).java
+++ Math/Product of Array Except Self (238).java
@@ -1,19 +1,24 @@
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
         int len = nums.length;
-        int[] rec = new int[len];
-        rec[0] = 1;
-        int val = 1;
+        int[] res = new int[len];
+        
+        if (len == 0) {
+            return res;
+        }
+        
+        res[0] = 1;
+        int product = 1;
         
         for (int i = 1; i < len; i++) {
-            rec[i] = rec[i - 1] * nums[i - 1];
+            res[i] = nums[i - 1] * res[i - 1];
         }
         
-        for (int i = len - 1; i >= 0; i--) {
-            rec[i] *= val;
-            val *= nums[i];
+        for (int i = len - 2; i >= 0; i--) {
+            product *= nums[i + 1];
+            res[i] *= product;
         }
         
-        return rec;
+        return res;
     }
 }
