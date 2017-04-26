f70e3c3348cb09374f81d5212eef1126e0a93721
Jason51122 <jason51122@users.noreply.github.com>
2017-04-25 22:31:05 -0700

    Update Climbing Stairs (70).java

--- DP/Climbing Stairs (70).java
+++ DP/Climbing Stairs (70).java
@@ -1,14 +1,28 @@
 public class Solution {
     public int climbStairs(int n) {
-        if (n <= 2) {
-            return n;
+        int[] dp = new int[n == 0 ? 2 : n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n];
+    }
+}
+
+public class Solution {
+    public int climbStairs(int n) {
+        if (n <= 1) {
+            return 1;
         }
         
         int pre1 = 1;
-        int pre2 = 2;
-        int curr = 3;
+        int pre2 = 1;
+        int curr = 0;
         
-        for (int i = 3; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             curr = pre1 + pre2;
             pre1 = pre2;
             pre2 = curr;
