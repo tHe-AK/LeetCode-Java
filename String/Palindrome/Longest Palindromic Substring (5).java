1d4bd01cea3cffc3595ec9c55325ced8d0b5a7fa
Jason51122 <jason51122@users.noreply.github.com>
2017-04-27 21:25:24 -0700

    Update Longest Palindromic Substring (5).java

--- String/Palindrome/Longest Palindromic Substring (5).java
+++ String/Palindrome/Longest Palindromic Substring (5).java
@@ -1,5 +1,35 @@
 public class Solution {
     public String longestPalindrome(String s) {
+        String res = "";
+        
+        for (int i = 0; i < s.length(); i++) {
+            String candidate1 = expandAroundCenter(s, i, i);
+            String candidate2 = expandAroundCenter(s, i, i + 1);
+            
+            if (candidate1.length() > res.length()) {
+                res = candidate1;
+            }
+            
+            if (candidate2.length() > res.length()) {
+                res = candidate2;
+            }
+        }
+        
+        return res;
+    }
+    
+    private String expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        
+        return right - left - 1 > 0 ? s.substring(left + 1, right) : "";
+    }
+}
+
+public class Solution {
+    public String longestPalindrome(String s) {
         int start = 0;
         int end = 0;
         
