8c5e0fdda46bdcfef9c9ca0d46de8147a04d9c0b
Jason51122 <jason51122@users.noreply.github.com>
2017-04-29 21:54:47 -0700

    Update Maximal Square (221).java

--- DP/Maximal Square (221).java
+++ DP/Maximal Square (221).java
@@ -11,7 +11,7 @@ public class Solution {
         
         for (int i = 1; i <= row; i++) {
             for (int j = 1; j <= col; j++) {
-                if (matrix[i][j] == '1') {
+                if (matrix[i - 1][j - 1] == '1') {
                     dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
                     max = Math.max(max, dp[i][j]);
                 }
