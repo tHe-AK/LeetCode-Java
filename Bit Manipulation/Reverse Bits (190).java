8d9f55b2b5dc3c846bb096e8ff7b605d138e3a91
Jason51122 <jason51122@users.noreply.github.com>
2017-05-04 00:12:58 -0700

    Update Reverse Bits (190).java

--- Bit Manipulation/Reverse Bits (190).java
+++ Bit Manipulation/Reverse Bits (190).java
@@ -5,7 +5,7 @@ public class Solution {
         
         for (int i = 0; i < 32; i++) {
             m = (m << 1) + (n & 1);
-            n = n >>> 1;
+            n >>>= 1;
         }
         
         return m;
