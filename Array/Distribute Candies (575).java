4c4f02661ccb3b052e5168a2f7371f422d970003
Jason51122 <jason51122@users.noreply.github.com>
2017-05-07 17:00:33 -0700

    Create Distribute Candies (575).java

---
+++ Array/Distribute Candies (575).java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int distributeCandies(int[] candies) {
+        Set<Integer> set = new HashSet<>();
+        
+        for (int candy: candies) {
+            set.add(candy);
+        }
+        
+        return Math.min(set.size(), candies.length / 2);
+    }
+}
