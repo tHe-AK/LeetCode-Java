f0f80e1a6bc230b256c370b46d3f37f4523cd61a
Jason51122 <jason51122@users.noreply.github.com>
2017-05-09 01:00:50 -0700

    Update Reverse Linked List (206).java

--- Linked List/Reverse Linked List (206).java
+++ Linked List/Reverse Linked List (206).java
@@ -42,3 +42,35 @@ public class Solution {
         return newHead;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        
+        Stack<ListNode> stack = new Stack<>();
+        
+        while (head != null) {
+            stack.push(head);
+            head = head.next;
+        }
+        
+        head = stack.peek();
+        
+        while (!stack.isEmpty()) {
+            ListNode peek = stack.pop();
+            peek.next = stack.isEmpty() ? null : stack.peek();
+        }
+        
+        return head;
+    }
+}
