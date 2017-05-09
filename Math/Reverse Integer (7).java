dd088114fe2966b8991f904b9c1d4b90b8ca613e
Jason51122 <jason51122@users.noreply.github.com>
2017-05-08 21:23:05 -0700

    Update Reverse Integer (7).java

--- Math/Reverse Integer (7).java
+++ Math/Reverse Integer (7).java
@@ -1,17 +1,17 @@
 public class Solution {
     public int reverse(int x) {
         long y = 0;
-
+        
         while (x != 0) {
-            int digit = x % 10;
-            x /= 10;
-            y = y * 10 + digit;
+            y = y * 10 + x % 10;
             
             if (y > Integer.MAX_VALUE || y < Integer.MIN_VALUE) {
                 return 0;
             }
+            
+            x /= 10;
         }
-
+        
         return (int) y;
     }
 }
