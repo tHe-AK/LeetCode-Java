b2bff144b381270ecd98543887c479e6bc5f365c
Jason51122 <jason51122@users.noreply.github.com>
2017-05-09 00:32:20 -0700

    Update Populating Next Right Pointers in Each Node II (117).java

--- Binary Tree/Populating Next Right Pointers in Each Node II (117).java
+++ Binary Tree/Populating Next Right Pointers in Each Node II (117).java
@@ -8,6 +8,48 @@
  */
 public class Solution {
     public void connect(TreeLinkNode root) {
+        if (root == null) {
+            return;
+        }
+        
+        Queue<TreeLinkNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            TreeLinkNode pre = null;
+            
+            for (int i = 0; i < size; i++) {
+                TreeLinkNode curr = queue.poll();
+                
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+                
+                if (pre != null) {
+                    pre.next = curr;
+                }
+                
+                pre = curr;
+            }
+        }
+    }
+}
+
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
         while (root != null) {
             TreeLinkNode dummy = new TreeLinkNode(0);
             TreeLinkNode curr = dummy;
