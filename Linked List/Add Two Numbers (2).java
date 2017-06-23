f7acbf5706b027adfac72db9b0f4f1e9d77649dc
Jason51122 <jason51122@users.noreply.github.com>
2017-06-22 22:04:07 -0700

    Update Add Two Numbers (2).java

--- Linked List/Add Two Numbers (2).java
+++ Linked List/Add Two Numbers (2).java
@@ -14,20 +14,20 @@ public class Solution {
         
         while (l1 != null || l2 != null || carry > 0) {
             int sum = carry;
-            sum += (l1 != null) ? l1.val : 0;
-            sum += (l2 != null) ? l2.val : 0;
-
-            carry = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
             
             if (l1 != null) {
+                sum += l1.val;
                 l1 = l1.next;
             }
             
             if (l2 != null) {
+                sum += l2.val;
                 l2 = l2.next;
             }
+
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
         }
         
         return dummy.next;
