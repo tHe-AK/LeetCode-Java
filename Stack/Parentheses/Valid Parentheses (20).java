0eb2b078ca5e5fb34fdf423ca5ef26367b515591
Jason51122 <jason51122@users.noreply.github.com>
2017-06-26 20:57:15 -0700

    Update Valid Parentheses (20).java

--- Stack/Parentheses/Valid Parentheses (20).java
+++ Stack/Parentheses/Valid Parentheses (20).java
@@ -1,6 +1,6 @@
 public class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<Character>();
+        Stack<Character> stack = new Stack<>();
         
     	for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
