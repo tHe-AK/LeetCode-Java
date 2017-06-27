a7a630d4df45f1f8bf59863cdebcf6899f406e3b
Jason51122 <jason51122@users.noreply.github.com>
2017-06-26 21:28:21 -0700

    Update Generate Parentheses (22).java

--- Stack/Parentheses/Generate Parentheses (22).java
+++ Stack/Parentheses/Generate Parentheses (22).java
@@ -2,23 +2,23 @@ public class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
         
-        helper(n, n, "", result);
+        dfs(n, n, "", result);
         
         return result;
     }
     
-    private void helper(int left, int right, String pre, List<String> result) {
+    private void dfs(int left, int right, String pre, List<String> result) {
         if (right == 0) {
             result.add(pre);
             return;
         }
         
         if (left > 0) {
-            helper(left - 1, right, pre + '(', result);
+            dfs(left - 1, right, pre + '(', result);
         }
         
         if (right > left) {
-            helper(left, right - 1, pre + ')', result);
+            dfs(left, right - 1, pre + ')', result);
         }
     }
 }
