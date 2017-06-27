016f0111e5402474e33db5e278d7272ff908becc
Jason51122 <jason51122@users.noreply.github.com>
2017-06-27 00:16:23 -0700

    Create Course Schedule III (630).java

---
+++ Heap/Course Schedule III (630).java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a, b) -> Integer.compare(a[1], b[1]));
+        Queue<Integer> queue = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+        
+        int time = 0;
+        
+        for (int[] course : courses) {
+            if (time + course[0] <= course[1]) {
+                time += course[0];
+                queue.offer(course[0]);
+            } else {
+                if (!queue.isEmpty() && course[0] < queue.peek()) {
+                    time += course[0] - queue.poll();
+                    queue.offer(course[0]);
+                }
+            }
+        }
+        
+        return queue.size();
+    }
+}
