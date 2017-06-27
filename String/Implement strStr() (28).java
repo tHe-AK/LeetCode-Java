f5bb34dfb7ed882d74dc1adf8afd8ade800f0807
Jason51122 <jason51122@users.noreply.github.com>
2017-06-26 22:25:29 -0700

    Update Implement strStr() (28).java

--- String/Implement strStr() (28).java
+++ String/Implement strStr() (28).java
@@ -8,16 +8,14 @@ public class Solution {
         int len2 = needle.length();
         
         for (int i = 0; i < len1 - len2 + 1; i++) {
-            int j = 0;
-            
-            for (j = 0; j < len2; j++) {
-                if (haystack.charAt(i + j) != needle.charAt(j)) {
-                    break;
+            for (int j = 0; j <= len2; j++) {
+                if (j == len2) {
+                    return i;
+                } else {
+                    if (haystack.charAt(i + j) != needle.charAt(j)) {
+                        break;
+                    }
                 }
-            }
-            
-            if (j == len2) {
-                return i;
             }
         }
         
