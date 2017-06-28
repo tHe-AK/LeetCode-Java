fda322dfa6ac31b4b0f1b970387df43d4be9e6c2
Jason51122 <jason51122@users.noreply.github.com>
2017-06-28 00:17:42 -0700

    Update Sudoku Solver (37).java

--- Backtracking/Sudoku Solver (37).java
+++ Backtracking/Sudoku Solver (37).java
@@ -1,5 +1,54 @@
 public class Solution {
     public void solveSudoku(char[][] board) {
+        int N = 9;
+        boolean[][] row = new boolean[N][N];
+        boolean[][] col = new boolean[N][N];
+        boolean[][] sq = new boolean[N][N];
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] != '.') {
+                    int val = board[i][j] - '1';
+                    int k = i / 3 * 3 + j / 3;
+                    row[i][val] = col[j][val] = sq[k][val] = true;
+                }
+            }
+        }
+        
+        dfs(board, row, col, sq);
+    }
+    
+    private boolean dfs(char[][] board, boolean[][] row, boolean[][] col, boolean[][] sq) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    int k = i / 3 * 3 + j / 3;
+
+                    for (int val = 0; val < 9; val++) {
+                        if (!row[i][val] && !col[j][val] && !sq[k][val]) {
+                            board[i][j] = (char)('1' + val);
+                            row[i][val] = col[j][val] = sq[k][val] = true;
+                            
+                            if (dfs(board, row, col, sq)) {
+                                return true;
+                            }
+                            
+                            board[i][j] = '.';
+                            row[i][val] = col[j][val] = sq[k][val] = false;
+                        }
+                    }
+                    
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
+
+public class Solution {
+    public void solveSudoku(char[][] board) {
         dfs(board);
     }
     
