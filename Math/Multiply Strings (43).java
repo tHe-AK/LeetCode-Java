4f4453200e97cb6b1565eab75cb9a523c7a7f1af
Jason51122 <jason51122@users.noreply.github.com>
2017-06-29 01:08:17 -0700

    Update Multiply Strings (43).java

--- Math/Multiply Strings (43).java
+++ Math/Multiply Strings (43).java
@@ -6,21 +6,24 @@ public class Solution {
         
         for (int i = len1 - 1; i >= 0; i--) {
             for (int j = len2 - 1; j >= 0; j--) {
-                int index1 = i + j;
-                int index2 = i + j + 1;
-                int sum = (num1.charAt(i) - '0') * (num2.charAt(j) - '0') + rec[index2];
+                int idx1 = i + j;
+                int idx2 = i + j + 1;
+                int sum = (num1.charAt(i) - '0') * (num2.charAt(j) - '0') + rec[idx2];
     
-                rec[index1] += sum / 10;
-                rec[index2] = sum % 10;
+                rec[idx1] += sum / 10;
+                rec[idx2] = sum % 10;
             }
         }
         
         StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < rec.length; i++) {
-            if (i == rec.length - 1 || sb.length() != 0 || rec[i] != 0) {
-                sb.append(rec[i]);
-            }
+        int i = 0;
+        
+        while (i < rec.length - 1 && rec[i] == 0) {
+            i++;
+        }
+        
+        while (i < rec.length) {
+            sb.append(rec[i++]);
         }
         
         return sb.toString();
