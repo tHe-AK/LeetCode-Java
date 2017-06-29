49f0c2ab7668cc8c5f3e499c8b649b9fa0492c79
Jason51122 <jason51122@users.noreply.github.com>
2017-06-29 01:13:25 -0700

    Update Wildcard Matching (44).java

--- String/Wildcard Matching (44).java
+++ String/Wildcard Matching (44).java
@@ -5,8 +5,10 @@ public class Solution {
         boolean[][] dp = new boolean[m + 1][n + 1];
         dp[0][0] = true;
         
-        for (int j = 1; j <= n && p.charAt(j - 1) == '*'; j++){
-            dp[0][j] = true;
+        for (int j = 1; j <= n; j++){
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
         }
         
         for (int i = 1; i <= m; i++) {
