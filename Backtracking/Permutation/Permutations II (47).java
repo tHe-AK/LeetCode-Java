653d4cba5d3bb1f936ab87de51c15cc167508c5b
Jason51122 <jason51122@users.noreply.github.com>
2017-06-29 02:07:36 -0700

    Update Permutations II (47).java

--- Backtracking/Permutation/Permutations II (47).java
+++ Backtracking/Permutation/Permutations II (47).java
@@ -3,23 +3,23 @@ public class Solution {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
 
-        helper(nums, new boolean[nums.length], new ArrayList<Integer>(), result);
+        dfs(nums, new boolean[nums.length], new ArrayList<Integer>(), result);
         
         return result;
     }
     
-    private void helper(int[] nums, boolean[] rec, List<Integer> curr, List<List<Integer>> result) {
+    private void dfs(int[] nums, boolean[] visited, List<Integer> curr, List<List<Integer>> result) {
         if (curr.size() == nums.length) {
             result.add(new ArrayList<Integer>(curr));
             return;
         }
         
         for (int i = 0; i < nums.length; i++) {
-            if (!rec[i] && (i == 0 || nums[i] != nums[i - 1] || rec[i - 1])) {
-                rec[i] = true;
+            if (!visited[i] && (i == 0 || nums[i] != nums[i - 1] || visited[i - 1])) {
+                visited[i] = true;
                 curr.add(nums[i]);
-                helper(nums, rec, curr, result);
-                rec[i] = false;
+                dfs(nums, visited, curr, result);
+                visited[i] = false;
                 curr.remove(curr.size() - 1);
             }
         }
