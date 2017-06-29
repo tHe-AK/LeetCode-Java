8b9cf6c37b9b872a90314db9f3640b1dd2ff00c2
Jason51122 <jason51122@users.noreply.github.com>
2017-06-29 00:10:43 -0700

    Update Trapping Rain Water (42).java

--- Heap/Trapping Rain Water (42).java
+++ Heap/Trapping Rain Water (42).java
@@ -1,28 +1,22 @@
 public class Solution {
     public int trap(int[] height) {
-        int low = 0;
-        int high = height.length - 1;
+        int len = height.length;
         int leftMax = 0;
         int rightMax = 0;
+        int[] left = new int[len];
         int sum = 0;
         
-        while (low <= high) {
-            if (leftMax <= rightMax) {
-                int curr = height[low++];
-                
-                if (leftMax > curr) {
-                    sum += leftMax - curr;
-                } else {
-                    leftMax = curr;
-                }
-            } else {
-                int curr = height[high--];
-                
-                if (rightMax > curr) {
-                    sum += rightMax - curr;
-                } else {
-                    rightMax = curr;
-                }
+        for (int i = 0; i < len; i++) {
+            left[i] = leftMax;
+            leftMax = Math.max(leftMax, height[i]);
+        }
+        
+        for (int i = len - 1; i >= 0; i--) {
+            int min = Math.min(left[i], rightMax);
+            rightMax = Math.max(rightMax, height[i]);
+            
+            if (min > height[i]) {
+                sum += min - height[i];
             }
         }
         
