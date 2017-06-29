8c9bacefd2b5839923e3d774567743d85c19839d
Jason51122 <jason51122@users.noreply.github.com>
2017-06-29 01:48:59 -0700

    Update Jump Game II (45).java

--- Backtracking/Game/Jump Game II (45).java
+++ Backtracking/Game/Jump Game II (45).java
@@ -16,3 +16,22 @@ public class Solution {
         return step;
     }
 }
+
+public class Solution {
+    public int jump(int[] nums) {
+        int len = nums.length;
+        int[] dp = new int[len];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] != Integer.MAX_VALUE && j + nums[j] >= i) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        
+        return dp[len - 1];
+    }
+}
