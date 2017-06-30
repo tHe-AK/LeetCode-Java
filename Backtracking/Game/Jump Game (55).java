da05f36665a155d4fee6e938f5adc0770ee578c9
Jason51122 <jason51122@users.noreply.github.com>
2017-06-29 23:10:58 -0700

    Update Jump Game (55).java

--- Backtracking/Game/Jump Game (55).java
+++ Backtracking/Game/Jump Game (55).java
@@ -13,3 +13,22 @@ public class Solution {
         return true;
     }
 }
+
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int len = nums.length;
+        boolean[] dp = new boolean[len];
+        dp[0] = true;
+        
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && j + nums[j] >= i) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[len - 1];
+    }
+}
