91ba292adeed12d82e6de09fe9257d618027f58b
Jason51122 <jason51122@users.noreply.github.com>
2017-07-19 00:30:49 -0700

    Create Smallest Range (632).java

---
+++ Heap/Smallest Range (632).java
@@ -0,0 +1,38 @@
+public class Solution {
+    public int[] smallestRange(List<List<Integer>> nums) {
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums.get(i).get(0);
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+            pq.offer(new int[] { num, i, 0 });
+        }
+        
+        int[] res = { min, max };
+        
+        while (!pq.isEmpty()) {
+            int[] peek = pq.poll();
+            min = peek[0];
+            int i = peek[1];
+            int j = peek[2];
+            
+            if (max - min < res[1] - res[0] || min < res[0]) {
+                res[0] = min;
+                res[1] = max;
+            }
+            
+            if (j + 1 == nums.get(i).size()) {
+                break;
+            } else {
+                int next = nums.get(i).get(j + 1);
+                max = Math.max(max, next);
+                pq.offer(new int[] { next, i, j + 1 });
+            }
+        }
+        
+        return res;
+    }
+}
