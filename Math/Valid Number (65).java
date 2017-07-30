a6213ef3286f758108ed97c281912a4dc784b45d
Jason51122 <jason51122@users.noreply.github.com>
2017-07-29 23:17:32 -0700

    Update Valid Number (65).java

--- Math/Valid Number (65).java
+++ Math/Valid Number (65).java
@@ -1,38 +1,31 @@
 public class Solution {
     public boolean isNumber(String s) {
         s = s.trim();
-        int i = 0;
         int len = s.length();
         boolean num = false;
         boolean sign = false;
         boolean e = false;
         boolean point = false;
         
-        while (i < len) {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             
             if ('0' <= c && c <= '9') {
                 num = true;
-            }
-            else if ((c == '+' || c == '-') && !sign && !num && !point) {
+            } else if ((c == '+' || c == '-') && !sign && !num && !point) {
                 sign = true;
-            }
-            else if (c == '.' && !point && !e) {
+            } else if (c == '.' && !point && !e) {
                 point = true;
-            }
-            else if (c == 'e' && !e && num) {
+            } else if (c == 'e' && !e && num) {
                 e = true;
                 num = false;
                 sign = false;
                 point = false;
-            }
-            else {
+            } else {
                 return false;
             }
-            
-            i++;
         }
         
-        return num;
+        return num;    
     }
 }
