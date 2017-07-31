f17d915f9ed6e5be25523f2a73926f11d1b29b09
Jason51122 <jason51122@users.noreply.github.com>
2017-07-31 11:39:33 -0700

    Update 2 Keys Keyboard (650).java

--- DP/2 Keys Keyboard (650).java
+++ DP/2 Keys Keyboard (650).java
@@ -3,14 +3,11 @@ public class Solution {
         int[] dp = new int[n + 1];
 
         for (int i = 2; i <= n; i++) {
-            dp[i] = i;
-            
-            for (int j = i - 1; j > 1; j--) {
+            for (int j = i - 1; j >= 1; j--) {
                 if (i % j == 0) {
                     dp[i] = dp[j] + i / j;
                     break;
                 }
-                
             }
         }
         
