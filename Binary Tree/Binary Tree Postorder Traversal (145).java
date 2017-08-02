f30e59c6001345071771402e9389c0fd971e34a2
Jason51122 <jason51122@users.noreply.github.com>
2017-08-02 01:00:45 -0700

    Update Binary Tree Postorder Traversal (145).java

--- Binary Tree/Binary Tree Postorder Traversal (145).java
+++ Binary Tree/Binary Tree Postorder Traversal (145).java
@@ -15,11 +15,43 @@ public class Solution {
         while (root != null || !stack.empty()) {
             if (root != null) {
                 result.add(0, root.val);
-                stack.add(root);
+                stack.add(root.left);
                 root = root.right;
             } else {
+                root = stack.pop();
+            }
+        }
+        
+        return result;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        
+        while (root != null || !stack.empty()) {
+            if (root != null) {
+                stack.add(root);
+                root = root.left;
+            } else if (stack.peek().right != null && stack.peek().right != pre) {
+                root = stack.peek().right;
+            } else {
                 TreeNode peek = stack.pop();
-                root = peek.left;
+                result.add(peek.val);
+                pre = peek;
+                root = null;
             }
         }
         
