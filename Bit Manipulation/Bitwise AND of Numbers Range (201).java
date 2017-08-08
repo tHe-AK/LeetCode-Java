d8a1b6e6ec39c2a4e658399bd78f9aead9b868fc
Jason51122 <jason51122@users.noreply.github.com>
2017-08-07 23:55:49 -0700

    Update Bitwise AND of Numbers Range (201).java

--- Bit Manipulation/Bitwise AND of Numbers Range (201).java
+++ Bit Manipulation/Bitwise AND of Numbers Range (201).java
@@ -1,5 +1,19 @@
 public class Solution {
     public int rangeBitwiseAnd(int m, int n) {
+        int shift = 0;
+        
+        while (m != n) {
+            m >>= 1;
+            n >>= 1;
+            shift++;
+        }
+        
+        return m << shift;
+    }
+}
+
+public class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
         while (m < n) {
             n &= n - 1;
         }
