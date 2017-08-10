c4fd4359333a11a3214dc631449098367412ab34
Jason51122 <jason51122@users.noreply.github.com>
2017-08-09 22:11:25 -0700

    Update Summary Ranges (228).java

--- Array/Summary Ranges (228).java
+++ Array/Summary Ranges (228).java
@@ -1,5 +1,27 @@
 public class Solution {
     public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+        int len = nums.length;
+        int start = 0;
+        
+        for (int i = 1; i <= len; i++) {
+            if (i == len || nums[i - 1] + 1 < nums[i]) {
+                if (start == i - 1) {
+                    res.add(nums[start] + "");
+                } else {
+                    res.add(nums[start] + "->" + nums[i - 1]);
+                }
+                
+                start = i;
+            }
+        }
+        
+        return res;
+    }
+}
+
+public class Solution {
+    public List<String> summaryRanges(int[] nums) {
         List<String> result = new ArrayList<>();
         int len = nums.length;
         
