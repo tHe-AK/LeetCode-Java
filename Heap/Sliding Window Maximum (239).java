6377b1c6b72481ccbbde6c2787b2372c325a4182
Jason51122 <jason51122@users.noreply.github.com>
2017-08-10 01:34:52 -0700

    Update Sliding Window Maximum (239).java

--- Heap/Sliding Window Maximum (239).java
+++ Heap/Sliding Window Maximum (239).java
@@ -4,6 +4,29 @@ public class Solution {
             return new int[0];
         }
         
+        Queue<Integer> queue = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+        int len = nums.length;
+        int[] result = new int[len - k + 1];
+        
+        for (int i = 0; i < len; i++) {
+            queue.offer(nums[i]);
+            
+            if (i >= k - 1) {
+                result[i - k + 1] = queue.peek();
+                queue.remove(nums[i - k + 1]);
+            }
+        }
+        
+        return result;
+    }
+}
+
+public class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (k <= 0) {
+            return new int[0];
+        }
+        
         Deque<Integer> queue = new LinkedList<Integer>();
         int len = nums.length;
         int[] result = new int[len - k + 1];
