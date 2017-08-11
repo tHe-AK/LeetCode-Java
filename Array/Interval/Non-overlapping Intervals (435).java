b4f92b9e078a32ea6836361543fbed24dd79dc20
Jason51122 <jason51122@users.noreply.github.com>
2017-08-10 23:30:24 -0700

    Update Non-overlapping Intervals (435).java

--- Array/Interval/Non-overlapping Intervals (435).java
+++ Array/Interval/Non-overlapping Intervals (435).java
@@ -13,6 +13,38 @@ public class Solution {
             return 0;
         }
         
+        Arrays.sort(intervals, (a, b) -> a.end - b.end);
+        
+        int count = 0;
+        int min = intervals[0].end;
+        
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].start < min) {
+                count++;
+            } else {
+                min = intervals[i].end;
+            }
+        }
+        
+        return count;
+    }
+}
+
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public int eraseOverlapIntervals(Interval[] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+        
         Arrays.sort(intervals, (a, b) -> a.start - b.start);
         
         int count = 0;
