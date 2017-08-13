76e87b188e136b455d852d1d02dc677b4df1310a
Jason51122 <jason51122@users.noreply.github.com>
2017-08-13 01:19:17 -0700

    Update Paint Fence (276).java

--- DP/Paint Fence (276).java
+++ DP/Paint Fence (276).java
@@ -1,19 +1,20 @@
 public class Solution {
-    public int numWays2(int n, int k) {
+    public int numWays(int n, int k) {
         if (n == 0) {
             return 0;
         } else if (n == 1) {
             return k;
         } else {
-            int[] rec = new int[n + 1];
-            rec[1] = k;
-            rec[2] = k * k;
-            
+            int same = k;
+            int diff = k * (k - 1);
+
             for (int i = 3; i <= n; i++) {
-                rec[i] = (rec[i - 2] + rec[i - 1]) * (k - 1);
+                int temp = same;
+                same = diff;
+                diff = (temp + diff) * (k - 1);
             }
-            
-            return rec[n];
+
+            return same + diff;
         }
     }
 }
