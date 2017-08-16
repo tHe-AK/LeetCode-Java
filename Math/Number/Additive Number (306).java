c83bee8d32a8eccff0eac92af08543c01b58e889
Jason51122 <jason51122@users.noreply.github.com>
2017-08-16 14:40:45 -0700

    Update Additive Number (306).java

--- Math/Number/Additive Number (306).java
+++ Math/Number/Additive Number (306).java
@@ -6,8 +6,8 @@ public class Solution {
         
         for (int i = 0; i < len; i++) {
             if (i == 0 || num.charAt(0) != '0') {
-                for (int j = i + 1; j < len; j++) {
-                    if ((j == i + 1 || num.charAt(i + 1) != '0') && j + 1 < len) {
+                for (int j = i + 1; j < len - 1; j++) {
+                    if (j == i + 1 || num.charAt(i + 1) != '0') {
                         BigInteger num1 = new BigInteger(num.substring(0, i + 1));
                         BigInteger num2 = new BigInteger(num.substring(i + 1, j + 1));
                         
