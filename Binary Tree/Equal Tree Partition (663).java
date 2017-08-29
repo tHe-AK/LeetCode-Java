fdaae8793f3f53a8871221b7c80aad15dba351b5
Jason51122 <jason51122@users.noreply.github.com>
2017-08-29 00:26:35 -0700

    Update Equal Tree Partition (663).java

--- Binary Tree/Equal Tree Partition (663).java
+++ Binary Tree/Equal Tree Partition (663).java
@@ -4,20 +4,20 @@ class Solution {
         int sum = postorder(root, map);
         
         if (sum == 0) {
-                return map.get(sum) > 1;
+            return map.get(sum) > 1;
         } else {
-                return map.containsKey(sum / 2);
+            return map.containsKey(sum / 2);
         }
     }
     
     private int postorder(TreeNode root, Map<Integer, Integer> map) {
-    		if (root == null) {
-    			return 0;
-    		}
+        if (root == null) {
+        	    return 0;
+        }
 
-    		int sum = root.val + postorder(root.left, map) + postorder(root.right, map);
-    		map.put(sum, map.getOrDefault(sum, 0) + 1);
-    		
-    		return sum;
+        int sum = root.val + postorder(root.left, map) + postorder(root.right, map);
+        map.put(sum, map.getOrDefault(sum, 0) + 1);
+        
+        return sum;
     }
 }
