d57aa2d642cf95e314284c04fb25e466b3851947
Jason51122 <jason51122@users.noreply.github.com>
2017-08-29 00:25:39 -0700

    Create Equal Tree Partition (663).java

---
+++ Binary Tree/Equal Tree Partition (663).java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkEqualTree(TreeNode root) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = postorder(root, map);
+        
+        if (sum == 0) {
+                return map.get(sum) > 1;
+        } else {
+                return map.containsKey(sum / 2);
+        }
+    }
+    
+    private int postorder(TreeNode root, Map<Integer, Integer> map) {
+    		if (root == null) {
+    			return 0;
+    		}
+
+    		int sum = root.val + postorder(root.left, map) + postorder(root.right, map);
+    		map.put(sum, map.getOrDefault(sum, 0) + 1);
+    		
+    		return sum;
+    }
+}
