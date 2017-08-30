bfb9ee45857d9bd6716983636ccb64553a8e8a96
Jason51122 <jason51122@users.noreply.github.com>
2017-08-30 15:52:18 -0700

    Update Remove Boxes (546).java

--- DP/Remove Boxes (546).java
+++ DP/Remove Boxes (546).java
@@ -2,6 +2,37 @@ public class Solution {
     public int removeBoxes(int[] boxes) {
         int n = boxes.length;
         int[][][] dp = new int[n][n][n];
+        
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k <= i; k++) {
+                dp[i][i][k] = (k + 1) * (k + 1);
+            }
+        }
+        
+        for (int d = 1; d < n; d++) {
+            for (int i = 0; i < n - d; i++) {
+                int j = i + d;
+                
+                for (int k = 0; k <= i; k++) {
+                    dp[i][j][k] = (k + 1) * (k + 1) + dp[i + 1][j][0];
+                    
+                    for (int m = i + 1; m <= j; m++) {
+                        if (boxes[i] == boxes[m]) {
+                            dp[i][j][k] = Math.max(dp[i][j][k], dp[i + 1][m - 1][0] + dp[m][j][k + 1]);
+                        }
+                    }
+                }
+            }
+        }
+
+        return dp[0][n - 1][0];
+    }
+}
+
+public class Solution {
+    public int removeBoxes(int[] boxes) {
+        int n = boxes.length;
+        int[][][] dp = new int[n][n][n];
 
         return dfs(boxes, dp, 0, n - 1, 0);
     }
