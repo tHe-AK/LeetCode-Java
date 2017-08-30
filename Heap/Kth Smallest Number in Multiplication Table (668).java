0e9750f6805a41805453b6eea9acbcad54acad85
Jason51122 <jason51122@users.noreply.github.com>
2017-08-29 22:04:15 -0700

    Update Kth Smallest Number in Multiplication Table (668).java

--- Heap/Kth Smallest Number in Multiplication Table (668).java
+++ Heap/Kth Smallest Number in Multiplication Table (668).java
@@ -1,5 +1,35 @@
 class Solution {
     public int findKthNumber(int m, int n, int k) {
+        int low = 1;
+        int high = m * n;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int count = getCount(m, n, mid);
+            
+            if (k <= count) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return low;
+    }
+    
+    private int getCount(int m, int n, int k) {
+        int count = 0;
+        
+        for (int i = 1; i <= m; i++) {
+            count += Math.min(n, k / i);
+        }
+        
+        return count;
+    }
+}
+
+class Solution {
+    public int findKthNumber(int m, int n, int k) {
         Queue<int[]> queue = new PriorityQueue<int[]>((a, b) -> Integer.compare(a[0] * a[1], b[0] * b[1]));
         queue.offer(new int[] { 1, 1 });
         
