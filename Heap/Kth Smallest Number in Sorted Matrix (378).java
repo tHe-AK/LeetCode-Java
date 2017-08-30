748aa776e56969b97f38c4902c29c58a83491107
Jason51122 <jason51122@users.noreply.github.com>
2017-08-30 00:11:30 -0700

    Update Kth Smallest Number in Sorted Matrix (378).java

--- Heap/Kth Smallest Number in Sorted Matrix (378).java
+++ Heap/Kth Smallest Number in Sorted Matrix (378).java
@@ -7,30 +7,41 @@ class Solution {
             int mid = low + (high - low) / 2;
             int count = getCount(matrix, mid);
             
-            if (k <= count) {
+            if (count >= k) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
         
-        return low - 1;
+        return low;
     }
     
     private int getCount(int[][] matrix, int k) {
         int count = 0;
         
         for (int i = 0; i < matrix.length; i++) {
-            int idx = Arrays.binarySearch(matrix[i], k);
-            
-            if (idx < 0) {
-                idx = -(idx + 1);
-            }
-            
-            count += idx;
+            count += binarySearch(matrix[i], k);
         }
         
         return count;
+    }
+    
+    private int binarySearch(int[] array, int k) {
+        int low = 0;
+        int high = array.length - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (array[mid] <= k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return low;
     }
 }
 
