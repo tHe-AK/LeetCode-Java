816a4867972af7c4681c942a72c17f870f888f2d
Jason51122 <jason51122@users.noreply.github.com>
2017-09-03 00:38:33 -0700

    Update Self Crossing (335).java

--- Math/Self Crossing (335).java
+++ Math/Self Crossing (335).java
@@ -1,4 +1,4 @@
-public class Solution {
+class Solution {
     public boolean isSelfCrossing(int[] x) {
         for (int i = 3; i < x.length; i++) {
             if (x[i] >= x[i - 2] && x[i - 1] <= x[i - 3]) {
@@ -9,7 +9,7 @@ public class Solution {
                 return true;
             }
               
-            if (i >= 5 && x[i] >= x[i - 2] - x[i - 4] && x[i - 2] >= x[i - 4] && x[i - 3] >= x[i - 1] && x[i - 5] >= x[i - 3] - x[i - 1]) {
+            if (i >= 5 && x[i] >= x[i - 2] - x[i - 4] && x[i - 1] <= x[i - 3] && x[i - 1] >= x[i - 3] - x[i - 5] && x[i - 2] >= x[i - 4]) {
                 return true;
             }
         }
