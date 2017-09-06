836456dde0c0e8301ad74b2f4a9fdb8f0ed4e2f8
Jason51122 <jason51122@users.noreply.github.com>
2017-09-06 01:04:12 -0700

    Update and rename Design/Top K Frequent Elements (347).java to Heap/Top K Frequent Elements (347).java

--- Heap/Top K Frequent Elements (347).java
+++ Heap/Top K Frequent Elements (347).java
@@ -1,6 +1,34 @@
 class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
         int len = nums.length;
+    	Map<Integer, Integer> map = new HashMap<>();
+    	Queue<int[]> queue = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+        List<Integer> res = new ArrayList<>();
+        
+    	for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+    	}
+    	
+    	for (int key : map.keySet()) {
+            int val = map.get(key);
+            queue.offer(new int[] { key, val });
+            
+    	    if (queue.size() > k) {
+                queue.poll();
+            }
+    	}
+    	
+    	while (!queue.isEmpty()) {
+            res.add(queue.poll()[0]);
+        }
+    	
+    	return res;
+    }
+}
+
+class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        int len = nums.length;
         List<Integer> result = new ArrayList<>();
     	Map<Integer, Integer> map = new HashMap<>();
         Set<Integer>[] buckets = new Set[len + 1];
