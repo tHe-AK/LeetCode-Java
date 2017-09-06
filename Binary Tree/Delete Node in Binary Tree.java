720a38451e7256ac4a95c58e7fd6918f41faa151
Jason51122 <jason51122@users.noreply.github.com>
2017-09-05 21:24:36 -0700

    Update Delete Node in Binary Tree.java

--- Binary Tree/Delete Node in Binary Tree.java
+++ Binary Tree/Delete Node in Binary Tree.java
@@ -1,34 +1,34 @@
 class Solution {
-	public List<TreeNode> removeNodes(TreeNode root, Set<Integer> set) {
-		List<TreeNode> res = new ArrayList<>();
+    public List<TreeNode> removeNodes(TreeNode root, Set<Integer> set) {
+        List<TreeNode> res = new ArrayList<>();
 
-		if (removeNodes(root, set, res) != null) {
-			res.add(root);
-		}
+        if (removeNodes(root, set, res) != null) {
+            res.add(root);
+        }
 
-		return res;
-	}
+        return res;
+    }
 
-	private TreeNode removeNodes(TreeNode root, Set<Integer> set, List<TreeNode> res) {
-		if (root == null) {
-			return null;
-		}
+    private TreeNode removeNodes(TreeNode root, Set<Integer> set, List<TreeNode> res) {
+        if (root == null) {
+            return null;
+        }
 
-		root.left = removeNodes(root.left, set, res);
-		root.right = removeNodes(root.right, set, res);
+        root.left = removeNodes(root.left, set, res);
+        root.right = removeNodes(root.right, set, res);
 
-		if (set.contains(root.val)) {
-			if (root.left != null) {
-				res.add(root.left);
-			}
+        if (set.contains(root.val)) {
+            if (root.left != null) {
+                res.add(root.left);
+            }
 
-			if (root.right != null) {
-				res.add(root.right);
-			}
+            if (root.right != null) {
+                res.add(root.right);
+            }
 
-			return null;
-		} else {
-			return root;
-		}
-	}
+            return null;
+        } else {
+            return root;
+        }
+    }
 }
