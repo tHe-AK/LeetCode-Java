836bd721b9ff4faf2d4c3fd732b572a06f008e80
Jason51122 <jason51122@users.noreply.github.com>
2017-09-05 23:59:36 -0700

    Update Integer Break (343).java

--- DP/Integer Break (343).java
+++ DP/Integer Break (343).java
@@ -18,10 +18,9 @@ public class Solution {
     }
 }
 
-public class Solution {
+class Solution {
     public int integerBreak(int n) {
         int[] dp = new int[n + 1];
-        dp[1] = 1;
 
         for (int i = 2; i <= n; i++) {
             for (int j = 1; j < i; j++) {
