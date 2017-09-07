9dfbd80b20138830f2e8b2ea3c7fe6cf9ecee3b8
Jason51122 <jason51122@users.noreply.github.com>
2017-09-06 23:25:34 -0700

    Update Russian Doll Envelopes (354).java

--- String/Longest Increasing Sequence/Russian Doll Envelopes (354).java
+++ String/Longest Increasing Sequence/Russian Doll Envelopes (354).java
@@ -1,26 +1,19 @@
-public class Solution {
+class Solution {
     public int maxEnvelopes(int[][] envelopes) {
-        Arrays.sort(envelopes, (a, b) -> {
-            if (a[0] != b[0]) {
-                return a[0] - b[0];
-            } else {
-                return b[1] - a[1];
-            }
-        });
-
+        Arrays.sort(envelopes, (a, b) -> a[0] != b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(b[1], a[1]));
         int[] rec = new int[envelopes.length];
         int len = 0;
 
         for (int[] envelope : envelopes) {
-            int index = Arrays.binarySearch(rec, 0, len, envelope[1]);
+            int idx = Arrays.binarySearch(rec, 0, len, envelope[1]);
             
-            if (index < 0) {
-                index = -(index + 1);
+            if (idx < 0) {
+                idx = -(idx + 1);
             }
             
-            rec[index] = envelope[1];
+            rec[idx] = envelope[1];
             
-            if (index == len) {
+            if (idx == len) {
                 len++;
             }
         }
@@ -29,9 +22,9 @@ public class Solution {
     }
 }
 
-public class Solution {
+class Solution {
     public int maxEnvelopes(int[][] envelopes) {
-        Arrays.sort(envelopes, (a, b) -> a[0] - b[0]);
+        Arrays.sort(envelopes, (a, b) -> Integer.compare(a[0], b[0]));
         int len = envelopes.length;
         int[] rec = new int[len];
         int max = 0;
