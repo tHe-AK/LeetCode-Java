2f56a3c17acec3c21d8c00f52c19c7366b67d43b
Jason51122 <jason51122@users.noreply.github.com>
2017-09-12 01:24:36 -0700

    Update Implement Magic Dictionary (676).java

--- Binary Tree/Trie/Implement Magic Dictionary (676).java
+++ Binary Tree/Trie/Implement Magic Dictionary (676).java
@@ -41,3 +41,76 @@ class MagicDictionary {
  * obj.buildDict(dict);
  * boolean param_2 = obj.search(word);
  */
+
+class MagicDictionary {
+    private class TrieNode {
+        public TrieNode[] links;
+        public boolean isEnd;
+        
+        public TrieNode() {
+            links = new TrieNode[26];
+        }
+    }
+    
+    private TrieNode root;
+    
+    /** Initialize your data structure here. */
+    public MagicDictionary() {
+        root = new TrieNode();
+    }
+    
+    /** Build a dictionary through a list of words */
+    public void buildDict(String[] dict) {
+        for (String word : dict) {
+            TrieNode node = root;
+            
+            for (int i = 0; i < word.length(); i++) {
+                int idx = word.charAt(i) - 'a';
+                
+                if (node.links[idx] == null) {
+                    node.links[idx] = new TrieNode();
+                }
+                
+                node = node.links[idx];
+            }
+            
+            node.isEnd = true;
+        }
+    }
+    
+    /** Returns if there is any word in the trie that equals to the given word after modifying exactly one character */
+    public boolean search(String word) {
+        return search(root, word, 0, false);
+    }
+    
+    private boolean search(TrieNode node, String word, int idx, boolean isChanged) {
+        if (node == null) {
+            return false;
+        }
+        
+        if (idx == word.length()) {
+            return node.isEnd && isChanged;
+        }
+        
+        int i = word.charAt(idx) - 'a';
+        
+        if (isChanged) {
+            return search(node.links[i], word, idx + 1, isChanged);
+        } else {
+            for (int j = 0; j < 26; j++) {
+                if (search(node.links[j], word, idx + 1, j != i ? true : false)) {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+    }
+}
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary obj = new MagicDictionary();
+ * obj.buildDict(dict);
+ * boolean param_2 = obj.search(word);
+ */
