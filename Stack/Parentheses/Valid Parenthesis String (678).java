8994eef9d79a9539f781b632c6e80c032482858f
Jason51122 <jason51122@users.noreply.github.com>
2017-09-19 20:51:45 -0700

    Update Valid Parenthesis String (678).java

--- Stack/Parentheses/Valid Parenthesis String (678).java
+++ Stack/Parentheses/Valid Parenthesis String (678).java
@@ -1,9 +1,33 @@
 class Solution {
     public boolean checkValidString(String s) {
+        int low = 0;
+        int high = 0;
+        
+        for (char ch: s.toCharArray()) {
+            low += ch == '(' ? 1 : -1;
+            high += ch != ')' ? 1 : -1;
+            
+            if (high < 0) {
+                return 
+            }
+            
+            low = Math.max(low, 0);
+        }
+        
+        return low == 0;
+    }
+}
+
+class Solution {
+    public boolean checkValidString(String s) {
         return checkValidString(s, 0, 0);
     }
     
     private boolean checkValidString(String s, int i, int count) {
+        if (count < 0) {
+            return false;
+        }
+        
         if (i == s.length()) {
             return count == 0;
         }
@@ -13,9 +37,9 @@ class Solution {
         if (ch == '(') {
             return checkValidString(s, i + 1, count + 1);
         } else if (ch == ')') {
-            return count > 0 && checkValidString(s, i + 1, count - 1);
+            return checkValidString(s, i + 1, count - 1);
         } else {
-            return checkValidString(s, i + 1, count) || checkValidString(s, i + 1, count + 1) || (count > 0 && checkValidString(s, i + 1, count - 1));
+            return checkValidString(s, i + 1, count) || checkValidString(s, i + 1, count + 1) || checkValidString(s, i + 1, count - 1);
         }
     }
 }
