f1c6b4f8ba4e2cc253f1bddf84301b9b49afea71
Jason51122 <jason51122@users.noreply.github.com>
2017-09-20 22:50:40 -0700

    Update Longest Repeating Character Replacement (424).java

--- String/Window Substring/Longest Repeating Character Replacement (424).java
+++ String/Window Substring/Longest Repeating Character Replacement (424).java
@@ -1,21 +1,26 @@
-public class Solution {
+class Solution {
     public int characterReplacement(String s, int k) {
-        int result = 0;
-        int[] rec = new int[256];
+        int[] rec = new int[26];
         int start = 0;
         int end = 0;
-        int count = 0;
+        int cnt = 0;
+        int res = 0;
         
         while (end < s.length()) {
-            count = Math.max(count, ++rec[s.charAt(end++)]);
+            cnt = Math.max(cnt, ++rec[s.charAt(end++) - 'A']);
 
-            while (end - start - count > k) {
-                rec[s.charAt(start++)]--;
+            while (end - start - cnt > k) {
+                rec[s.charAt(start++) - 'A']--;
+                cnt = 0;
+                
+                for (int i = 0; i < rec.length; i++) {
+                    cnt = Math.max(cnt, rec[i]);
+                }
             }
             
-            result = Math.max(result, end - start);
+            res = Math.max(res, end - start);
         }
         
-        return result;
+        return res;
     }
 }
