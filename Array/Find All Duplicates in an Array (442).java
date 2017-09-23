cc6abc9798c40ee559b6a7cf9a793eed02c72cbc
Jason51122 <jason51122@users.noreply.github.com>
2017-09-23 16:46:43 -0700

    Update Find All Duplicates in an Array (442).java

--- Array/Find All Duplicates in an Array (442).java
+++ Array/Find All Duplicates in an Array (442).java
@@ -1,17 +1,17 @@
-public class Solution {
+class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        List<Integer> result = new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
         
         for (int i = 0; i < nums.length; i++) {
             int idx = Math.abs(nums[i]) - 1;
             
             if (nums[idx] < 0) {
-                result.add(Math.abs(idx + 1));
+                res.add(idx + 1);
+            } else {
+                nums[idx] = -nums[idx];
             }
-            
-            nums[idx] = -nums[idx];
         }
         
-        return result;
+        return res;
     }
 }
