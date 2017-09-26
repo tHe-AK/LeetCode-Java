5483a3a6edd98a55a821e4047e90bdd1fdc8ad0c
Jason51122 <jason51122@users.noreply.github.com>
2017-09-26 00:09:25 -0700

    Update K Empty Slots (683).java

--- Array/K Empty Slots (683).java
+++ Array/K Empty Slots (683).java
@@ -1,5 +1,38 @@
 class Solution {
     public int kEmptySlots(int[] flowers, int k) {
+        int n = flowers.length;
+        int[] rec = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            rec[flowers[i] - 1] = i + 1;
+        }
+        
+        int start = 0;
+        int end = k + 1;
+        int min = Integer.MAX_VALUE; 
+        
+        while (end < n) {
+            int max = Math.max(rec[start], rec[end]);
+            int i = start + 1;
+            
+            while (i < end && rec[i] > max) {
+                i++;
+            }
+            
+            if (i == end) {
+                min = Math.min(min, max);
+            }
+            
+            start = i;
+            end = start + k + 1;
+        }
+        
+        return min == Integer.MAX_VALUE ? -1 : min;
+    }
+}
+
+class Solution {
+    public int kEmptySlots(int[] flowers, int k) {
         TreeSet<Integer> set = new TreeSet<>();
         
         for (int i = 0; i < flowers.length; i++) {
