2b30e1a1af91fdc8fa7bc0fb0c08fba9995ae6da
Jason51122 <jason51122@users.noreply.github.com>
2017-10-04 01:51:07 -0700

    Create Employee Importance (690).java

---
+++ Backtracking/Employee Importance (690).java
@@ -0,0 +1,34 @@
+/*
+// Employee info
+class Employee {
+    // It's the unique id of each node;
+    // unique id of this employee
+    public int id;
+    // the importance value of this employee
+    public int importance;
+    // the id of direct subordinates
+    public List<Integer> subordinates;
+};
+*/
+class Solution {
+    public int getImportance(List<Employee> employees, int id) {
+        Map<Integer, Employee> map = new HashMap<>();
+        
+        for (Employee e : employees) {
+            map.put(e.id, e);
+        }
+        
+        return dfs(map, id);
+    }
+    
+    private int dfs(Map<Integer, Employee> map, int id) {
+        Employee e = map.get(id);
+        int sum = e.importance;
+        
+        for (int i : e.subordinates) {
+            sum += dfs(map, i);
+        }
+        
+        return sum;
+    }
+}
