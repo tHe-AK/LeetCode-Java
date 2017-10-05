25a69d56da93b637a02ab7dd4af662e42fcd1f49
Jason51122 <jason51122@users.noreply.github.com>
2017-10-04 23:31:48 -0700

    Update Minimum Number of Arrows to Burst Balloons (452).java

--- Array/Interval/Minimum Number of Arrows to Burst Balloons (452).java
+++ Array/Interval/Minimum Number of Arrows to Burst Balloons (452).java
@@ -1,4 +1,26 @@
-public class Solution {
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        if (points.length == 0) {
+            return 0;
+        }
+        
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+        
+        int count = 1;
+        int min = points[0][1];
+        
+        for (int i = 1; i < points.length; i++) {
+            if (min < points[i][0]) {
+                count++;
+                min = points[i][1];
+            }
+        }
+        
+        return count;
+    }
+}
+
+class Solution {
     public int findMinArrowShots(int[][] points) {
         if (points.length == 0) {
             return 0;
