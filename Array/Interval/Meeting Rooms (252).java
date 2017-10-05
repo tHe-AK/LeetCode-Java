0fd2a0e3467e697a995bd623b92e70a6013f9cde
Jason51122 <jason51122@users.noreply.github.com>
2017-10-04 23:28:20 -0700

    Update Meeting Rooms (252).java

--- Array/Interval/Meeting Rooms (252).java
+++ Array/Interval/Meeting Rooms (252).java
@@ -9,7 +9,7 @@
  */
 public class Solution {
     public boolean canAttendMeetings(Interval[] intervals) {
-        Arrays.sort(intervals, (i1, i2) -> i1.start - i2.start);
+        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1.start, i2.start));
 
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i].start < intervals[i - 1].end) {
