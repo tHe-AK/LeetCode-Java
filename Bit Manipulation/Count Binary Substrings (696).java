ef467ed6d0d322fa6d8bd43e5b2297d5f0367ce0
Jason51122 <jason51122@users.noreply.github.com>
2018-01-24 21:03:59 -0800

    Update Count Binary Substrings (696).java

--- Bit Manipulation/Count Binary Substrings (696).java
+++ Bit Manipulation/Count Binary Substrings (696).java
@@ -4,6 +4,29 @@ class Solution {
         int curr = 1;
         int count = 0;
         
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                curr++;
+            } else {
+                pre = curr;
+                curr = 1;
+            }
+            
+            if (curr <= pre) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
+
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int pre = 0;
+        int curr = 1;
+        int count = 0;
+        
         for (int i = 1; i <= s.length(); i++) {
             if (i < s.length() && s.charAt(i) == s.charAt(i - 1)) {
                 curr++;
