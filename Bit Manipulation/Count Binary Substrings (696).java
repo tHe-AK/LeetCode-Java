ca2ba4b1d43c1bf619c2c5375b4b99af7080c74c
Jason51122 <jason51122@users.noreply.github.com>
2018-01-24 20:58:11 -0800

    Create Count Binary Substrings (696).java

---
+++ Bit Manipulation/Count Binary Substrings (696).java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int pre = 0;
+        int curr = 1;
+        int count = 0;
+        
+        for (int i = 1; i <= s.length(); i++) {
+            if (i < s.length() && s.charAt(i) == s.charAt(i - 1)) {
+                curr++;
+            } else {
+                count += Math.min(pre, curr);
+                pre = curr;
+                curr = 1;
+            }
+        }
+        
+        return count;
+    }
+}
