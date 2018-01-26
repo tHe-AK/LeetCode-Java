f63b1a6f9fc0f905116d4e0b639050b34f7dd945
Jason51122 <jason51122@users.noreply.github.com>
2018-01-25 22:43:34 -0800

    Create Partition to K Equal Sum Subsets (698).java

---
+++ Backtracking/Partition to K Equal Sum Subsets (698).java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = 0;
+        
+        for (int num : nums) {
+            sum += num;
+        }
+        
+        if (sum % k != 0) {
+            return false;
+        }
+        
+        return dfs(nums, k, 0, sum / k, sum / k, 0, new boolean[nums.length]);
+    }
+    
+    private boolean dfs(int[] nums, int k, int start, int target, int sum, int count, boolean[] visited) {
+        if (k == 0) {
+            return true;
+        } else {
+            if (target == 0 && count > 0) {
+                return dfs(nums, k - 1, 0, sum, sum, 0, visited);
+            } else {
+                for (int i = start; i < nums.length; i++) {
+                    if (!visited[i] && nums[i] <= target) {
+                        visited[i] = true;
+                        
+                        if (dfs(nums, k, i + 1, target - nums[i], sum, count + 1, visited)) {
+                            return true;
+                        }
+                        
+                        visited[i] = false;
+                    }
+                }
+                
+                return false;
+            }
+        }
+    }
+}
