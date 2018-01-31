4107de960d762b3459b2c3731afa595d0cffd4d9
Jason51122 <jason51122@users.noreply.github.com>
2018-01-30 16:44:15 -0800

    Update Optimal Account Balancing (465).java

--- Backtracking/Optimal Account Balancing (465).java
+++ Backtracking/Optimal Account Balancing (465).java
@@ -43,3 +43,48 @@ public class Solution {
         }
     }
 }
+
+public class Solution {
+    public int minTransfers(int[][] transactions) {
+        Map<Integer, Long> map = new HashMap<>();
+        
+        for (int[] t : transactions){
+            map.put(t[0], map.getOrDefault(t[0], 0L) - t[2]);
+            map.put(t[1], map.getOrDefault(t[1], 0L) + t[2]);
+        }        
+
+        List<Long> list = new ArrayList<>();
+        
+        for (long val: map.values()){
+            if (val != 0) {
+                list.add(val);
+            }
+        }
+
+        int[] min = new int[1];
+        min[0] = Integer.MAX_VALUE;
+		dfs(list.toArray(new Long[0]), 0, 0, 0, 0, list.size(), new boolean[list.size()], min);
+        
+        return min[0];
+    }
+    
+	private void dfs(Long[] nums, int start, long target, int count, int total, int remaining, boolean[] visited, int[] min) {
+		if (target == 0 && count > 0) {
+			total += count - 1;
+
+			if (remaining == 0) {
+				min[0] = Math.min(min[0], total);
+			} else {
+				dfs(nums, 0, 0, 0, total, remaining, visited, min);
+			}
+		} else {
+			for (int i = start; i < nums.length; i++) {
+				if (!visited[i]) {
+					visited[i] = true;
+					dfs(nums, i + 1, target - nums[i], count + 1, total, remaining - 1, visited, min);
+					visited[i] = false;
+				}
+			}
+		}
+	}
+}
