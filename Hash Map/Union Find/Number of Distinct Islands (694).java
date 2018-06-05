558f424b4943f540050c008a39c5373098deee07
Jason51122 <jason51122@users.noreply.github.com>
2018-06-04 17:47:44 -0700

    Update Number of Distinct Islands (694).java

--- Hash Map/Union Find/Number of Distinct Islands (694).java
+++ Hash Map/Union Find/Number of Distinct Islands (694).java
@@ -31,3 +31,39 @@ public class Solution {
         }
     }
 }
+
+public class Solution {
+    public int numDistinctIslands(int[][] grid) {
+        Set<List<Integer>> set = new HashSet<>();
+        int[][] delta = { { 0, -1 }, { 0, 1 }, { -1, 0 }, { 1, 0 } };
+        int row = grid.length;
+        int col = grid[0].length;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    List<Integer> path = new ArrayList<>();
+                    dfs(grid, i, j, path, i * col + j, delta);
+                    set.add(path);
+                }
+            }
+        }
+
+        return set.size();
+    }
+    
+    private void dfs(int[][] grid, int i, int j, List<Integer> path, int start, int[][] delta) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) {
+            return;
+        }
+        
+        path.add(i * grid[0].length + j - start);
+        grid[i][j] = 0;
+
+        for (int[] diff : delta) {
+            int x = i + diff[0];
+            int y = j + diff[1];
+            dfs(grid, x, y, path, start, delta);
+        }
+    }
+}
