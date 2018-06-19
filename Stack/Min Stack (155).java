a2f83ca5e6bae7c82ebbdd61ca36a181a2a86457
Jason51122 <jason51122@users.noreply.github.com>
2018-06-19 00:19:47 -0700

    Update Min Stack (155).java

--- Stack/Min Stack (155).java
+++ Stack/Min Stack (155).java
@@ -1,4 +1,4 @@
-public class MinStack {
+class MinStack {
     private Stack<Integer> stack;
     private int min;
     
@@ -29,6 +29,48 @@ public class MinStack {
     
     public int getMin() {
         return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
+class MinStack {
+    private Stack<Integer> num;
+    private Stack<Integer> min;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        num = new Stack<>();
+        min = new Stack<>();
+    }
+    
+    public void push(int x) {
+        num.push(x);
+        
+        if (min.empty() || x <= min.peek()) {
+            min.push(x);
+        }
+    }
+    
+    public void pop() {
+        if (num.pop().equals(min.peek())) {
+            min.pop();
+        }
+    }
+    
+    public int top() {
+        return num.peek();
+    }
+    
+    public int getMin() {
+        return min.peek();
     }
 }
 
