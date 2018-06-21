eace4ff9e4c2ac440c492a89fc0bdaa90d602d4b
Jason51122 <jason51122@users.noreply.github.com>
2018-06-21 00:35:21 -0700

    Update Longest Word in Dictionary (720).java

--- Binary Tree/Trie/Longest Word in Dictionary (720).java
+++ Binary Tree/Trie/Longest Word in Dictionary (720).java
@@ -15,11 +15,80 @@ class Solution {
                 end--;
             }
             
-            if (end == 0 && (str.isEmpty() || str.length() < len || (str.length() == len && word.compareTo(str) < 0))) {
+            if (end == 0 && (str.length() < len || (str.length() == len && word.compareTo(str) < 0))) {
                 str = word;
             } 
         }
             
         return str;
+    }
+}
+
+class Solution {
+    public String longestWord(String[] words) {
+        Trie trie = new Trie();
+        
+        for (String word : words) {
+            trie.insert(word);
+        }
+         
+        return trie.longestWord();
+    }
+}
+
+class TrieNode {
+    public TrieNode[] nodes;
+    public boolean isEnd;
+    
+    public TrieNode() {
+        nodes = new TrieNode[26];
+    }
+}
+
+class Trie {
+    private TrieNode root;
+    
+    public Trie() {
+        root = new TrieNode();
+        root.isEnd = true;
+    }
+    
+    public void insert(String str) {
+        TrieNode curr = root;
+        
+        for (int i = 0; i < str.length(); i++) {
+            int idx = str.charAt(i) - 'a';
+            
+            if (curr.nodes[idx] == null) {
+                curr.nodes[idx] = new TrieNode();
+            }
+            
+            curr = curr.nodes[idx];
+        }
+        
+        curr.isEnd = true;
+    }
+    
+    public String longestWord() {
+        String[] res = { "" };
+        dfs(root, "", res);
+        return res[0];
+    }
+    
+    private void dfs(TrieNode root, String word, String[] res) {
+        if (!root.isEnd) {
+            return;
+        }
+        
+        if (word.length() > res[0].length()) {
+            res[0] = word;
+        }
+        
+        for (int i = 0; i < root.nodes.length; i++) {
+            TrieNode node = root.nodes[i];
+            if (node != null) {
+                dfs(node, word + (char)('a' + i), res);
+            }
+        }
     }
 }
