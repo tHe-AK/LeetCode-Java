04b01ca859542dd2137167610aa1d7654d049cee
Jason51122 <jason51122@users.noreply.github.com>
2018-07-11 01:01:10 -0700

    Create My Calendar III (732).java

---
+++ Array/Interval/My Calendar III (732).java
@@ -0,0 +1,27 @@
+class MyCalendarThree {
+    private TreeMap<Integer, Integer> map;
+
+    public MyCalendarThree() {
+        map = new TreeMap<>();
+    }
+    
+    public int book(int start, int end) {
+        map.put(start, map.getOrDefault(start, 0) + 1);
+        map.put(end, map.getOrDefault(end, 0) - 1);
+        int count = 0;
+        int max = 0;
+        
+        for (int val : map.values()) {
+            count += val;
+            max = Math.max(max, count);
+        }
+        
+        return max;
+    }
+}
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree obj = new MyCalendarThree();
+ * int param_1 = obj.book(start,end);
+ */
