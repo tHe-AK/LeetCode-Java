d32e9e16893b07b65bf157b634a5f092cc1b8ed8
Jason51122 <jason51122@users.noreply.github.com>
2018-07-31 00:02:21 -0700

    Update Flatten Binary Tree to Linked List (114).java

--- Binary Tree/Flatten Binary Tree to Linked List (114).java
+++ Binary Tree/Flatten Binary Tree to Linked List (114).java
@@ -1,56 +1,97 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public class Solution {
-    public void flatten(TreeNode root) {
-        flatten(root, new TreeNode[1]);
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+
+    public Node() {}
+
+    public Node(int _val,Node _prev,Node _next,Node _child) {
+        val = _val;
+        prev = _prev;
+        next = _next;
+        child = _child;
     }
-    
-    private void flatten(TreeNode root, TreeNode[] pre) {
-        if (root == null) {
-            return;
+};
+*/
+class Solution {
+    public Node flatten(Node head) {
+        if (head == null) {
+            return null;
         }
         
-        flatten(root.right, pre);
-        flatten(root.left, pre);
+        Node dummy = new Node();
+        Node prev = dummy;
+        Stack<Node> stack = new Stack<>();
         
-        root.right = pre[0];
-        root.left = null;
-        pre[0] = root;
+        while (head != null || !stack.empty()) {
+            if (head != null) {
+                prev.next = head;
+                head.prev = prev;
+                stack.push(head.next);
+                prev = head;
+                head = prev.child;
+                prev.child = null;
+            } else {
+                head = stack.pop();
+            }
+        }
+        
+        head = dummy.next;
+        head.prev = null;
+        return head;
     }
 }
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public class Solution {
-    public void flatten(TreeNode root) {
-        while (root != null) {
-            TreeNode curr = root.left;
-            
-            if (curr != null) {
-                while (curr.right != null) {
-                    curr = curr.right;
-                }
-                
-                curr.right = root.right;
-                root.right = root.left;
-                root.left = null;
-            }
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
 
-            root = root.right;
+    public Node() {}
+
+    public Node(int _val,Node _prev,Node _next,Node _child) {
+        val = _val;
+        prev = _prev;
+        next = _next;
+        child = _child;
+    }
+};
+*/
+class Solution {
+    public Node flatten(Node head) {
+        if (head == null) {
+            return null;
         }
+        
+        Node dummy = new Node();
+        Node[] prev = new Node[1];
+        prev[0] = dummy;
+        
+        preorder(head, prev);
+        
+        head = dummy.next;
+        head.prev = null;
+        return head;
+    }
+    
+    private void preorder(Node head, Node[] prev) {
+        if (head == null) {
+            return;
+        }
+        
+        prev[0].next = head;
+        head.prev = prev[0];
+        prev[0] = head;
+        Node next = head.next;
+        
+        preorder(head.child, prev);
+        head.child = null;
+        preorder(next, prev);
     }
 }
