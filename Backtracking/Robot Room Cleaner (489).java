c3d434c1d8d35f61f5c42fac08086a59e6c84d30
Jason51122 <jason51122@users.noreply.github.com>
2018-08-07 22:51:55 -0700

    Create Robot Room Cleaner (489).java

---
+++ Backtracking/Robot Room Cleaner (489).java
@@ -0,0 +1,17 @@
+/**
+ * // This is the robot‘s control interface.
+ * // You should not implement it, or speculate about its implementation
+ * class Robot {
+ *     // Returns true if the cell in front is open and robot moves into the cell.
+ *     // Returns false if the cell in front is blocked and robot stays in the current cell.
+ *     public bool move();
+ *
+ *     // Robot will stay in the same cell after calling turnLeft/turnRight.
+ *     // Each turn will be 90 degrees.
+ *     public void turnLeft();
+ *     public void turnRight();
+ *
+ *     // Clean the current cell.
+ *     public void clean();
+ * };
+ */
