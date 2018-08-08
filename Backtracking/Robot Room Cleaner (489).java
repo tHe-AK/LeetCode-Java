f2a7efc50a213480f8cafe7e8e5d06baf50fb0a2
Jason51122 <jason51122@users.noreply.github.com>
2018-08-07 23:41:16 -0700

    Update Robot Room Cleaner (489).java

--- Backtracking/Robot Room Cleaner (489).java
+++ Backtracking/Robot Room Cleaner (489).java
@@ -15,3 +15,37 @@
  *     public void clean();
  * };
  */
+
+class Solution {
+    public void cleanRoom(Robot robot) {
+        int[][] delta = { { 0, 1 }, { 0, 1 }, { -1, 0 }, { -1, 0 } };
+        cleanRoom(robot, 0, 0, new HashSet<String>(), 0, delta);
+    }
+
+    private void cleanRoom(Robot robot, int i, int j, Set<String> visited, int dir, int[][] delta) {
+        String idx = i + "-" + j;
+
+        if (visited.contains(idx)) {
+            return;
+        }
+        
+        robot.clean();
+        visited.add(idx);
+
+        for (int k = 0; k < 4; k++) {
+            if (((i == 0 && j == 0) || k != 2) && robot.move()) {
+                int x = i + delta[dir][0];
+                int y = j + delta[dir][1];
+                cleanRoom(robot, x, y, visited, dir, delta);
+                robot.turnRight();
+                robot.turnRight();
+                robot.move();
+                robot.turnRight();
+                robot.turnRight();
+            }
+
+            robot.turnRight();
+            dir = (dir + 1) % 4;
+        }
+    }
+}
