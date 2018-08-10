0ac1116326304c451a75240ee99520ca9ead19e7
Jason51122 <jason51122@users.noreply.github.com>
2018-08-10 00:03:54 -0700

    Update Random Pick with Weight (528).java

--- Math/Probability/Random Pick with Weight (528).java
+++ Math/Probability/Random Pick with Weight (528).java
@@ -26,3 +26,46 @@ class Solution {
  * Solution obj = new Solution(w);
  * int param_1 = obj.pickIndex();
  */
+
+class Solution {
+    private int[] nums;
+    private int sum;
+    private Random rand;
+    
+    public Solution(int[] w) {
+        nums = new int[w.length];
+        sum = 0;
+        rand = new Random();
+        
+        for (int i = 0; i < w.length; i++) {
+            nums[i] = sum;
+            sum += w[i];
+        }
+    }
+    
+    public int pickIndex() {
+        int target = rand.nextInt(sum);
+        int low = 0;
+        int high = nums.length - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return high;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
