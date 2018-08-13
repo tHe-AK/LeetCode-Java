305696d3abf1c81444d04cb3e0ba34f757683a09
Jason51122 <jason51122@users.noreply.github.com>
2018-08-12 23:52:43 -0700

    Update N-ary Tree Preorder Traversal (589).java

--- Binary Tree/N-ary Tree Preorder Traversal (589).java
+++ Binary Tree/N-ary Tree Preorder Traversal (589).java
@@ -69,3 +69,44 @@ class Solution {
         return list;
     }
 }
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<Node> stack = new Stack<>();
+        
+        while (root != null || !stack.empty()) {
+            if (root != null) {
+                list.add(root.val);
+                
+                if (root.children.isEmpty()) {
+                    root = null;
+                } else {
+                    for (int i = root.children.size() - 1; i > 0; i--) {
+                        stack.push(root.children.get(i));
+                    }
+                    
+                    root = root.children.get(0);
+                }
+            } else {
+                root = stack.pop();
+            }
+        }
+        
+        return list;
+    }
+}
