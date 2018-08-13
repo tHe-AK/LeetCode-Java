c9780825245255be6fd748ccdac470e84b0fe17d
Jason51122 <jason51122@users.noreply.github.com>
2018-08-13 10:42:26 -0700

    Create Design Circular Queue (622).java

---
+++ Design/Design Circular Queue (622).java
@@ -0,0 +1,75 @@
+class MyCircularQueue {
+    private int[] queue;
+    private int front;
+    private int rear;
+    private int capacity;
+    private int size;
+
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    public MyCircularQueue(int k) {
+        queue = new int[k];
+        front = -1;
+        rear = -1;
+        capacity = k;
+        size = 0;
+    }
+    
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    public boolean enQueue(int value) {
+        if (isFull()) {
+            return false;
+        } else {
+            rear = (rear + 1) % capacity;
+            queue[rear] = value;
+            size++;
+            
+            if (front == -1) {
+                front = 0;
+            }
+            
+            return true;
+        }
+    }
+    
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    public boolean deQueue() {
+        if (isEmpty()) {
+            return false;
+        } else {
+            front = (front + 1) % capacity;
+            size--;
+            return true;
+        }
+    }
+    
+    /** Get the front item from the queue. */
+    public int Front() {
+        return isEmpty() ? -1 : queue[front];
+    }
+    
+    /** Get the last item from the queue. */
+    public int Rear() {
+        return isEmpty() ? -1 : queue[rear];
+    }
+    
+    /** Checks whether the circular queue is empty or not. */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    /** Checks whether the circular queue is full or not. */
+    public boolean isFull() {
+        return size == capacity;
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
