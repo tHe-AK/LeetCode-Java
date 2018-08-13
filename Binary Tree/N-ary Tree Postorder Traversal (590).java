2307e7174c69a1a1882ac58aabece0c2f6ec293b
Jason51122 <jason51122@users.noreply.github.com>
2018-08-13 00:07:58 -0700

    Update N-ary Tree Postorder Traversal (590).java

--- Binary Tree/N-ary Tree Postorder Traversal (590).java
+++ Binary Tree/N-ary Tree Postorder Traversal (590).java
@@ -31,3 +31,42 @@ class Solution {
         list.add(root.val);
     }
 }
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+            
+        if (root == null) {
+            return list;
+        }
+        
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        
+        while (!stack.empty()) {
+            Node peek = stack.pop();
+            list.add(peek.val);
+            
+            for (Node child : peek.children) {
+                stack.push(child);
+            }
+        }
+        
+        Collections.reverse(list);
+        return list;
+    }
+}
