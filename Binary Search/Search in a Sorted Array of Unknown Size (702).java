26d48f5e31f9f5e2df984a83941794feaa85e32d
Jason51122 <jason51122@users.noreply.github.com>
2018-08-14 01:20:13 -0700

    Update Search in a Sorted Array of Unknown Size (702).java

--- Binary Search/Search in a Sorted Array of Unknown Size (702).java
+++ Binary Search/Search in a Sorted Array of Unknown Size (702).java
@@ -2,17 +2,18 @@ class Solution {
     public int search(ArrayReader reader, int target) {
         int low = 0;
         int high = 9999;
+        int ERROR = ;
         
         while (low <= high) {
             int mid = low + (high - low) / 2;
             int val = reader.get(mid);
             
-            if (val == target) {
-              return mid;
+            if (val == Integer.MAX_VALUE || val > target) {
+              high = mid - 1;
             } else if (val < target) {
               low = mid + 1;
             } else {
-              high = mid - 1;
+              return mid;
             }
         }
 
