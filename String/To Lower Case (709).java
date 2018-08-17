5be614443a76f492fbab97c389ce236e0f1e7f41
Jason51122 <jason51122@users.noreply.github.com>
2018-08-16 23:10:18 -0700

    Create To Lower Case (709).java

---
+++ String/To Lower Case (709).java
@@ -0,0 +1,13 @@
+class Solution {
+    public String toLowerCase(String str) {
+        char[] chars = str.toCharArray();
+        
+        for (int i = 0; i < chars.length; i++) {
+            if ('A' <= chars[i] && chars[i] <= 'Z') {
+                chars[i] = (char) (chars[i] - 'A' + 'a');
+            }
+        }
+        
+        return new String(chars);
+    }
+}
