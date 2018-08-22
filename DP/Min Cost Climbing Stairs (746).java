c5edb55a16bf90c8c70a41549528b54b88524230
Jason51122 <jason51122@users.noreply.github.com>
2018-08-22 00:47:55 -0700

    Create Min Cost Climbing Stairs (746).java

---
+++ DP/Min Cost Climbing Stairs (746).java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int prev = 0;
+        int curr = 0;
+        
+        for (int i = 2; i <= cost.length; i++) {
+            int next = Math.min(prev + cost[i - 2], curr + cost[i - 1]);
+            prev = curr;
+            curr = next;
+        }
+        
+        return curr;
+    }
+}
