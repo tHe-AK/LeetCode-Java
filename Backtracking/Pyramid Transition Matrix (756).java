5c05fdc1c1ccfcde7e85dd4b5f12f5c955ec37fd
Jason51122 <jason51122@users.noreply.github.com>
2018-09-04 23:38:03 -0700

    Create Pyramid Transition Matrix (756).java

---
+++ Backtracking/Pyramid Transition Matrix (756).java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean pyramidTransition(String bottom, List<String> allowed) {
+        Map<String, Set<Character>> map = new HashMap<>();
+        
+        for (String triple : allowed) {
+            String key = triple.substring(0, 2);
+            map.putIfAbsent(key, new HashSet<>());
+            map.get(key).add(triple.charAt(2));
+        }
+        
+        return dfs(bottom, 0, map);
+    }
+    
+    private boolean dfs(String bottom, int i, Map<String, Set<Character>> map) {
+        if (bottom.length() == 1) {
+            return true;
+        }
+        
+        String key = bottom.substring(i, i + 2);
+        
+        if (map.containsKey(key)) {
+            for (Character ch : map.get(key)) {
+                boolean isEnd = i == bottom.length() - 2;
+                String str = bottom.substring(0, i) + ch + (isEnd ? "" : bottom.substring(i + 1));
+                int idx = isEnd ? 0 : i + 1;
+
+                if (dfs(str, idx, map)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
