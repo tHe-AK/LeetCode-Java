e0f31d9b03f280bd9f29272bb8e2fe3cc7375455
Jason51122 <jason51122@users.noreply.github.com>
2018-09-08 00:26:16 -0700

    Update Employee Free Time (759).java

--- Array/Interval/Employee Free Time (759).java
+++ Array/Interval/Employee Free Time (759).java
@@ -43,3 +43,55 @@
         return res;
     }
 }
+
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+
+class Solution {
+    private class Pair {
+        public int id;
+        public int idx;
+
+        public Pair(int id, int idx) {
+            this.id = id;
+            this.idx = idx;
+        }
+    }
+
+    public List<Interval> employeeFreeTime(List<List<Interval>> avails) {
+        List<Interval> res = new ArrayList<>();
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Integer.compare(avails.get(a.id).get(a.idx).start, avails.get(b.id).get(b.idx).start));
+
+        for (int i = 0; i < avails.size(); i++) {
+            pq.add(new Pair(i, 0));
+        }
+
+        int end = -1;
+
+        while (!pq.isEmpty()) {
+            Pair pair = pq.poll();
+            Interval interval = avails.get(pair.id).get(pair.idx);
+
+            if (end != -1 && end < interval.start) {
+                res.add(new Interval(end, interval.start));
+            }
+
+            end = Math.max(end, interval.end);
+
+            if (pair.idx < avails.get(pair.id).size() - 1) {
+                pair.idx++;
+                pq.offer(pair);
+            }
+
+        }
+
+        return res;
+    }
+}
