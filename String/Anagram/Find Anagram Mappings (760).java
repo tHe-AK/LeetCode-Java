1b36a7564583b89d1c3c2b6a6681cc91ce6a2729
Jason51122 <jason51122@users.noreply.github.com>
2018-09-09 22:46:22 -0700

    Update Find Anagram Mappings (760).java

--- String/Anagram/Find Anagram Mappings (760).java
+++ String/Anagram/Find Anagram Mappings (760).java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] anagramMappings(int[] A, int[] B) {
         Map<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[A.length];
 
         for (int i = 0; i < B.length; i++) {
             map.put(B[i], i);
         }
 
-        int[] ans = new int[A.length];
-        int i = 0;
-
-        for (int j = 0; j < A.length; j++) {
-            ans[j] = map.get(A[i]);
+        for (int i = 0; i < A.length; i++) {
+            ans[i] = map.get(A[i]);
         }
 
         return ans;
