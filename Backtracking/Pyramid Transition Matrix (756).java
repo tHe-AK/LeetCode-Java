dba460df0863d60a8626522b1ffcb10ea3833e50
Jason51122 <jason51122@users.noreply.github.com>
2018-09-09 22:09:30 -0700

    Update Pyramid Transition Matrix (756).java

--- Backtracking/Pyramid Transition Matrix (756).java
+++ Backtracking/Pyramid Transition Matrix (756).java
@@ -8,12 +8,20 @@ class Solution {
             map.get(key).add(triple.charAt(2));
         }
         
-        return dfs(bottom, 0, map);
+        return dfs(bottom, 0, map, new HashSet<String>());
     }
     
-    private boolean dfs(String bottom, int i, Map<String, Set<Character>> map) {
+    private boolean dfs(String bottom, int i, Map<String, Set<Character>> map, Set<String> visited) {
         if (bottom.length() == 1) {
             return true;
+        }
+        
+        if (i == 0) {            
+            if (visited.contains(bottom)) {
+                return false;
+            }
+            
+            visited.add(bottom);
         }
         
         String key = bottom.substring(i, i + 2);
@@ -24,7 +32,7 @@ class Solution {
                 String str = bottom.substring(0, i) + ch + (isEnd ? "" : bottom.substring(i + 1));
                 int idx = isEnd ? 0 : i + 1;
 
-                if (dfs(str, idx, map)) {
+                if (dfs(str, idx, map, visited)) {
                     return true;
                 }
             }
