6ed73f177242ead00444b4f4cd96a062b7ff3230
Jason51122 <jason51122@users.noreply.github.com>
2018-09-30 21:42:40 -0700

    Create Jewels and Stones (771).java

---
+++ Hash Map/Jewels and Stones (771).java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        Set<Character> set = new HashSet<>();
+        int res = 0;
+        
+        for (int i = 0; i < J.length(); i++) {
+            set.add(J.charAt(i));
+        }
+        
+        for (int i = 0; i < S.length(); i++) {
+            if (set.contains(S.charAt(i))) {
+                res++;
+            }
+        }
+        
+        return res;
+    }
+}
