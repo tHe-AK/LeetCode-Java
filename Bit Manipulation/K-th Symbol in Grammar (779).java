4fe5ba0c697ae8998b1f86beee3b1e3751c9e34a
Jason51122 <jason51122@users.noreply.github.com>
2018-10-04 23:14:49 -0700

    Update K-th Symbol in Grammar (779).java

--- Bit Manipulation/K-th Symbol in Grammar (779).java
+++ Bit Manipulation/K-th Symbol in Grammar (779).java
@@ -3,3 +3,20 @@ class Solution {
         return Integer.bitCount(K - 1) & 1;
     }
 }
+
+class Solution {
+    public int kthGrammar(int N, int K) {
+        if (N == 1) {
+            return 0;
+        } else {
+            int digit = kthGrammar(N - 1, (K + 1) / 2);
+            boolean isEven = (K & 1) == 0;
+            
+            if (digit == 0) {
+                return isEven ? 1 : 0;
+            } else {
+                return isEven ? 0 : 1;
+            }
+        }
+    }
+}
