494c66c1d4af7b60ae06e989f3f15376fab09570
Jason51122 <jason51122@users.noreply.github.com>
2019-03-25 21:32:03 -0700

    Create Custom Sort String (791).java

---
+++ Sort/Custom Sort String (791).java
@@ -0,0 +1,24 @@
+class Solution {
+    public String customSortString(String S, String T) {
+        int[] count = new int[26];
+        StringBuilder sb = new StringBuilder();
+        
+        for (char ch : T.toCharArray()) {
+            count[ch - 'a']++;
+        }
+        
+        for (char ch : S.toCharArray()) {
+            while (count[ch - 'a']-- > 0) {
+                sb.append(ch);
+            }
+        }
+        
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            while (count[ch - 'a']-- > 0) {
+                sb.append(ch);
+            }
+        }
+        
+        return sb.toString();
+    }
+}
