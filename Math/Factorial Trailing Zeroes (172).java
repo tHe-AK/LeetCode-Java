d39d7156c1fa0e37922049ed4108044f7893df4a
Jason51122 <jason51122@users.noreply.github.com>
2019-03-28 16:13:33 -0700

    Update Factorial Trailing Zeroes (172).java

--- Math/Factorial Trailing Zeroes (172).java
+++ Math/Factorial Trailing Zeroes (172).java
@@ -1,17 +1,12 @@
-public class Solution {
-    public int trailingZeroes(int n) {
-        return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
-    }
-}
-
-public class Solution {
+class Solution {
     public int trailingZeroes(int n) {
         int count = 0;
         
-        for (long i = 5; i <= n; i *= 5) {
-            count += n / i;
+        while (n > 0) {
+            count += n / 5;
+            n /= 5;
         }
-        
+
         return count;
     }
 }
