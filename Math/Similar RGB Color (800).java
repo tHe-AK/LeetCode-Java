1ccd1ee6d960fd399c1a08c9a3e8e26d4a8e51d3
Jason51122 <jason51122@users.noreply.github.com>
2019-04-16 00:45:05 -0700

    Update Similar RGB Color (800).java

--- Math/Similar RGB Color (800).java
+++ Math/Similar RGB Color (800).java
@@ -1,48 +1,48 @@
 class Solution {
     public String similarRGB(String color) {
-        String str = "#";
+        String res = "#";
         
         for (int i = 1; i < color.length(); i += 2) {
             int num = Integer.parseInt(color.substring(i, i + 2), 16);
-            int digit = num / 0x11 + (num % 0x11 > 8 ? 1 : 0);
-            String hex = Integer.toHexString(digit);
-            str += hex + hex;
+            int hex = num / 0x11 + (num % 0x11 > 8 ? 1 : 0);
+            String str = Integer.toHexString(hex);
+            res += str + str;
         }
 
-        return str;
+        return res;
     }
 }
 
 class Solution {
     public String similarRGB(String color) {
-        String str = "#";
+        String res = "#";
         String base = "0123456789abcdef";
         
         for (int i = 1; i < color.length(); i += 2) {
             int num = Integer.parseInt(color.substring(i, i + 2), 16);
-            String hex = helper(num, base);
-            str += hex + hex;
+            String str = helper(num, base);
+            res += str + str;
         }
 
-        return str;
+        return res;
     }
 
     private String helper(int num, String base) {
-        String str = null;
+        String res = null;
         int min = Integer.MAX_VALUE;
         int idx = num / 16;
 
         for (int i = -1; i <= 1; i++) {
             int start = (idx + i + 16) % 16;
-            String digit = base.substring(start, start + 1);
-            int dist = Math.abs(Integer.parseInt(digit + digit, 16) - num);
+            String str = base.substring(start, start + 1);
+            int dist = Math.abs(Integer.parseInt(str + str, 16) - num);
 
             if (dist < min) {
-                str = digit;
+                res = str;
                 min = dist;
             }
         }
 
-        return str;
+        return res;
     }
 }
