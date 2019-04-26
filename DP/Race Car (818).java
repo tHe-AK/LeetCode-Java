88560fb235c80cf7a9625b7ebc08a8b187fc48a1
Jason51122 <jason51122@users.noreply.github.com>
2019-04-26 00:09:02 -0700

    Update Race Car (818).java

--- DP/Race Car (818).java
+++ DP/Race Car (818).java
@@ -1,9 +1,58 @@
 class Solution {
     public int racecar(int target) {
+        int k = 31 - Integer.numberOfLeadingZeros(target + 1);
+        
+        if ((1 << k) - 1 == target) {
+            return k;
+        }
+        
+        int maxPos = (1 << (k + 1)) - 1;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] { 0, 1 });
+        Set<String> visited = new HashSet<>();
+        visited.add("0 1");
+        int count = 0;
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            count++;
+            
+            for (int i = 0; i < size; i++) {
+                int[] peek = queue.poll();
+                int pos = peek[0];
+                int speed = peek[1];
+                int accPos = pos + speed;
+                int accSpeed = speed << 1;
+                
+                if (accPos == target) {
+                    return count;
+                }
+                
+                helper(queue, visited, accPos, accSpeed, maxPos);
+                int revSpeed = speed > 0 ? -1 : 1;
+                helper(queue, visited, pos, revSpeed, maxPos);
+            }
+        }
+        
+        return -1;
+    }
+    
+    private void helper(Queue<int[]> queue, Set<String> visited, int pos, int speed, int maxPos) {
+        String str = pos + " " + speed;
+        
+        if (!visited.contains(str) && 0 < pos && pos <= maxPos) {
+            visited.add(str);
+            queue.offer(new int[] { pos, speed });
+        }
+    }
+}
+
+class Solution {
+    public int racecar(int target) {
         int[] dp = new int[target + 1];
         
         for (int i = 1; i <= target; i++) {
-            int k = (int) (Math.log(i + 1) / Math.log(2));
+            int k = 31 - Integer.numberOfLeadingZeros(i + 1);
             int low = (1 << k) - 1;
             int high = (1 << (k + 1)) - 1;
             
