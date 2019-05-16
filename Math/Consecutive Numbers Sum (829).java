87b6e0b7665bc8c8a3eb58cee970a0340a636fd4
Jason51122 <jason51122@users.noreply.github.com>
2019-05-15 22:36:20 -0700

    Create Consecutive Numbers Sum (829).java

---
+++ Math/Consecutive Numbers Sum (829).java
@@ -0,0 +1,13 @@
+class Solution {
+    public int consecutiveNumbersSum(int N) {
+        int count = 0;
+        
+        for (int k = 1; k * (k - 1) / 2 < N; k++) {
+            if ((N - k * (k - 1) / 2) % k == 0) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
