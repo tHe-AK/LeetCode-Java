5200d66648cd6558fd1aab83dcffafa249155c9e
Jason51122 <jason51122@users.noreply.github.com>
2019-05-22 22:58:24 -0700

    Create Similar String Groups (839).java

---
+++ Hash Map/Union Find/Similar String Groups (839).java
@@ -0,0 +1,39 @@
+class Solution {
+    public int numSimilarGroups(String[] A) {
+        int N = A.length;
+        boolean[] visited = new boolean[N];
+        int count = 0;
+        
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                dfs(A, i, visited);
+                count++;
+            }
+        }
+        
+        return count;
+    }
+    
+    private void dfs(String[] A, int i, boolean[] visited) {
+        visited[i] = true;
+        
+        for (int j = 0; j < A.length; j++) {
+            if (!visited[j] && areSimilar(A[i], A[j])) {
+                dfs(A, j, visited);
+            }
+        }
+    }
+    
+    private boolean areSimilar(String s1, String s2) {
+        int i = 0;
+        int diff = 0;
+        
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diff++;
+            }
+        }
+        
+        return diff == 0 || diff == 2;
+    }
+}
