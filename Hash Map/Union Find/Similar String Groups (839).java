d8de8216649c1b0a144f624d61d7c91f25726709
Jason51122 <jason51122@users.noreply.github.com>
2019-05-22 23:11:59 -0700

    Update Similar String Groups (839).java

--- Hash Map/Union Find/Similar String Groups (839).java
+++ Hash Map/Union Find/Similar String Groups (839).java
@@ -1,3 +1,76 @@
+class UnionFind {
+    private Map<Integer, Integer> map;
+    private int size;
+    
+    public UnionFind() {
+        map = new HashMap<>();
+        size = 0;
+    }
+    
+    public void insert(int i) {
+        map.put(i, i);
+        size++;
+    }
+    
+    public int find(int child) {
+        int parent = child;
+        
+        while (parent != map.get(parent)) {
+            parent = map.get(parent);
+        }
+        
+        map.put(child, parent);
+        return parent;
+    }
+    
+    public void union(int child1, int child2) {
+        int parent1 = find(child1);
+        int parent2 = find(child2);
+        
+        if (parent1 != parent2) {
+            map.put(parent1, parent2);
+            size--;
+        }
+    }
+    
+    public int size() {
+        return size;
+    }
+}
+
+public class Solution {
+    public int numSimilarGroups(String[] A) {
+        int N = A.length;
+        UnionFind uf = new UnionFind();
+        
+        for (int i = 0; i < N; i++) {
+            uf.insert(i);
+        }
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (areSimilar(A[i], A[j])) {
+                    uf.union(i, j);
+                }
+            }
+        }
+        
+        return uf.size();
+    }
+    
+    private boolean areSimilar(String s1, String s2) {
+        int diff = 0;
+        
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diff++;
+            }
+        }
+        
+        return diff == 0 || diff == 2;
+    }
+}
+
 class Solution {
     public int numSimilarGroups(String[] A) {
         int N = A.length;
