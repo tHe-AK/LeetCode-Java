2a8386ef2c4a95d0d58a251b48d2b3f650d91d60
Jason51122 <jason51122@users.noreply.github.com>
2019-05-27 14:47:44 -0700

    Create Backspace String Compare (844).java

---
+++ String/Backspace String Compare (844).java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1;
+        int j = T.length() - 1;
+        
+        while (true) {
+            int count = 0;
+            
+            while (i >= 0 && (count > 0 || S.charAt(i) == '#')) {
+                count += S.charAt(i) == '#' ? 1 : -1;
+                i--;
+            }
+            
+            count = 0;
+            
+            while (j >= 0 && (count > 0 || T.charAt(j) == '#')) {
+                count += T.charAt(j) == '#' ? 1 : -1;
+                j--;
+            }
+            
+            if (i >= 0 && j >= 0 && S.charAt(i) == T.charAt(j)) {
+                i--;
+                j--;
+            } else {
+                return i == -1 && j == -1;
+            }
+        }
+    }
+}
