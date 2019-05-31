935e0113c32b36c11eb0317c06e8e2d6cb2fd2c0
Jason51122 <jason51122@users.noreply.github.com>
2019-05-31 00:56:38 -0700

    Update Peak Index in a Mountain Array (852).java

--- Binary Search/Peak Index in a Mountain Array (852).java
+++ Binary Search/Peak Index in a Mountain Array (852).java
@@ -9,3 +9,24 @@ class Solution {
         return -1;
     }
 }
+
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int low = 0;
+        int high = A.length - 1;
+        
+        while (low + 1 < high) {
+            int mid = low + (high - low) / 2;
+            
+            if (A[mid - 1] < A[mid] && A[mid] > A[mid + 1]) {
+                return mid;
+            } else if (A[mid - 1] < A[mid] && A[mid] < A[mid + 1]) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        
+        return -1;
+    }
+}
