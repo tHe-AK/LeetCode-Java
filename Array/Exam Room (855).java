648d8938bde40cfd27a90dff5289e33d31b7645b
Jason51122 <jason51122@users.noreply.github.com>
2019-06-02 23:53:46 -0700

    Create Exam Room (855).java

---
+++ Array/Exam Room (855).java
@@ -0,0 +1,49 @@
+class ExamRoom {
+    private Set<Integer> set;
+    private int N;
+
+    public ExamRoom(int N) {
+        this.N = N;
+        set = new TreeSet<>();
+    }
+    
+    public int seat() {
+        if (set.isEmpty()) {
+            set.add(0);
+            return 0;
+        }
+        
+        int prev = -1;
+        int idx = -1;
+        int max = 0;
+
+        for (Integer i : set) {
+            int dist = prev == -1 ? i - prev - 1 : (i - prev) / 2;
+            
+            if (dist > max) {
+                max = dist;
+                idx = prev == -1 ? 0 : prev + dist;
+            }
+            
+            prev = i;
+        }
+        
+        if (N - prev - 1 > max) {
+            idx = N - 1;
+        }
+        
+        set.add(idx);
+        return idx;
+    }
+    
+    public void leave(int p) {
+        set.remove(p);
+    }
+}
+
+/**
+ * Your ExamRoom object will be instantiated and called as such:
+ * ExamRoom obj = new ExamRoom(N);
+ * int param_1 = obj.seat();
+ * obj.leave(p);
+ */
