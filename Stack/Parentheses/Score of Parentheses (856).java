897aa926f6fc9e69a41a36d0e6f9b07b95ed06b1
Jason51122 <jason51122@users.noreply.github.com>
2019-06-03 20:42:49 -0700

    Update Score of Parentheses (856).java

--- Stack/Parentheses/Score of Parentheses (856).java
+++ Stack/Parentheses/Score of Parentheses (856).java
@@ -15,3 +15,24 @@ class Solution {
         return curr;
     }
 }
+
+class Solution {
+    public int scoreOfParentheses(String S) {
+        int ans = 0;
+        int curr = 0;
+        
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == '(') {
+                curr++;
+            } else {
+                curr--;
+                
+                if (S.charAt(i - 1) == '(') {
+                    ans += 1 << curr;
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
