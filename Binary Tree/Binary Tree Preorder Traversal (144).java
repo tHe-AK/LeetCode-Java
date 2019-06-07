e6706ffe38b9484de5ebd1497dfe0e9e6dbb35e9
Jason51122 <jason51122@users.noreply.github.com>
2019-06-07 02:29:37 -0700

    Update Binary Tree Preorder Traversal (144).java

--- Binary Tree/Binary Tree Preorder Traversal (144).java
+++ Binary Tree/Binary Tree Preorder Traversal (144).java
@@ -35,6 +35,43 @@ public class Solution {
  *     TreeNode(int x) { val = x; }
  * }
  */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        
+        if (root == null) {
+            return res;
+        }
+        
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        
+        while (!stack.empty()) {
+            TreeNode peek = stack.pop();
+            res.add(peek.val);
+            
+            if (peek.right != null) {
+                stack.push(peek.right);
+            }
+            
+            if (peek.left != null) {
+                stack.push(peek.left);
+            }
+        }
+        
+        return res;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
