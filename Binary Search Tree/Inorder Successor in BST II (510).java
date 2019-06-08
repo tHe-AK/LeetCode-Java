c3241e7385fbed6ddafb6b425716dacee75e1bcc
Jason51122 <jason51122@users.noreply.github.com>
2019-06-08 02:03:55 -0700

    Update Inorder Successor in BST II (510).java

--- Binary Search Tree/Inorder Successor in BST II (510).java
+++ Binary Search Tree/Inorder Successor in BST II (510).java
@@ -1,1 +1,29 @@
-
+public class Solution {
+    public TreeNode inorderSuccessor(TreeNode p) {
+        TreeNode successor = null;
+        TreeNode node = p.right;
+      
+        while (node != null) {
+            successor = node;
+            node = node.left;
+        }
+      
+        if (successor != null) {
+            return successor;
+        }
+      
+        node = p;
+      
+        while (node != null) {
+            TreeNode parent = node.parent;
+          
+            if (parent != null && parent.left == node) {
+              return parent;
+            } else {
+              node = parent;
+            }
+        }
+        
+        return null;
+    }
+}
