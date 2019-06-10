993dd1b1c0ac5be7010f86a1b2de2b962242f8eb
Jason51122 <jason51122@users.noreply.github.com>
2019-06-10 00:49:44 -0700

    Update Length of Longest Fibonacci Subsequence (873).java

--- DP/Length of Longest Fibonacci Subsequence (873).java
+++ DP/Length of Longest Fibonacci Subsequence (873).java
@@ -1,6 +1,38 @@
 class Solution {
     public int lenLongestFibSubseq(int[] A) {
         int N = A.length;
+        Set<Integer> set = new HashSet<>();
+        int max = 0;
+        
+        for (int i : A) {
+            set.add(i);
+        }
+        
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                int prev = A[i];
+                int curr = A[j];
+                int next = prev + curr;
+                int count = 0;
+                
+                while (set.contains(next)) {
+                    count++;
+                    prev = curr;
+                    curr = next;
+                    next = prev + curr;
+                }
+                
+                max = Math.max(max, count);
+            }
+        }
+        
+        return max > 0 ? max + 2: 0;
+    }
+}
+
+class Solution {
+    public int lenLongestFibSubseq(int[] A) {
+        int N = A.length;
         int[][] dp = new int[N][N];
         int max = 0;
         Map<Integer, Integer> map = new HashMap<>();
