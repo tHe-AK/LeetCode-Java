4916bc0c27feb1cdeab8585aae2da9261e134e7b
Jason51122 <jason51122@users.noreply.github.com>
2019-06-10 23:11:53 -0700

    Create Stone Game (877).java

---
+++ DP/Stone Game (877).java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
+
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int N = piles.length;
+        int[][] dp = new int[N][N];
+        
+        for (int i = 0; i < N; i++) {
+            dp[i][i] = piles[i];
+        }
+        
+        for (int k = 1; k < N; k++) {
+            for (int i = 0; i < N - k; i++) {
+                int j = i + k;
+                dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        
+        return dp[0][N - 1] > 0;
+    }
+}
