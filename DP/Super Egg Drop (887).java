1a783d7e2806f2b611bb3d4be5cebadea08819c6
Jason51122 <jason51122@users.noreply.github.com>
2019-06-15 02:28:40 -0700

    Update Super Egg Drop (887).java

--- DP/Super Egg Drop (887).java
+++ DP/Super Egg Drop (887).java
@@ -1,6 +1,100 @@
 class Solution {
     public int superEggDrop(int K, int N) {
         int[][] dp = new int[N + 1][K + 1];
+        
+        for (int i = 1; i <= N; i++) {
+            dp[i][1] = i;
+        }
+        
+        for (int i = 1; i <= N; i++) {
+            for (int j = 2; j <= K; j++) {
+                dp[i][j] = i;
+
+                for (int k = 1; k <= i; k++) {
+                    dp[i][j] = Math.min(dp[i][j], Math.max(dp[k - 1][j - 1], dp[i - k][j]) + 1);
+                }                
+            }
+        }
+        
+        return dp[N][K];
+    }
+}
+
+class Solution {
+    public int superEggDrop(int K, int N) {
+        int[][] dp = new int[N + 1][K + 1];
+        return helper(N, K, dp);
+    }
+    
+    private int helper(int N, int K, int[][] dp) {
+        if (N <= 1) {
+            return N;
+        }
+        if (K == 1) {
+            return N;
+        }
+        
+        if (dp[N][K] > 0) {
+            return dp[N][K];
+        }
+        
+        dp[N][K] = N;
+        
+        for (int i = 1; i <= N; i++) {
+            int left = helper(i - 1, K - 1, dp);
+            int right = helper(N - i, K, dp);
+            dp[N][K] = Math.min(dp[N][K], Math.max(left, right) + 1);
+        }
+        
+        return dp[N][K];
+    }
+}
+
+class Solution {
+    public int superEggDrop(int K, int N) {
+        int[][] dp = new int[N + 1][K + 1];
+        return binarySearch(N, K, dp);
+    }
+    
+    private int binarySearch(int N, int K, int[][] dp) {
+        if (K == 0 || N == 0) {
+            return 0;
+        }
+        
+        if (K == 1) {
+            return N;
+        }
+        
+        if (dp[N][K] > 0) {
+            return dp[N][K];
+        }
+        
+        int low = 1;
+        int high = N;
+        dp[N][K] = N;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int left = binarySearch(mid - 1, K - 1, dp);
+            int right = binarySearch(N - mid, K, dp);
+            dp[N][K] = Math.min(dp[N][K], Math.max(left, right) + 1);
+            
+            if (left == right) {
+                break;
+            } else if (left < right) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return dp[N][K];
+    }
+}
+
+class Solution {
+    public int superEggDrop(int K, int N) {
+        int[][] dp = new int[N + 1][K + 1];
         int i = 0;
         
         while (dp[i][K] < N) {
