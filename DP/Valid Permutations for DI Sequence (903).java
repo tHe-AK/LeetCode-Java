6351486c5c156946da029a70004d377ccaa8ea13
Jason51122 <jason51122@users.noreply.github.com>
2019-06-18 00:35:04 -0700

    Update Valid Permutations for DI Sequence (903).java

--- DP/Valid Permutations for DI Sequence (903).java
+++ DP/Valid Permutations for DI Sequence (903).java
@@ -27,3 +27,36 @@ class Solution {
         return dp[N - 1][0];
     }
 }
+
+class Solution {
+    public int numPermsDISequence(String S) {
+        int MOD = 1_000_000_007;
+        int N = S.length() + 1;
+        int[] dp = new int[N];
+        
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+        }
+        
+        for (int i = 1; i < N; i++) {
+            int[] temp = new int[N];
+            int curr = 0;
+
+            if (S.charAt(i - 1) == 'D') {
+                for (int j = N - i; j >= 1; j--) {
+                    curr = (curr + dp[j]) % MOD;
+                    temp[j - 1] = curr;
+                }
+            } else {
+                for (int j = 0; j < N - i; j++) {
+                    curr = (curr + dp[j]) % MOD;
+                    temp[j] = curr;
+                }
+            }
+            
+            dp = temp;
+        }
+        
+        return dp[0];
+    }
+}
