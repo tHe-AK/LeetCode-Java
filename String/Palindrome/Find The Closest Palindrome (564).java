fd40de225d943614b36a5f4fadff69186f4189ea
Jason51122 <jason51122@users.noreply.github.com>
2019-06-19 22:26:14 -0700

    Update Find The Closest Palindrome (564).java

--- String/Palindrome/Find The Closest Palindrome (564).java
+++ String/Palindrome/Find The Closest Palindrome (564).java
@@ -1,42 +1,44 @@
 class Solution {
     public String nearestPalindromic(String n) {
-        List<String> list = new ArrayList<>();
+        List<Long> list = new ArrayList<>();
+        long num =  Long.parseLong(n);
         long min = Long.MAX_VALUE;
-        String res = "";
+        long res = 0;
         
         list.add(mirroring(n, -1));
         list.add(mirroring(n, 0));
         list.add(mirroring(n, 1));
         
-        for (String str : list) {
-            long diff = Math.abs(Long.parseLong(str) - Long.parseLong(n));
+        for (Long candidate : list) {
+            long diff = Math.abs(candidate - num);
             
             if (diff > 0 && diff < min) {
                 min = diff;
-                res = str;
+                res = candidate;
             }
         }
         
-        return res;
+        return String.valueOf(res);
     }
     
-    private String mirroring(String s, int diff) {
+    private long mirroring(String s, int diff) {
         int len = s.length();
         int idx = (len + 1) / 2;
         int x = Integer.valueOf(s.substring(0, idx));
         int y = x + diff;
         
         if (y == 0 || String.valueOf(y).length() < String.valueOf(x).length()) {
-            String str = "";
+            long num = 0;
             
             for (int i = 0; i < len - 1; i++) {
-                str += "9";
+                num = num * 10 + 9;
             }
             
-            return str.isEmpty() ? "0" : str;
+            return num;
         }
         
-        String str = String.valueOf(y);
-        return str + new StringBuilder(str.substring(0, len - idx)).reverse().toString();
+        String left = String.valueOf(y);
+        String right = new StringBuilder(left.substring(0, len - idx)).reverse().toString();
+        return Long.parseLong(left + right);
     }
 }
