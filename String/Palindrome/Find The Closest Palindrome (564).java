d0b8026eaa68d98d41409b4c81784d4072d412f2
Jason51122 <jason51122@users.noreply.github.com>
2019-06-19 22:13:33 -0700

    Update Find The Closest Palindrome (564).java

--- String/Palindrome/Find The Closest Palindrome (564).java
+++ String/Palindrome/Find The Closest Palindrome (564).java
@@ -1,62 +1,42 @@
-public class Solution {
+class Solution {
     public String nearestPalindromic(String n) {
-        if (n.equals("1")) {
-            return "0";
-        }
-
-        String a = mirroring(n);
-        long diff1 = Math.abs(Long.parseLong(n) - Long.parseLong(a));
-        if (diff1 == 0) {
-            diff1 = Long.MAX_VALUE;
-        }
-
-        StringBuilder s = new StringBuilder(n);
-        int i = (s.length() - 1) / 2;
+        List<String> list = new ArrayList<>();
+        long min = Long.MAX_VALUE;
+        String res = "";
         
-        while (i >= 0 && s.charAt(i) == '0') {
-            s.replace(i, i + 1, "9");
-            i--;
-        }
+        list.add(mirroring(n, -1));
+        list.add(mirroring(n, 0));
+        list.add(mirroring(n, 1));
         
-        if (i == 0 && s.charAt(i) == '1') {
-            s.delete(0, 1);
-            int mid = (s.length() - 1) / 2;
-            s.replace(mid, mid + 1, "9");
-        } else {
-            s.replace(i, i + 1, "" + (char)(s.charAt(i) - 1));
+        for (String str : list) {
+            long diff = Math.abs(Long.parseLong(str) - Long.parseLong(n));
+            
+            if (diff > 0 && diff < min) {
+                min = diff;
+                res = str;
+            }
         }
         
-        String b = mirroring(s.toString());
-        long diff2 = Math.abs(Long.parseLong(n) - Long.parseLong(b));
-
-        s = new StringBuilder(n);
-        i = (s.length() - 1) / 2;
-        
-        while (i >= 0 && s.charAt(i) == '9') {
-            s.replace(i, i + 1, "0");
-            i--;
-        }
-        
-        if (i < 0) {
-            s.insert(0, "1");
-        } else {
-            s.replace(i, i + 1, "" + (char)(s.charAt(i) + 1));
-        }
-        
-        String c = mirroring(s.toString());
-        long diff3 = Math.abs(Long.parseLong(n) - Long.parseLong(c));
-
-        if (diff2 <= diff1 && diff2 <= diff3) {
-            return b;
-        } else if (diff1 <= diff3 && diff1 <= diff2) {
-            return a;
-        } else {
-            return c;
-        }
+        return res;
     }
     
-    private String mirroring(String s) {
-        String x = s.substring(0, s.length() / 2);
-        return x + (s.length() % 2 == 1 ? s.charAt(s.length() / 2) : "") + new StringBuilder(x).reverse().toString();
+    private String mirroring(String s, int diff) {
+        int len = s.length();
+        int idx = (len + 1) / 2;
+        int x = Integer.valueOf(s.substring(0, idx));
+        int y = x + diff;
+        
+        if (y == 0 || String.valueOf(y).length() < String.valueOf(x).length()) {
+            String str = "";
+            
+            for (int i = 0; i < len - 1; i++) {
+                str += "9";
+            }
+            
+            return str.isEmpty() ? "0" : str;
+        }
+        
+        String str = String.valueOf(y);
+        return str + new StringBuilder(str.substring(0, len - idx)).reverse().toString();
     }
 }
