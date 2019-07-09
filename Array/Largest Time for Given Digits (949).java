34898fc1eebd5c2f4f6517c6db26c6da3870a0e9
Jason51122 <jason51122@users.noreply.github.com>
2019-07-09 01:03:13 -0700

    Create Largest Time for Given Digits (949).java

---
+++ Array/Largest Time for Given Digits (949).java
@@ -0,0 +1,23 @@
+class Solution {
+    public String largestTimeFromDigits(int[] A) {
+        String ans = "";
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 4; k++) {
+                    if (i != j && i != k && j != k) {
+                        String h = "" + A[i] + A[j];
+                        String m = "" + A[k] + A[6 - i - j - k];
+                        String t = h + ":" + m;
+                        
+                        if (h.compareTo("24") < 0 && m.compareTo("60") < 0 && t.compareTo(ans) > 0) {
+                            ans = t;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
