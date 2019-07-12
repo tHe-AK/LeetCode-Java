fb927c2becbd480bd3d89089a0debadd8d8f8b5e
Jason51122 <jason51122@users.noreply.github.com>
2019-07-11 19:11:13 -0700

    Update Delete Columns to Make Sorted III (960).java

--- String/Longest Increasing Sequence/Delete Columns to Make Sorted III (960).java
+++ String/Longest Increasing Sequence/Delete Columns to Make Sorted III (960).java
@@ -3,9 +3,9 @@ class Solution {
         int N = A[0].length();
         int[] dp = new int[N];
         Arrays.fill(dp, 1);
-        int max = 1;
+        int max = 0;
         
-        for (int i = 1; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             search: for (int j = 0; j < i; j++) {
                 for (String row: A) {
                     if (row.charAt(j) > row.charAt(i)) {
@@ -14,8 +14,9 @@ class Solution {
                 }
 
                 dp[i] = Math.max(dp[i], dp[j] + 1);
-                max = Math.max(max, dp[i]);
             }
+            
+            max = Math.max(max, dp[i]);
         }
 
         return N - max;
