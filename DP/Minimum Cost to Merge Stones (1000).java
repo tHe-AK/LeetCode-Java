9e2b56ed3235d25e273fd9a7d9546321e26f23c3
Jason51122 <jason51122@users.noreply.github.com>
2019-07-25 01:43:17 -0700

    Create Minimum Cost to Merge Stones (1000).java

---
+++ DP/Minimum Cost to Merge Stones (1000).java
@@ -0,0 +1,39 @@
+class Solution {
+    public int mergeStones(int[] stones, int K) {
+        int N = stones.length;
+        K--;
+        
+        if ((N - 1) % K != 0) {
+            return -1;
+        }
+        
+        int[] sum = new int[N + 1];
+        int[][] dp = new int[N][N];
+        
+        for (int j = 0; j < N; j++) {
+            sum[j + 1] = sum[j] + stones[j];
+        }
+        
+        for (int k = K; k < N; k++) {
+            for (int i = 0; i + k < N; i++) {
+                int j = i + k;
+
+                if (k == K) {
+                    dp[i][j] = sum[j + 1] - sum[i];
+                } else {
+                    dp[i][j] = Integer.MAX_VALUE;
+
+                    for (int mid = i; mid < j; mid += K) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i][mid] + dp[mid + 1][j]);
+                    }
+
+                    if ((j - i) % K == 0) {
+                        dp[i][j] += sum[j + 1] - sum[i];
+                    }
+                }
+            }
+        }
+        
+        return dp[0][N - 1];
+    }
+}
