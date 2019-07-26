371d1ecb19e8a5a885a9c0676ba8a8195423e841
Jason51122 <jason51122@users.noreply.github.com>
2019-07-26 00:17:39 -0700

    Update Minimum Domino Rotations For Equal Row (1007).java

--- Array/Minimum Domino Rotations For Equal Row (1007).java
+++ Array/Minimum Domino Rotations For Equal Row (1007).java
@@ -46,3 +46,29 @@ class Solution {
         return -1;
     }
 }
+
+class Solution {
+    public int minDominoRotations(int[] A, int[] B) {
+        int N = 6;
+        int[] count1 = new int[N + 1];
+        int[] count2 = new int[N + 1];
+        int[] same = new int[N + 1];
+        
+        for (int i = 0; i < A.length; i++) {
+            count1[A[i]]++;
+            count2[B[i]]++;
+            
+            if (A[i] == B[i]) {
+                same[A[i]]++;
+            }
+        }
+        
+        for (int i = 1; i <= N; i++) {
+            if (count1[i] + count2[i] - same[i] == A.length) {
+                return Math.min(count1[i], count2[i]) - same[i];
+            }
+        }
+
+        return -1;
+    }
+}
