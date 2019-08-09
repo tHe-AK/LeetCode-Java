aef0740081425620798582c24c43d6e3d70d4225
Jason51122 <jason51122@users.noreply.github.com>
2019-08-09 01:44:29 -0700

    Update Missing Element in Sorted Array (1060).java

--- Binary Search/Missing Element in Sorted Array (1060).java
+++ Binary Search/Missing Element in Sorted Array (1060).java
@@ -2,6 +2,32 @@ class Solution {
     public int missingElement(int[] nums, int k) {
         int low = 0;
         int high = nums.length - 1;
+        int missing = nums[high] - nums[0] - high;
+        
+        if (k > missing) {
+            return nums[high] + k - missing;
+        }
+
+        while (low + 1 < high) {
+            int mid = low + (high - low) / 2;
+            missing = nums[mid] - nums[low] - (mid - low);
+            
+            if (missing >= k) {
+                high = mid;
+            } else {
+                k -= missing;
+                low = mid;
+            }
+        }
+        
+        return nums[low] + k;
+    }
+}
+
+class Solution {
+    public int missingElement(int[] nums, int k) {
+        int low = 0;
+        int high = nums.length - 1;
         
         while (low <= high) {
             int mid = low + (high - low) / 2;
