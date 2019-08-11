e1d7fe481021307c6840faa04f2137b4eba92216
Jason51122 <jason51122@users.noreply.github.com>
2019-08-11 03:51:10 -0700

    Update Campus Bikes II (1066).java

--- Heap/Campus Bikes II (1066).java
+++ Heap/Campus Bikes II (1066).java
@@ -35,3 +35,41 @@ class Solution {
         return -1;
     }
 }
+
+class Solution {
+    public int assignBikes(int[][] workers, int[][] bikes) {
+        int M = workers.length;
+        int N = bikes.length;
+        int min = Integer.MAX_VALUE;
+        int[][] dp = new int[M + 1][1 << N];
+        
+        for (int i = 0; i <= M; i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+        
+        dp[0][0] = 0;
+        
+        for (int i = 0; i <= M; i++) {
+            for (int j = 0; j < (1 << N); j++) {
+                if (dp[i][j] != Integer.MAX_VALUE) {
+                    if (i < M) {
+                        int[] worker = workers[i];
+                        
+                        for (int k = 0; k < N; k++) {
+                            if ((j & (1 << k)) == 0) {
+                                int next = j | (1 << k);
+                                int[] bike = bikes[k];
+                                int dist = Math.abs(bike[0] - worker[0]) + Math.abs(bike[1] - worker[1]);
+                                dp[i + 1][next] = Math.min(dp[i + 1][next], dp[i][j] + dist);
+                            }
+                        }
+                    } else {
+                        min = Math.min(min, dp[i][j]); 
+                    }
+                }
+            }
+        }
+                
+        return min;
+    }
+}
