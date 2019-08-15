c630e9ca0c0ff578b54822c011492af3f0625361
Jason51122 <jason51122@users.noreply.github.com>
2019-08-14 20:39:28 -0700

    Create Print Odd Even Num.java

---
+++ Concurrency/Print Odd Even Num.java
@@ -0,0 +1,44 @@
+class PrintOddEvenNum {
+    private int num;
+    private int max;
+
+    public PrintOddEvenNum(int max) {
+        num = 0;
+        this.max = max;
+    }
+
+    public synchronized void print(int k) {
+        while (true) {
+            try {
+                while (num <= max && num % 2 == k) {
+                    wait();
+                }
+
+                if (num > max) {
+                    break;
+                }
+
+                System.out.println(num++);
+                notify();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintOddEvenNum s = new PrintOddEvenNum(100);
+        Thread t1 = new Thread() {
+            public void run() {
+                s.print(0);
+            }
+        };
+        Thread t2 = new Thread() {
+            public void run() {
+                s.print(1);
+            }
+        };
+        t1.start();
+        t2.start();
+    }
+}
