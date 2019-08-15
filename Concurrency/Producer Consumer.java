4afc713578f7aa306cfe0ad4702df6106877dd97
Jason51122 <jason51122@users.noreply.github.com>
2019-08-14 22:04:41 -0700

    Update Producer Consumer.java

--- Concurrency/Producer Consumer.java
+++ Concurrency/Producer Consumer.java
@@ -1,51 +1,55 @@
 import java.util.*;
 
-class ProducerConsumer {
+class Solution {
     private Queue<Integer> queue;
     private int num;
     private int max;
 
-    public ProducerConsumer(int max) {
+    public Solution(int max) {
         queue = new LinkedList<>();
         num = 0;
         this.max = max;
     }
 
-    public synchronized void produce() {
+    public void produce() {
         while (true) {
-            try {
-                while (queue.size() == max) {
-                    wait();
-                }
+            synchronized (this) {
+                try {
+                    while (queue.size() == max) {
+                        wait();
+                    }
 
-                System.out.println("Produce: " + num);
-                queue.offer(num++);
-                notify();
-                Thread.sleep(1000); 
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                    System.out.println("Produce: " + num);
+                    queue.offer(num++);
+                    notify();
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
-    
-    public synchronized void consume() {
-        while (true) {
-            try {
-                while (queue.isEmpty()) {
-                    wait();
-                }
 
-                System.out.println("Consume: " + queue.poll());
-                notify();
-                Thread.sleep(1000); 
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+    public void consume() {
+        while (true) {
+            synchronized (this) {
+                try {
+                    while (queue.isEmpty()) {
+                        wait();
+                    }
+
+                    System.out.println("Consume: " + queue.poll());
+                    notify();
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
     public static void main(String[] args) {
-        ProducerConsumer pc = new ProducerConsumer(2);
+        Solution pc = new Solution(2);
         Thread t1 = new Thread() {
             public void run() {
                 pc.produce();
